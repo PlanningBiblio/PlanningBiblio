5d7a1e91f2680403d00eb5efcad0a518a59ae339
Jérôme Combes <jerome@planningbiblio.fr>
2013-09-27 14:46:17 +0200

    Ajoute des sauts de lignes dans agenda/index.php de façon à pouvoir utiliser correctement le calendrier

--- agenda/index.php
+++ agenda/index.php
@@ -7,7 +7,7 @@ Copyright (C) 2011-2013 - Jérôme Combes
 
 Fichier : agenda/index.php
 Création : mai 2011
-Dernière modification : 26 septembre 2013
+Dernière modification : 27 septembre 2013
 Auteur : Jérôme Combes, jerome@planningbilbio.fr
 
 Description :
@@ -103,6 +103,7 @@ else{
 }
 
 if(!isset($agent)){
+  echo "<br/><br/>";
   include "include/footer.php";
   exit;
 }
