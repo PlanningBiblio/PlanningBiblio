a6d652a3c455e97e7e1142c893b25e79aa392798
Jérôme Combes <jerome@planningbiblio.fr>
2013-11-08 08:08:16 +0100

    Affichage du site dans la liste des postes, Affichage de la liste des postes avec DataTable

--- postes/index.php
+++ postes/index.php
@@ -1,20 +1,20 @@
 <?php
-/********************************************************************************************************************************
-* Planning Biblio, Version 1.6
-* Licence GNU/GPL (version 2 et au dela)											*
-* Voir les fichiers README.txt et COPYING.txt											*
-* Copyright (C) 2011-2013 - Jérôme Combes											*
-*																*
-* Fichier : postes/index.php													*
-* Création : mai 2011														*
-* Dernière modification : 16 janvier 2013											*
-* Auteur : Jérôme Combes, jerome@planningbilbio.fr										*
-*																*
-* Description :															*
-* Affiche la liste des postes dans un tableau avec un filtre sur le nom des postes.						*
-*																*
-* Page appelée par le fichier index.php. Accessible à partir du menu "Administration / Les postes" 				*
-*********************************************************************************************************************************/
+/*
+Planning Biblio, Version 1.6
+Licence GNU/GPL (version 2 et au dela)
+Voir les fichiers README.txt et COPYING.txt
+Copyright (C) 2011-2013 - Jérôme Combes
+
+Fichier : postes/index.php
+Création : mai 2011
+Dernière modification : 8 novembre 2013
+Auteur : Jérôme Combes, jerome@planningbilbio.fr
+
+Description :
+Affiche la liste des postes dans un tableau avec un filtre sur le nom des postes.
+
+Page appelée par le fichier index.php. Accessible à partir du menu "Administration / Les postes" 
+*/
 
 require_once "class.postes.php";
 
@@ -41,23 +41,25 @@ if($db->result){
     $postes_utilises[]=$elem['poste'];
   }
 }
+
+// Sélection des activités
+$activitesTab=array();
+$db=new db();
+$db->select("activites");
+if($db->result){
+  foreach($db->result as $elem){
+    $activitesTab[$elem["id"]]=$elem["nom"];
+  }
+}
+
 ?>
 <br/>
 
 <form name="form" action="#">
 <input type='hidden' name='page' value='postes/index.php' />
 <table><tr valign='top'>
-<td style='width:270px'>
-<h3 style='margin-top:0px;'>Liste des postes</h3>
-</td>
-<td>
-Rechercher : 
-</td><td>
-<?php echo "<input type='text' name='nom' size='8' value='$nom' />\n"; ?>
-</td><td style='width:80px'>
-<input type='submit' value='OK'/>
-</td><td>
-<input type="button" value="Ajouter" onclick='location.href="index.php?page=postes/modif.php"'/>
+<td style='width:270px'><h3 style='margin-top:0px;'>Liste des postes</h3></td>
+<td><input type="button" value="Ajouter" id="ajouter" onclick='location.href="index.php?page=postes/modif.php"'/>
 </td></tr></table>
 </form>
 
@@ -68,86 +70,89 @@ $p=new postes();
 $p->fetch($tri,$nom,$groupe);
 $postes=$p->elements;
 
-echo "<table style='width:100%;' cellspacing='0'>\n";
-echo "<tr class='th'><td>";
-echo "&nbsp;";
+echo "<table id='tablePostes'>\n";
+echo "<thead><tr><th>&nbsp;</th>\n";
 if(in_array(13,$droits)){
-  echo "</td><td>";
-  echo "ID\n";
+  echo "<th>ID</th>";
 }
-echo "</td><td>";
-echo "Etage\n";
-echo "&nbsp;&nbsp;<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=etage'><img src='img/up.png' alt='+' border='0' style='width:10px;'/></a>\n";
-echo "<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=etage%20desc'><img src='img/down.png' alt='-' border='0' style='width:10px;'/></a>\n";
-echo "</td><td>";
-echo "Nom du poste\n";
-echo "&nbsp;&nbsp;<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=nom'><img src='img/up.png' alt='+' border='0' style='width:10px;'/></a>\n";
-echo "<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=nom%20desc'><img src='img/down.png' alt='-' border='0' style='width:10px;'/></a>\n";
-echo "</td><td>";
-echo "Activités\n";
-echo "</td><td>";
-echo "Obligatoire/renfort\n";
-echo "&nbsp;&nbsp;<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=obligatoire'><img src='img/up.png' alt='+' border='0' style='width:10px;'/></a>\n";
-echo "<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=obligatoire%20desc'><img src='img/down.png' alt='-' border='0' style='width:10px;'/></a>\n";
-echo "</td><td>";
-echo "Bloquant\n";
-echo "&nbsp;&nbsp;<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=bloquant'><img src='img/up.png' alt='+' border='0' style='width:10px;'/></a>\n";
-echo "<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=bloquant%20desc'><img src='img/down.png' alt='-' border='0' style='width:10px;'/></a>\n";
-echo "</td><td>\n";
-echo "Statistiques\n";
-echo "&nbsp;&nbsp;<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=statistiques'><img src='img/up.png' alt='+' border='0' style='width:10px;'/></a>\n";
-echo "<a href='index.php?page=postes/index.php&amp;nom=$nom&amp;tri=statistiques%20desc'><img src='img/down.png' alt='-' border='0' style='width:10px;'/></a>\n";
-echo "</td></tr>\n";
+echo "<th>Nom du poste</th>\n";
+if($config['Multisites-nombre']>1){
+  echo "<th>Site</th>\n";
+}
+echo <<<EOD
+  <th>Etage</th>
+  <th>Activités</th>
+  <th>Obligatoire/renfort</th>
+  <th>Bloquant</th>
+  <th>Statistiques</th>
+  </tr></thead>
+  <tbody>
+EOD;
 
 for($i=0;$i<count($postes);$i++){
   $id=$postes[$i]['id'];
-  $class=$i%2?"tr1":"tr2";
-  
-  echo "<tr class='$class'><td>\n";
-  echo "<a href='index.php?page=postes/modif.php&amp;id=$id'><img src='img/modif.png' border='0' alt='modif' /></a>\n";
-  if(!in_array($id,$postes_utilises)){
-    echo "&nbsp;&nbsp;";
-    echo "<a href='javascript:supprime(\"postes\",$id);'><img src='img/suppr.png' border='0' alt='supp' /></a>\n";
-  }
-  if(in_array(13,$droits)){
-    echo "</td><td>\n";
-    echo $postes[$i]['id'];
-  }
-  echo "</td><td>\n";
-  echo $postes[$i]['etage'];
-  echo "</td><td>\n";
-  echo $postes[$i]['nom'];
-  echo "</td><td>\n";
-  $activites=unserialize($postes[$i]['activites']);
-  $liste=array();
-  if(is_array($activites)){
-    $activites=join($activites,",");
-    $act=new db();
-    $act->query("SELECT `nom` FROM `{$dbprefix}activites` WHERE `id` IN ($activites);");
-    $j=0;
-    if($act->result){
-      echo "<div id='act$i' onmouseover='affiche_activites($i,\"affiche\");'>\n";		// mettre un timeout dans la fonction
-      foreach($act->result as $elem){
-	$liste[]=$elem['nom'];
-	if($j++<3){
-	  echo $elem['nom'].", ";
-	}
-      }
-      echo "...</div>\n";
-      echo "<div id='act{$i}b' style='display:none;' onmouseout='affiche_activites($i,\"cache\");'>";
-      foreach($liste as $elem){
-	echo $elem."<br/>\n";
-      }
-      echo "</div>\n";
+
+  // Affichage des 3 premières activités dans le tableau, toutes les activités dans l'infobulle
+  $activites=array();
+  $activitesAffichees=array();
+  $activitesPoste=unserialize($postes[$i]['activites']);
+  foreach($activitesPoste as $act){
+    $activites[]=$activitesTab[$act];
+    if(count($activitesAffichees)<3){
+      $activitesAffichees[]=$activitesTab[$act];
     }
   }
-  echo "</td><td>\n";
-  echo $postes[$i]['obligatoire'];
-  echo "</td><td>\n";
-  echo $postes[$i]['bloquant']?"Oui":"Non";
-  echo "</td><td>\n";
-  echo $postes[$i]['statistiques']?"Oui":"Non";
-  echo "</td></tr>\n";
+  $activites=join(", ",$activites);
+  $activitesAffichees=join(", ",$activitesAffichees);
+  if(count($activitesPoste)>3){
+    $activitesAffichees.=" ...";
+  }
+
+  echo "<tr><td style='white-space:nowrap;'>\n";
+  echo "<a href='index.php?page=postes/modif.php&amp;id=$id'><img src='img/modif.png' border='0' alt='modif' /></a>\n";
+  if(!in_array($id,$postes_utilises)){
+    echo "&nbsp;<a href='javascript:supprime(\"postes\",$id);'><img src='img/suppr.png' border='0' alt='supp' /></a></td>\n";
+  }
+  if(in_array(13,$droits)){
+    echo "<td>{$postes[$i]['id']}</td>\n";
+  }
+  echo "<td>{$postes[$i]['nom']}</td>\n";
+  if($config['Multisites-nombre']>1){
+    echo "<td>".$config["Multisites-site{$postes[$i]['site']}"]."</td>\n";
+  }
+  echo "<td>{$postes[$i]['etage']}</td>\n";
+  echo "<td title='$activites' size='100'>$activitesAffichees</td>\n";
+  echo "<td>{$postes[$i]['obligatoire']}</td>\n";
+  echo "<td>".($postes[$i]['bloquant']?"Oui":"Non")."</td>\n";
+  echo "<td>".($postes[$i]['statistiques']?"Oui":"Non")."</td>\n";
+  echo "</tr>\n";
 }
 
+echo "</tbody>\n";
 echo "</table>\n";
+?>
+
+<script type='text/JavaScript'>
+$(document).ready(function() {
+  $("#tablePostes").dataTable({
+    "bJQueryUI": true,
+    "sPaginationType": "full_numbers",
+    "bStateSave": false,
+    "aaSorting" : [[2,"asc"],[3,"asc"],[4,"asc"]],
+    "aoColumns" : [{"bSortable":false},{"bSortable":true},{"bSortable":true},{"bSortable":true},{"bSortable":true},
+      {"bSortable":true},{"bSortable":true},{"bSortable":true},
+      <?php
+      if($config['Multisites-nombre']>1){
+	echo '{"bSortable":true},';
+      }
+      ?>
+      ],
+    "aLengthMenu" : [[25,50,75,100,-1],[25,50,75,100,"Tous"]],
+    "iDisplayLength" : 25,
+    "oLanguage" : {"sUrl" : "js/dataTables/french.txt"}
+  });
+
+  $(document).tooltip();
+  $("#ajouter").button();
+});
+</script>
