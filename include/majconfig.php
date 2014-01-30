ca155b68059359e480398b2bf93f18352fcd497e
Jérôme Combes <jerome@planningbiblio.fr>
2014-01-30 18:47:18 +0100

    Modification du numéro de version (1.6.8)

--- include/majconfig.php
+++ include/majconfig.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Planning Biblio, Version 1.6.7
+Planning Biblio, Version 1.6.8
 Licence GNU/GPL (version 2 et au dela)
 Voir les fichiers README.txt et COPYING.txt
 Copyright (C) 2011-2014 - Jérôme Combes
@@ -28,7 +28,7 @@ $Fnm = "include/config.php";
 $file=Array();
 $file[]="<?php\n";
 $file[]="/*\n";
-$file[]="Planning Biblio, Version 1.6.7\n";
+$file[]="Planning Biblio, Version 1.6.8\n";
 $file[]="Licence GNU/GPL (version 2 et au dela)\n";
 $file[]="Voir les fichiers README.txt et COPYING.txt\n";
 $file[]="Copyright (C) 2011-2014 - Jérôme Combes\n";
