52af541158225362f8f52027e9bd0fb1b90b6a5a
Jérôme Combes <jerome@planningbiblio.fr>
2014-12-08 20:49:57 +0100

    Modifie .gitignore et ajoute un fichier index dans le dossier data pour conserver ce dernier dans git

---
+++ data/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Planning Biblio, Version 1.8.7
+Licence GNU/GPL (version 2 et au dela)
+Voir les fichiers README.md et LICENSE
+Copyright (C) 2011-2014 - Jérôme Combes
+
+Fichier : data/index.php
+Création : 8 décembre 2014
+Dernière modification : 8 décembre 2014
+Auteur : Jérôme Combes, jerome@planningbilbio.fr
+
+Description :
+Fichier permettant de conserver le dossier data dans git
+?>
