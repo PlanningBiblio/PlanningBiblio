bcd6512cbc930167039cfe1d5430b50d78eac9de
Jérôme Combes <jerome@planningbiblio.fr>
2015-07-02 00:05:50 +0200

    Modification du numéro de version (2.0)

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Planning Biblio, Version 1.9.7
+Planning Biblio, Version 2.0
 Licence GNU/GPL (version 2 et au dela)
 Voir les fichiers README.md et LICENSE
 Copyright (C) 2011-2015 - Jérôme Combes
@@ -23,7 +23,7 @@ Inclut à la fin le fichier footer.php
 session_start();
 
 // Version
-$version="1.9.7";
+$version="2.0";
 
 // Redirection vers setup si le fichier config est absent
 if(!file_exists("include/config.php")){
