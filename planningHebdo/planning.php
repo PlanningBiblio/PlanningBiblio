06d9544e74646272aa86ff2dedda4ec7a68c6e9d
Jérôme Combes <jerome@planningbiblio.fr>
2015-11-08 11:05:21 +0100

    Ajout fichier planning.php dans planningHebdo pour affichage liste présent en bas des plannings

---
+++ planningHebdo/planning.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Planning Biblio, Version 2.0.4
+Licence GNU/GPL (version 2 et au dela)
+Voir les fichiers README.md et LICENSE
+Copyright (C) 2013-2015 - Jérôme Combes
+
+Fichier : planningHebdo/planning.php
+Création : 22 mars 2014
+Dernière modification : 7 novembre 2015
+Auteur : Jérôme Combes, jerome@planningbilbio.fr
+
+Description :
+Fichier permettant de trouver le bon emploi du temps et le bon site de chaque agent
+Inclus dans planning/poste/index.php pour afficher ces informations dans la liste des agents présents
+Inclus dans planning/poste/menudiv.php pour exclure les agents non prévus sur le site séléctionné
+
+Variables initialisées dans planning/poste/index.php et dans  planning/poste/menudiv.php :
+$elem['id'] = id de l'agent
+$date = date courante
+*/
+
+include_once "class.planningHebdo.php";
+$p=new planningHebdo();
+$p->debut=$date;
+$p->fin=$date;
+$p->valide=true;
+$p->fetch();
+
+$tempsPlanningHebdo=array();
+
+if(!empty($p->elements)){
+  foreach($p->elements as $elem){
+    $tempsPlanningHebdo[$elem["perso_id"]]=$elem["temps"];
+  }
+}
+?>
