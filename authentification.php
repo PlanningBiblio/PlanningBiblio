952e6a50af7fcf9b68ab3408577904c392e1ca72
Jérôme Combes <jerome@planningbiblio.fr>
2018-01-24 21:57:19 +0100

    Mise à jour du numéro de version (2.7.12)

--- authentification.php
+++ authentification.php
@@ -28,7 +28,7 @@ if(PHP_VERSION_ID<50400 and session_id()==''){
 }
 
 // Initialisation des variables
-$version="2.7.11";
+$version="2.7.12";
 
 // Redirection vers setup si le fichier config est absent
 if(!file_exists("include/config.php")){
