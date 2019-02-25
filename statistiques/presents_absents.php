48917e69a941b45c517d6a374a4d403f398f306a
Alex Arnaud <alex.arnaud@biblibre.com>
2019-02-25 14:52:39 +0000

    MT 24615: Ability to type a date range

--- statistiques/presents_absents.php
+++ statistiques/presents_absents.php
@@ -22,45 +22,57 @@ use PlanningBiblio\PresentSet;
 
 $params = $request->request->all();
 if ($params) {
-    $date = dateSQL($params['date']);
+    $startTime = strtotime(dateSQL($params['from']));
+    $endTime = strtotime(dateSQL($params['to']));
+    $validated = isset($params['validated']) && $params['validated'] == 'on' ? true : false;
 
-    $d = new datePL($date);
-    $absences = new absences();
-    $absences->valide = false;
-    $absent_ids = array(2);
-    if (isset($params['validated']) && $params['validated'] == 'on') {
-        $absences->valide = true;
-    }
-    $absences->fetch("`nom`,`prenom`,`debut`,`fin`", null, $date, $date, array(1));
-    $absents = $absences->elements;
+    $by_date = array();
+    for ( $i = $startTime; $i <= $endTime; $i = $i + 86400 ) {
+        $date = date('Y-m-d', $i);
 
-    foreach ($absents as $key => $absent) {
-        $absents[$key]['motif'] = html_entity_decode($absent['motif'], ENT_QUOTES|ENT_HTML5);
-        preg_match('/00:00:00$/', $absent['debut'], $matche_start, PREG_OFFSET_CAPTURE);
-        preg_match('/23:59:59$/', $absent['fin'], $matche_end, PREG_OFFSET_CAPTURE);
-        if ($matche_start && $matche_end) {
-            $absents[$key]['all_the_day'] = 1;
-        } else {
-            $absents[$key]['from'] = substr($absent['debutAff'], -5);
-            $absents[$key]['to'] = substr($absent['finAff'], -5);
+        $absences = new absences();
+        $absences->valide = false;
+        $absent_ids = array(2);
+        $absences->valide = $validated;
+        $absences->fetch("`nom`,`prenom`,`debut`,`fin`", null, $date, $date, array(1));
+        $absents = $absences->elements;
+
+        foreach ($absents as $key => $absent) {
+            $absents[$key]['motif'] = html_entity_decode($absent['motif'], ENT_QUOTES|ENT_HTML5);
+            preg_match('/00:00:00$/', $absent['debut'], $matche_start, PREG_OFFSET_CAPTURE);
+            preg_match('/23:59:59$/', $absent['fin'], $matche_end, PREG_OFFSET_CAPTURE);
+            if ($matche_start && $matche_end) {
+                $absents[$key]['all_the_day'] = 1;
+            } else {
+                $absents[$key]['from'] = substr($absent['debutAff'], -5);
+                $absents[$key]['to'] = substr($absent['finAff'], -5);
+            }
+
+            if ($absent['debut'] <= $date . " 00:00:00"
+                and $absent['fin'] >= $date . " 23:59:59"
+                and $absent['valide'] > 0) {
+                $absent_ids[] = $absent['perso_id'];
+            }
         }
 
-        if ($absent['debut'] <= $date . " 00:00:00"
-            and $absent['fin'] >= $date . " 23:59:59"
-            and $absent['valide'] > 0) {
-            $absent_ids[] = $absent['perso_id'];
+        $d = new datePL($date);
+        $presentset = new PresentSet($date, $d, $absent_ids, new db());
+        $presents = $presentset->all();
+        foreach ($presents as $key => $present) {
+            $presents[$key]['heures'] = html_entity_decode($present['heures'], ENT_QUOTES|ENT_HTML5);
         }
+
+        $by_date[] = array(
+            'date' => date('d/m/Y', $i),
+            'presents' => $presents,
+            'absents' => $absents
+        );
     }
 
-
-    $presentset = new PresentSet($date, $d, $absent_ids, new db());
-    $presents = $presentset->all();
-    foreach ($presents as $key => $present) {
-        $presents[$key]['heures'] = html_entity_decode($present['heures'], ENT_QUOTES|ENT_HTML5);
-    }
-
-    $templates_params['absents'] = $absents;
-    $templates_params['presents'] = $presents;
+    $templates_params['by_date'] = $by_date;
+    $templates_params['from'] = $params['from'];
+    $templates_params['to'] = $params['to'];
+    $templates_params['validated'] = $validated;
 }
 
 
