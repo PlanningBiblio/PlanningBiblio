caa4140b79ec571248a54452a757a3d077a42746
Matthias Meusburger <matthias.meusburger@biblibre.com>
2019-09-12 14:26:13 +0200

    MT 28388: Retrieve absences from microsoft graph API
    Installation:
    
     - edit your .env.dev.local or .env.dev.local (see APP_ENV in .env), and add
       the following variables:
       MS_GRAPH_TENANT_ID
       MS_GRAPH_CLIENT_ID
       MS_GRAPH_CLIENT_SECRET
    
     - optional variables:
       MS_GRAPH_REASON_NAME=Outlook [default: Outlook]
       MS_GRAPH_LOGIN_SUFFIX=@domain.tld [default: null]
    
     - in the same file, edit the following variables:
       DATABASE_URL
       DATABASE_PREFIX
    
     - apply the database update
    
     - composer update
    
    Usage:
    
    php bin/console PlanningBiblio:ImportMSGraph [--full=1]
    
    The script is silent, see the logs database table for processing informations.
    
    Test plan:
    
     - Modify one or several users' email addresses to match addresses that exist
       in the Active Directory.
    
     - Add one or several events for those users.
    
     - Start the script.
    
     - Check that these events have been added in PLB.
    
     - Modify one or several events, start the script, check that those events have
       been updated in PLB.
    
     - Delete one or several event, start the script, check that those events have
       been deleted in PLB.

---
+++ src/PlanningBiblio/Logger.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\PlanningBiblio;
+
+class Logger
+{
+    private $entityManager;
+    private $dbprefix;
+
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->dbprefix = $_ENV['DATABASE_PREFIX'];
+    }
+
+    public function log($message, $application) {
+        //echo $message . "\n";
+        $query = "INSERT INTO " . $this->dbprefix . "log (msg, program) VALUES (?,?)";
+        $this->entityManager->getConnection()->prepare($query)->execute([$message, $application]);
+    }
+}
