b29442eb74a4fdf00c67317ca894711a48432bd4
Alex Arnaud <alex.arnaud@biblibre.com>
2019-11-04 17:12:21 +0000

    Holidays in days: move conges/voir.php to a symfony controller

--- public/init_ajax.php
+++ public/init_ajax.php
@@ -16,6 +16,6 @@
 
 session_start();
 
-require_once(__DIR__.'/vendor/autoload.php');
+require_once(__DIR__.'/../vendor/autoload.php');
 require_once(__DIR__.'/include/config.php');
 require_once(__DIR__.'/init_entitymanager.php');
---
+++ public/setup/atomicupdate/20191015_update_page_conges_voir.php
@@ -0,0 +1,4 @@
+<?php
+$sql[] = "UPDATE `{$dbprefix}menu` SET `url` = '/holiday/index' WHERE `url` = 'conges/voir.php';";
+$sql[] = "UPDATE `{$dbprefix}menu` SET `url` = '/holiday/index/1' WHERE `url` = 'conges/voir.php&amp;recup=1';";
+$sql[] = "UPDATE `{$dbprefix}acces` SET `page` = '/holiday/index' WHERE `page` = 'conges/voir.php';";
