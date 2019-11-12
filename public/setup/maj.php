cf7bdb3214ba119fb48313ed97b98dc747b8b543
Alex Arnaud <alex.arnaud@biblibre.com>
2019-11-12 15:31:11 +0000

    DBRev: 19.05.00.003

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -1664,6 +1664,46 @@ if (version_compare($config['Version'], $v) === -1) {
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
 
+$v="19.11.00.003";
+if (version_compare($config['Version'], $v) === -1) {
+    $sql[] = "UPDATE `{$dbprefix}acces` SET `page` = '/absence' WHERE `page` = 'absences/ajouter.php';";
+    $sql[] = "UPDATE `{$dbprefix}menu` SET `url` = '/absence' WHERE `url` = 'absences/ajouter.php';";
+    $sql[] = "DELETE FROM `{$dbprefix}acces` WHERE `page` = 'absences/modif.php';";
+
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `valeur` = 'Non justifiée' WHERE `valeur` = 'Non justifi&eacute;e';";
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `valeur` = 'Congés payés' WHERE `valeur` = 'Cong&eacute;s pay&eacute;s';";
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `valeur` = 'Congé maternité' WHERE `valeur` = 'Cong&eacute; maternit&eacute;';";
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `valeur` = 'Réunion syndicale' WHERE `valeur` = 'R&eacute;union syndicale';";
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `valeur` = 'Grève' WHERE `valeur` = 'Gr&egrave;ve';";
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `valeur` = 'Réunion' WHERE `valeur` = 'R&eacute;union';";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Destinataires des notifications de nouvelles absences (Circuit A)' WHERE `nom` = 'Absences-notifications1';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `nom` = 'Absences-notifications-A1' WHERE `nom` = 'Absences-notifications1';";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Destinataires des notifications de modification d&apos;absences (Circuit A)' WHERE `nom` = 'Absences-notifications2';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `nom` = 'Absences-notifications-A2' WHERE `nom` = 'Absences-notifications2';";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Destinataires des notifications des validations niveau 1 (Circuit A)' WHERE `nom` = 'Absences-notifications3';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `nom` = 'Absences-notifications-A3' WHERE `nom` = 'Absences-notifications3';";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Destinataires des notifications des validations niveau 2 (Circuit A)' WHERE `nom` = 'Absences-notifications4';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `nom` = 'Absences-notifications-A4' WHERE `nom` = 'Absences-notifications4';";
+
+
+    $sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `valeurs`, `commentaires`, `categorie`, `ordre`) VALUES ('Absences-notifications-B1','checkboxes','[0,1,2,3]','[[0,\"Agents ayant le droit de g&eacute;rer les absences\"],[1,\"Responsables directs\"],[2,\"Cellule planning\"],[3,\"Agent concern&eacute;\"]]','Destinataires des notifications de nouvelles absences (Circuit B)','Absences','40');";
+    $sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `valeurs`, `commentaires`, `categorie`, `ordre`) VALUES ('Absences-notifications-B2','checkboxes','[0,1,2,3]','[[0,\"Agents ayant le droit de g&eacute;rer les absences\"],[1,\"Responsables directs\"],[2,\"Cellule planning\"],[3,\"Agent concern&eacute;\"]]','Destinataires des notifications de modification d&apos;absences (Circuit B)','Absences','50');";
+    $sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `valeurs`, `commentaires`, `categorie`, `ordre`) VALUES ('Absences-notifications-B3','checkboxes','[1]','[[0,\"Agents ayant le droit de g&eacute;rer les absences\"],[1,\"Responsables directs\"],[2,\"Cellule planning\"],[3,\"Agent concern&eacute;\"]]','Destinataires des notifications des validations niveau 1 (Circuit B)','Absences','60');";
+    $sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `valeurs`, `commentaires`, `categorie`, `ordre`) VALUES ('Absences-notifications-B4','checkboxes','[3]','[[0,\"Agents ayant le droit de g&eacute;rer les absences\"],[1,\"Responsables directs\"],[2,\"Cellule planning\"],[3,\"Agent concern&eacute;\"]]','Destinataires des notifications des validations niveau 2 (Circuit B)','Absences','65');";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Gestion des notifications et des droits de validations agent par agent. Si cette option est activée, les paramètres Absences-notifications-A1, A2, A3 et A4 ou B1, B2, B3 et B4 seront écrasés par les choix fait dans la page de configuration des notifications du menu Administration - Notifications / Validations' WHERE `nom` = 'Absences-notifications-agent-par-agent';",
+
+    $sql[] = "ALTER table `{$dbprefix}select_abs` ADD COLUMN `notification_workflow` CHAR(1) AFTER `type`;";
+    $sql[] = "UPDATE `{$dbprefix}select_abs` SET `notification_workflow` = 'A'; WHERE `type` != 1";
+
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
+
 //	Execution des requetes et affichage
 foreach ($sql as $elem) {
     $db=new db();
