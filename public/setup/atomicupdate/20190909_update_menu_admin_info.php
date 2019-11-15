cb8b4a9b35bfae27a41f41c1fbec35e98f6ae37d
Alex Arnaud <alex.arnaud@biblibre.com>
2019-11-15 16:41:46 +0000

    DBRev 19.11.00.005

--- public/setup/atomicupdate/20190909_update_menu_admin_info.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sql[] = "UPDATE `{$dbprefix}menu` SET `url` = '/admin/info' WHERE `url` = 'infos/index.php';";
-$sql[] = "UPDATE `{$dbprefix}acces` SET `page` = '/admin/info' WHERE `page` = 'infos/index.php';";
-$sql[] = "UPDATE `{$dbprefix}acces` SET `page` = '/admin/info/add' WHERE `page` = 'infos/ajout.php';";
-
-$sql[] = "DELETE FROM `{$dbprefix}acces` WHERE `page` = 'infos/modif.php';";
-$sql[] = "DELETE FROM `{$dbprefix}acces` WHERE `page` = 'infos/supprime.php';";
-$sql[] = "DELETE FROM `{$dbprefix}acces` WHERE `page` = 'infos/ajout.php';";
