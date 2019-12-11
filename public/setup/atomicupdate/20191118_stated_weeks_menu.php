2a9c49140d246b234588de87ab14a445df4e519a
Alex Arnaud <alex.arnaud@biblibre.com>
2019-12-11 15:52:34 +0000

    MT 27495: Ability to disable stated weeks feature

--- public/setup/atomicupdate/20191118_stated_weeks_menu.php
+++ public/setup/atomicupdate/20191118_stated_weeks_menu.php
@@ -1,3 +1,3 @@
 <?php
-$sql[] = "INSERT INTO `{$dbprefix}menu` (niveau1, niveau2, titre, url) VALUES(30, 95, 'Semaines fixes', '/statedweek')";
-$sql[] = "INSERT INTO `{$dbprefix}acces` (nom, groupe_id, page, ordre) VALUES('Semaine fixes', 100, '/statedweek', 0)";
+$sql[] = "INSERT INTO `{$dbprefix}menu` (`niveau1`, `niveau2`, `titre`, `url`, `condition`) VALUES(30, 95, 'Semaines fixes', '/statedweek', 'config=statedweek_enabled')";
+$sql[] = "INSERT INTO `{$dbprefix}acces` (`nom`, `groupe_id`, `page`, `ordre`) VALUES('Semaine fixes', 100, '/statedweek', 0)";
