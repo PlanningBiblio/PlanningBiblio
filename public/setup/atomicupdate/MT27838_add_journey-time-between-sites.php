06d78c3353ae537d7674c92f8fccebf9f5e4183b
Alex Arnaud <alex.arnaud@biblibre.com>
2020-04-17 08:34:46 +0000

    MT 27838 add option journey-time-between-sites

---
+++ public/setup/atomicupdate/MT27838_add_journey-time-between-sites.php
@@ -0,0 +1,2 @@
+<?php
+$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-sites', 'text', '20', 'Time it takes an agent to travel between sites (minutes)', 'Planning', 95);";
