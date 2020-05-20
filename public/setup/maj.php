b310f115565bab77793516ba6ad03fc9baa5ebcd
Alex Arnaud <alex.arnaud@biblibre.com>
2020-05-20 14:04:26 +0000

    DBRev: 19.11.00.013

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -1841,6 +1841,22 @@ if (version_compare($config['Version'], $v) === -1) {
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
 
+$v="19.11.00.013";
+if (version_compare($config['Version'], $v) === -1) {
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '70' WHERE `nom` = 'Absences-notifications-A4';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '80' WHERE `nom` = 'Absences-notifications-B1';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '90' WHERE `nom` = 'Absences-notifications-B2';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '100' WHERE `nom` = 'Absences-notifications-B3';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '110' WHERE `nom` = 'Absences-notifications-B4';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '120' WHERE `nom` = 'Absences-notifications-agent-par-agent';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '130' WHERE `nom` = 'Absences-notifications-titre';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '140' WHERE `nom` = 'Absences-notifications-message';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `ordre` = '150' WHERE `nom` = 'Absences-DelaiSuppressionDocuments';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur` = '365' WHERE `nom` = 'Absences-DelaiSuppressionDocuments';";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
+
 //	Execution des requetes et affichage
 foreach ($sql as $elem) {
     $db=new db();
