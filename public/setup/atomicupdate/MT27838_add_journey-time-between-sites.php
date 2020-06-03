ab240d845a925095f7e9233c4a8ffe4ef85bd390
Alex Arnaud <alex.arnaud@biblibre.com>
2020-06-03 07:46:01 +0000

    MT 27838 - Set Journey-time-between-sites default value to 0

--- public/setup/atomicupdate/MT27838_add_journey-time-between-sites.php
+++ public/setup/atomicupdate/MT27838_add_journey-time-between-sites.php
@@ -1,2 +1,2 @@
 <?php
-$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-sites', 'text', '20', 'Time it takes an agent to travel between sites (minutes)', 'Planning', 95);";
+$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-sites', 'text', '0', 'Time it takes an agent to travel between sites (minutes)', 'Planning', 95);";
