7744ba48f294b10549afddd590f99ed57811f64f
Alex Arnaud <alex.arnaud@biblibre.com>
2020-07-29 13:26:20 +0000

    MT 27838: rename Journey-time-between-floors to Journey-time-between-areas Translate new options in french Set Journey-time-between-areas option order to 96

--- public/setup/atomicupdate/MT27838_add_journey-time-between-sites.php
+++ public/setup/atomicupdate/MT27838_add_journey-time-between-sites.php
@@ -1,3 +1,3 @@
 <?php
-$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-sites', 'text', '0', 'Time it takes an agent to travel between sites (minutes)', 'Planning', 95);";
-$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-floors', 'text', '0', 'Time it takes an agent to travel between floors (minutes)', 'Planning', 96);";
+$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-sites', 'text', '0', 'Temps de trajet moyen entre sites (en minutes)', 'Planning', 95);";
+$sql[]="INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `commentaires`, `categorie`, `ordre`) VALUES ('Journey-time-between-areas', 'text', '0', 'Temps de trajet moyen entre zones (en minutes)', 'Planning', 96);";
