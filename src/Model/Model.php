b33b07b51ef696ac6745c0f6930287dfbed08fab
Alex Arnaud <alex.arnaud@biblibre.com>
2020-07-30 12:20:59 +0000

    Symfonize admin template page: handle multiple lines

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -3,7 +3,7 @@
 namespace App\Model;
 
 /**
- * @Entity @Table(name="pl_poste_modeles")
+ * @Entity @Table(name="pl_poste_modeles_tab")
  **/
 class Model extends PLBEntity
 {
@@ -14,25 +14,10 @@ class Model extends PLBEntity
     protected $nom;
 
     /** @Column(type="integer") **/
-    protected $perso_id;
+    protected $jour;
 
     /** @Column(type="integer") **/
-    protected $poste;
-
-    /** @Column(type="string") **/
-    protected $commentaire;
-
-    /** @Column(type="time") **/
-    protected $debut;
-
-    /** @Column(type="time") **/
-    protected $fin;
-
-    /** @Column(type="text") **/
     protected $tableau;
-
-    /** @Column(type="text") **/
-    protected $jour;
 
     /** @Column(type="integer") **/
     protected $site;
---
+++ src/Model/ModelAgent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * @Entity @Table(name="pl_poste_modeles")
+ **/
+class ModelAgent extends PLBEntity
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+
+    /** @Column(type="string") **/
+    protected $nom;
+
+    /** @Column(type="integer") **/
+    protected $perso_id;
+
+    /** @Column(type="integer") **/
+    protected $poste;
+
+    /** @Column(type="string") **/
+    protected $commentaire;
+
+    /** @Column(type="time") **/
+    protected $debut;
+
+    /** @Column(type="time") **/
+    protected $fin;
+
+    /** @Column(type="text") **/
+    protected $tableau;
+
+    /** @Column(type="text") **/
+    protected $jour;
+
+    /** @Column(type="integer") **/
+    protected $site;
+
+}
