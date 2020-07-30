0755ea50739e15075aaa0487a6f2411e3996f999
Matthias Meusburger <matthias.meusburger@biblibre.com>
2020-07-30 11:11:59 +0200

    MT27842: Having "Full day" checked by default in absences is configurable, Follow-up
     - Absences-journeeEntiere is enabled by default

--- public/setup/atomicupdate/20200420_MT27842_Full_Day_switch.php
+++ public/setup/atomicupdate/20200420_MT27842_Full_Day_switch.php
@@ -1,4 +1,4 @@
 <?php
-$sql[] = "INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `categorie`, `ordre`, `commentaires`, `valeurs`) VALUES ('Absences-journeeEntiere', 'boolean', '0', 'Absences','38', 'Le paramètre \"Journée(s) entière(s)\" est coché par défaut lors de la saisie d\'une absence.', '');";
+$sql[] = "INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `categorie`, `ordre`, `commentaires`, `valeurs`) VALUES ('Absences-journeeEntiere', 'boolean', '1', 'Absences','38', 'Le paramètre \"Journée(s) entière(s)\" est coché par défaut lors de la saisie d\'une absence.', '');";
 
 
