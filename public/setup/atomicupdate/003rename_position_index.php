3c8ac1ee2b05354bf0748eefe32919337a199c6b
Sarah Deborde <sarah.deborde@biblibre.com>
2020-09-10 16:17:33 +0200

    Followup fixes further to last comment

--- public/setup/atomicupdate/003rename_position_index.php
+++ public/setup/atomicupdate/003rename_position_index.php
@@ -2,6 +2,11 @@
 
 $sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/position'  WHERE  `page` = 'postes/index.php' ;";
 
+$sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/position/add'  WHERE  `page` = 'postes/modif.php' ;";
+
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE  `page` = 'postes/valid.php' ;";
+
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/position'  WHERE  `url` = 'postes/index.php' ;";
+
 
 ?>
--- public/setup/atomicupdate/004_add_job_edit.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$sql[]="UPDATE `{$dbprefix}acces` SET `page`=`/position/add` WHERE `page`=`postes/modif.php`;" ;
-
-?>
