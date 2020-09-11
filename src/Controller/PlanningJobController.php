4536016e6919ff648783d9ecddc66e54e2f60378
Jérôme Combes <jerome@planningbiblio.fr>
2020-09-12 00:28:48 +0200

    MT 27840: Optimization: move loop to check availabilities into the ajax file

--- src/Controller/PlanningJobController.php
+++ src/Controller/PlanningJobController.php
@@ -23,26 +23,56 @@ class PlanningJobController extends BaseController
         $date = $request->get('date');
         $start = $request->get('from');
         $end = $request->get('to');
-        $agent_id = $request->get('agent');
+        $agents = json_decode($request->get('agents'));
 
-        $result = array('error' => false);
-        $agent = $this->entityManager->find(Agent::class, $agent_id);
-        $fullname = $agent->prenom() . ' ' . $agent->nom();
-        if ($agent->isAbsentOn("$date $start", "$date $end")) {
-            $result['error'] = $fullname;
+        $availables = array();
+        $unavailables = array();
+        $errors = array();
+
+        foreach ($agents as $agent_id) {
+
+            try {
+                $agent = $this->entityManager->find(Agent::class, $agent_id);
+                $fullname = $agent->prenom() . ' ' . $agent->nom();
+                $available = true;
+
+                if ($agent->isAbsentOn("$date $start", "$date $end")) {
+                    $available = false;
+                }
+
+                if ($available and $agent->isOnVacationOn("$date $start", "$date $end")) {
+                    $result['unavailable'][] = $fullname;
+                    $available = false;
+                }
+
+                if ($available) {
+                    $d = new \datePl($date);
+                    $day = $d->planning_day_index_for($agent_id);
+                    $working_hours = $agent->getWorkingHoursOn($date);
+
+                    if (!calculSiPresent($start, $end, $working_hours['temps'], $day)) {
+                        $result['unavailable'][] = $fullname;
+                        $available = false;
+                    }
+                }
+
+                if ($available) {
+                    $availables[] = $agent_id;
+                } else {
+                    $unavailables[] = $fullname;
+                }
+            } catch(Exception $e) {
+                $errors[] = $e;
+            }
         }
 
-        if ($agent->isOnVacationOn("$date $start", "$date $end")) {
-            $result['error'] = $fullname;
-        }
+        $unavailables_string = !empty($unavailables) ? "\n - " . implode("\n - ", $unavailables) : null;
 
-        $d = new \datePl($date);
-        $day = $d->planning_day_index_for($agent_id);
-        $working_hours = $agent->getWorkingHoursOn($date);
-
-        if (!calculSiPresent($start, $end, $working_hours['temps'], $day)) {
-            $result['error'] = $fullname;
-        }
+        $result = array(
+            'availables' => $availables,
+            'unavailables' => $unavailables_string,
+            'errors' => $errors,
+        );
 
         return $this->json($result);
     }
