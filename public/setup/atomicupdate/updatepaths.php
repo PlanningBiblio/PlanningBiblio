ff1d711c5e4ad4231b317abc7a153ea044661440
Sarah Deborde <sarah.deborde@biblibre.com>
2020-09-29 10:44:29 +0200

    correct typo in 2nd path

--- public/setup/atomicupdate/updatepaths.php
+++ public/setup/atomicupdate/updatepaths.php
@@ -2,6 +2,5 @@
 
 $sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/closingday' WHERE `page` ='joursFeries/index.php';";
 
-
-$sql[]="UPDATE `{$dbprefix}menu` SET `page` = '/closingday' WHERE `url` ='joursFeries/index.php';";
+$sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/closingday' WHERE `url` ='joursFeries/index.php';";
 ?>
