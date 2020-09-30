aaa003d08b4fb60af6b8f7bd905654ef81fe179a
Sarah Deborde <sarah.deborde@biblibre.com>
2020-09-30 11:58:56 +0200

    correct typo line 3

--- public/setup/atomicupdate/update_path.php
+++ public/setup/atomicupdate/update_path.php
@@ -1,6 +1,6 @@
 <?php
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `page`='/statistics/position' WHERE `page`='statistiques/postes.php ;";
+$sql[]="UPDATE `{$dbprefix}acces` SET `page`='/statistics/position' WHERE `page`='statistiques/postes.php' ;";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`='/statistics/position' WHERE `url`='statistiques/postes.php' ;";
 
