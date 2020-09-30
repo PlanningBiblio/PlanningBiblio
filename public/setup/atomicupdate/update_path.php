3f5de2e419bcd5e2d4cc5aae97c298e3294ca686
Sarah Deborde <sarah.deborde@biblibre.com>
2020-09-30 10:54:24 +0200

    separate time templates and controller from index

--- public/setup/atomicupdate/update_path.php
+++ public/setup/atomicupdate/update_path.php
@@ -1,8 +1,6 @@
-<?php 
+<?php
 
 $sql[]="UPDATE `{$dbprefix}acces` SET `page`='/statistics' WHERE `page` = 'statistiques/index.php';";
-
-$sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/statistics.time' WHERE `page`='statistiques/temps.php';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/statistics' WHERE `url`='statistiques/index.php';";
 ?>
