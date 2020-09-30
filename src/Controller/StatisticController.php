86c583be4a763c80d727beeb450d5dc89c6f5520
Sarah Deborde <sarah.deborde@biblibre.com>
2020-09-30 11:49:00 +0200

    set route and method for stats per position

--- src/Controller/StatisticController.php
+++ src/Controller/StatisticController.php
@@ -15,10 +15,250 @@ $version = 'symfony';
 include_once __DIR__ . "/../../public/conges/class.conges.php";
 include_once __DIR__ . "/../../public/include/function.php";
 require_once __DIR__ . "/../../public/include/db.php";
+require_once __DIR__ . "/../../public/include/horaires.php";
+require_once __DIR__ . "/../../public/statistiques/class.statistiques.php";
 include_once __DIR__ . '/../../public/absences/class.absences.php';
+include_once __DIR__ . '/../../public/postes/class.postes.php';
+include_once __DIR__ . '/../../public/personnel/class.personnel.php';
 
 class StatisticController extends BaseController
 {
+
+    /**
+     * @Route("/statistics/position", name="statistics.position", methods={"GET", "POST"})
+     */
+    public function position(Request $request, Session $session)
+    {
+        // Initialisation des variables :
+        $debut = $request->get("debut");
+        $fin = $request->get("fin");
+        $tri = $request->get("tri");
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_NUMBER_INT);
+
+        $debut = filter_var($debut, FILTER_CALLBACK, array("options"=>"sanitize_dateFr"));
+        $fin = filter_var($fin, FILTER_CALLBACK, array("options"=>"sanitize_dateFr"));
+
+        $post_postes = isset($post['postes']) ? $post['postes'] : null;
+        $post_sites = isset($post['selectedSites']) ? $post['selectedSites'] : null;
+
+        $joursParSemaine = $this->config('Dimanche') ? 7 : 6;
+
+        if (!array_key_exists('stat_poste_postes', $_SESSION)) {
+            $_SESSION['stat_poste_postes'] = null;
+            $_SESSION['stat_poste_tri'] = null;
+        }
+
+        if (!$debut and array_key_exists("stat_debut", $_SESSION)) {
+            $debut = $_SESSION['stat_debut'];
+        }
+
+        if (!$fin and array_key_exists("stat_fin", $_SESSION)) {
+            $fin = $_SESSION['stat_fin'];
+        }
+
+        if (!$tri and array_key_exists("stat_poste_tri", $_SESSION)) {
+            $tri = $_SESSION['stat_poste_tri'];
+        }
+
+        if (!$debut) {
+            $debut = "01/01/".date("Y");
+        }
+
+        if (!$fin) {
+            $fin = date("d/m/Y");
+        }
+
+        if (!$tri) {
+            $tri = "cmp_01";
+        }
+
+        $_SESSION['stat_debut'] = $debut;
+        $_SESSION['stat_fin'] = $fin;
+        $_SESSION['stat_poste_tri'] = $tri;
+
+        $debutSQL = dateFr($debut);
+        $finSQL = dateFr($fin);
+
+        // Postes
+        $postes = array();
+
+        if ($post_postes) {
+            foreach ($post_postes as $elem) {
+                $postes[] = $elem;
+            }
+        } else {
+            $postes = $_SESSION['stat_poste_postes'];
+        }
+        $_SESSION['stat_poste_postes'] = $postes;
+
+        // Filtre les sites
+        if (!array_key_exists('stat_poste_sites', $_SESSION)) {
+            $_SESSION['stat_poste_sites'] = array();
+        }
+
+        if ($post_sites) {
+            $selectedSites = array();
+            foreach ($post_sites as $elem) {
+                $selectedSites[] = $elem;
+            }
+        } else {
+            $selectedSites = $_SESSION['stat_poste_sites'];
+        }
+
+        $nbSites = $this->config('Multisites-nombre');
+
+        if ($nbSites > 1 and empty($selectedSites)) {
+            for ($i = 1; $i <= $nbSites; $i++) {
+                $selectedSites[] = $i;
+            }
+        }
+        $_SESSION['stat_poste_sites'] = $selectedSites;
+
+        // Filtre les sites dans les requêtes SQL
+        if ($nbSites > 1 and is_array($selectedSites)) {
+            $sitesSQL = "0,".join(",", $selectedSites);
+        } else {
+            $sitesSQL = "0,1";
+        }
+
+        $tab = array();
+
+        // Récupération des infos sur les agents
+        $p = new \personnel();
+        $p->fetch();
+        $agents_infos = $p->elements;
+
+        //-------------- Récupération de la liste des postes pour le menu déroulant ------------------------
+        $db = new \db();
+        $db->select2("postes", "*", array("statistiques"=>"1"), "ORDER BY `etage`,`nom`");
+        $postes_list = $db->result;
+
+        if (!empty($postes)) {
+            //    Recherche du nombre de jours concernés
+            $db = new \db();
+            $db->select2("pl_poste", "date", array("date"=>"BETWEEN{$debutSQL}AND{$finSQL}", "site"=>"IN{$sitesSQL}"), "GROUP BY `date`;");
+            $nbJours = $db->nb;
+
+            // Recherche des absences dans la table absences
+            $a = new \absences();
+            $a->valide = true;
+            $a->fetchForStatistics("$debutSQL 00:00:00", "$finSQL 23:59:59");
+            $absencesDB = $a->elements;
+
+            //    Recherche des infos dans pl_poste et personnel pour tous les postes sélectionnés
+            //    On stock le tout dans le tableau $resultat
+            $postes_select = join(",", $postes);
+            $db = new \db();
+
+            $db->selectInnerJoin(
+                array("pl_poste","perso_id"),
+                array("personnel","id"),
+                array("debut","fin","date","poste","site"),
+                array("nom","prenom",array("name"=>"id", "as"=>"perso_id")),
+                array("date"=>"BETWEEN{$debutSQL}AND{$finSQL}", "poste"=>"IN{$postes_select}","absent"=>"<>1","supprime"=>"<>1", "site"=>"IN{$sitesSQL}"),
+                array(),
+                "ORDER BY `poste`,`nom`,`prenom`"
+            );
+
+            $resultat = $db->result;
+
+
+            //    Recherche des infos dans le tableau $resultat (issu de pl_poste et personnel)
+            //    pour chaques postes sélectionnés
+            foreach ($postes as $poste) {
+                if (array_key_exists($poste, $tab)) {
+                    $heures = $tab[$poste][2];
+                    $sites = $tab[$poste]["sites"];
+                } else {
+                    $heures = 0;
+                    for ($i = 1; $i<= $nbSites; $i++) {
+                        $sites[$i] = 0;
+                    }
+                }
+                $agents = array();
+                $services = array();
+                $statuts = array();
+                if (is_array($resultat)) {
+                    foreach ($resultat as $elem) {
+                        if ($poste == $elem['poste']) {
+                            // Vérifie à partir de la table absences si l'agent est absent
+                            // S'il est absent : continue
+                            if ( !empty($absencesDB[$elem['perso_id']]) ) {
+                                foreach ($absencesDB[$elem['perso_id']] as $a) {
+                                    if ($a['debut'] < $elem['date'].' '.$elem['fin'] and $a['fin']> $elem['date']." ".$elem['debut']) {
+                                        continue 2;
+                                    }
+                                }
+                            }
+
+                            // on créé un tableau par agent avec son nom, prénom et la somme des heures faites par poste
+                            if (!array_key_exists($elem['perso_id'], $agents)) {
+                                $agents[$elem['perso_id']] = array($elem['perso_id'],$elem['nom'],$elem['prenom'],0,"site" => $elem['site']);
+                            }
+                            $agents[$elem['perso_id']][3] += diff_heures($elem['debut'], $elem['fin'], "decimal");
+
+                            // On compte les heures de chaque site
+                            if ($nbSites > 1) {
+                                $sites[$elem['site']] += diff_heures($elem['debut'], $elem['fin'], "decimal");
+                            }
+
+                            // On compte toutes les heures (globales)
+                            $heures += diff_heures($elem['debut'], $elem['fin'], "decimal");
+
+                            foreach ($postes_list as $elem2) {
+                                if ($elem2['id'] == $poste) {    // on créé un tableau avec le nom et l'étage du poste.
+                                    $poste_tab = array($poste,$elem2['nom'],$elem2['etage'],$elem2['obligatoire']);
+                                    break;
+                                }
+                            }
+
+                            // On créé un tableau par service
+                            if (array_key_exists($elem['perso_id'], $agents_infos)) {
+                                $service = $agents_infos[$elem['perso_id']]['service'];
+                            }
+                            $service = isset($service) ? $service : "ZZZ_Autre";
+                            if (!array_key_exists($service, $services)) {
+                                $services[$service] = array("nom"=>$service,"heures"=>0);
+                            }
+                            $services[$service]["heures"] += diff_heures($elem['debut'], $elem['fin'], "decimal");
+
+                            // On créé un tableau par statut
+                            if (array_key_exists($elem['perso_id'], $agents_infos)) {
+                                $statut = $agents_infos[$elem['perso_id']]['statut'];
+                            }
+                            $statut = isset($statut) ? $statut : "ZZZ_Autre";
+                             if (!array_key_exists($statut, $statuts)) {
+                                $statuts[$statut] = array("nom" => $statut, "heures" => 0);
+                            }
+                            $statuts[$statut]["heures"] += diff_heures($elem['debut'], $elem['fin'], "decimal");
+
+                            // On met dans tab tous les éléments (infos postes + agents + heures du poste)
+                            $tab[$poste] = array($poste_tab, $agents, $heures, "services" => $services, "statuts" => $statuts, "sites" => $sites);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Heures et jours d'ouverture au public
+        $s = new \statistiques();
+        $s->debut = $debutSQL;
+        $s->fin = $finSQL;
+        $s->joursParSemaine = $joursParSemaine;
+        $s->selectedSites = $selectedSites;
+        $s->ouverture();
+        $ouverture = $s->ouvertureTexte;
+
+        //        -------------        Tri du tableau        ------------------------------
+        usort($tab, $tri);
+
+        // passage en session du tableau pour le fichier export.php
+        $_SESSION['stat_tab'] = $tab;
+
+
+        return $this->output('statistics/position.html.twig');
+    }
+
     /**
      * @Route("/statistics/attendeesmissing", name="statistics.attendeesmissing", methods={"GET", "POST"})
      */
