d34bb9b45730c76857db05c136139237a013574d
Sarah Deborde <sarah.deborde@biblibre.com>
2020-10-01 09:29:06 +0200

    edit request in file

---
+++ public/setup/atomicupdate/update_path.php
@@ -0,0 +1,7 @@
+ <?php
+
+$sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/statistics/time' WHERE `page`='statistiques/temps.php';";
+
+$sql[]="UPDATE `{$dbprefix}acces` SET `url` = '/statistics/time' WHERE `url`='statistiques/temps.php';";
+
+?>
