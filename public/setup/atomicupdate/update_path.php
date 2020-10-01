43782806dee33cb8f819f3afab30c3ceec9316bf
Sarah Deborde <sarah.deborde@biblibre.com>
2020-10-01 10:14:37 +0200

    edit table name in request

--- public/setup/atomicupdate/update_path.php
+++ public/setup/atomicupdate/update_path.php
@@ -2,6 +2,6 @@
 
 $sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/statistics/time' WHERE `page`='statistiques/temps.php';";
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `url` = '/statistics/time' WHERE `url`='statistiques/temps.php';";
+$sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/statistics/time' WHERE `url`='statistiques/temps.php';";
 
 ?>
