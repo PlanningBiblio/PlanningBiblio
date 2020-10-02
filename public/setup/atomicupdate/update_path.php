860555b2fd4d0b34f1ef7f77eb541f37edf64b3d
Sarah Deborde <sarah.deborde@biblibre.com>
2020-10-02 17:45:57 +0200

    delete insert request

--- public/setup/atomicupdate/update_path.php
+++ public/setup/atomicupdate/update_path.php
@@ -2,8 +2,6 @@
 
 $sql[]="UPDATE `{$dbprefix}acces` SET `page`='/statistics/position' WHERE `page`='statistiques/postes.php' ;";
 
-$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`groupe`,`page`,`categorie`,`ordre`) VALUES ('stats postes par agent', 17, 'Accès aux statistiques', 'statistiques/postes.php','Statistiques','170');";
-
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`='/statistics/position' WHERE `url`='statistiques/postes.php' ;";
 
 ?>
