1000ecd340d84cf908d3067e37f57a886108d722
Alex Arnaud <alex.arnaud@biblibre.com>
2020-10-05 14:24:10 +0000

    DBRev: 20.05.00.008

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -1960,6 +1960,86 @@ if (version_compare($config['Version'], $v) === -1) {
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
 
+$v="20.05.00.008";
+if (version_compare($config['Version'], $v) === -1) {
+    $sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/position'  WHERE  `page` = 'postes/index.php' ;";
+    $sql[]="DELETE FROM `{$dbprefix}acces` WHERE  `page` = 'postes/valid.php' ;";
+    $sql[]="DELETE FROM `{$dbprefix}acces` WHERE  `page` = 'postes/modif.php' ;";
+    $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/position'  WHERE  `url` = 'postes/index.php' ;";
+
+    $db = new db();
+    $db->select2('select_etages', array('id', 'valeur'), "`valeur` LIKE '%&%'");
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['valeur'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}select_etages` SET `valeur` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+
+    $db = new db();
+    $db->select2('select_groupes', array('id', 'valeur'), "`valeur` LIKE '%&%'");
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['valeur'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}select_groupes` SET `valeur` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+
+    $db = new db();
+    $db->select2('postes', array('id', 'nom'), "`nom` LIKE '%&%'");
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['nom'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}postes` SET `nom` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+
+    $db = new db();
+    $db->select2('postes', array('id', 'etage'), "`etage` LIKE '%&%'");
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['etage'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}postes` SET `etage` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+
+    $db = new db();
+    $db->select2('postes', array('id', 'groupe'), "`groupe` LIKE '%&%'");
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['groupe'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}postes` SET `groupe` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
+
 //	Execution des requetes et affichage
 foreach ($sql as $elem) {
     $db=new db();
