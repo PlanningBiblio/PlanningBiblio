f85c5036e95fc86524aec2b7eb43aebd9c6fde19
Alex Arnaud <alex.arnaud@biblibre.com>
2020-10-09 09:47:15 +0000

    Release 20.10.00

--- init/init.php
+++ init/init.php
@@ -11,8 +11,8 @@ $_SESSION['oups']['Auth-Mode'] = isset($_SESSION['oups']['Auth-Mode']) ? $_SESSI
 $_SESSION['oups']['week'] = isset($_SESSION['oups']['week']) ? $_SESSION['oups']['week'] : '';
 
 // Version
-$version="20.05.00.008"; // xx.xx.xx.xxx
-$displayed_version="20.05.00"; // xx.xx.xx
+$version="20.10.00.000"; // xx.xx.xx.xxx
+$displayed_version="20.10.00"; // xx.xx.xx
 
 require_once __DIR__.'/../vendor/autoload.php';
 
