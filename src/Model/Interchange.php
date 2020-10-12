0518f898a5396191eb952ef92299ccf23ae20e1d
Jérôme Combes <jerome@planningbiblio.fr>
2020-10-12 10:45:42 +0200

    Rebase BSG branch on master - 20.10.00

---
+++ src/Model/Interchange.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * @Entity @Table(name="interchanges")
+ **/
+class Interchange extends PLBEntity
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+
+    /** @Column(type="integer") **/
+    protected $planning;
+
+    /** @Column(type="integer") **/
+    protected $requester;
+
+    /** @Column(type="datetime") **/
+    protected $requested_on;
+
+    /** @Column(type="integer") **/
+    protected $requester_time;
+
+    /** @Column(type="integer") **/
+    protected $asked;
+
+    /** @Column(type="integer") **/
+    protected $asked_time;
+
+    /** @Column(type="integer") **/
+    protected $accepted_by;
+
+    /** @Column(type="datetime") **/
+    protected $accepted_on;
+
+    /** @Column(type="integer") **/
+    protected $rejected_by;
+
+    /** @Column(type="datetime") **/
+    protected $rejected_on;
+
+    /** @Column(type="integer") **/
+    protected $validated_by;
+
+    /** @Column(type="datetime") **/
+    protected $validated_on;
+
+    /** @Column(type="string") **/
+    protected $status;
+}
