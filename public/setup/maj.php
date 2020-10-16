289e153853cb5734a279c31eeb5691bbb2a84284
Alex Arnaud <alex.arnaud@biblibre.com>
2020-10-16 09:31:56 +0000

    DBRev: 20.11.00.001

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -2045,6 +2045,41 @@ if (version_compare($config['Version'], $v) === -1) {
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
 
+$v="20.11.00.001";
+if (version_compare($config['Version'], $v) === -1) {
+
+    $db = new db();
+    $db->select2('jours_feries', array('id', 'nom'), "`nom` LIKE '%&%'");
+
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['nom'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}jours_feries` SET `nom` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+
+    $db = new db();
+    $db->select2('jours_feries', array('id', 'commentaire'), "`commentaire` LIKE '%&%'");
+
+    if($db->result){
+        foreach ($db->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['commentaire'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}jours_feries` SET `commentaire` = '$new' WHERE `id` = '$id';";
+            }
+        }
+    }
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
+
 //	Execution des requetes et affichage
 foreach ($sql as $elem) {
     $db=new db();
