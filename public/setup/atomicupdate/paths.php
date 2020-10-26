4ac4ab0faa3303953deec5b41132fd2e5f8bf9a9
Sarah Deborde <sarah.deborde@biblibre.com>
2020-10-26 18:13:19 +0100

    WIP : edit template + edit and add methods in controller

--- public/setup/atomicupdate/paths.php
+++ public/setup/atomicupdate/paths.php
@@ -2,3 +2,4 @@
 
 $sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`groupe`,`page`,`categorie`,`ordre`) VALUES ('Planning Hebdo - Index','1101','Gestion des heures de présences, validation niveau 1','/attendance','Heures de présence','80');";
 $sql[]="INSERT INTO `{$dbprefix}menu` (`niveau1`,`niveau2`,`titre`,`url`,`condition`) VALUES   ('50','75','Heures de présence','/attendance','config=PlanningHebdo');";
+$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`page`) VALUES ('Planning Hebdo - Modif','100','/attendance/edit');";
