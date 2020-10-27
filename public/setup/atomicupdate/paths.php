cf3437f34e86afe6b160a0b022ce7ca46aa21154
Sarah Deborde <sarah.deborde@biblibre.com>
2020-10-27 14:46:56 +0100

    let modifications for index only + add script in index to delete attendance hours

--- public/setup/atomicupdate/paths.php
+++ public/setup/atomicupdate/paths.php
@@ -1,5 +1,4 @@
 <?php
 
-$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`groupe`,`page`,`categorie`,`ordre`) VALUES ('Planning Hebdo - Index','1101','Gestion des heures de présences, validation niveau 1','/attendance','Heures de présence','80');";
-$sql[]="INSERT INTO `{$dbprefix}menu` (`niveau1`,`niveau2`,`titre`,`url`,`condition`) VALUES   ('50','75','Heures de présence','/attendance','config=PlanningHebdo');";
-$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`page`) VALUES ('Planning Hebdo - Modif','100','/attendance/edit');";
+$sql[]="UPDATE `{$dbprefix}acces` set `page`='/attendance' WHERE `page`='planningHebdo/index.php';";
+$sql[]="UPDATE `{$dbprefix}menu` set `url`='/attendance' WHERE `url`='planningHebdo/index.php';";
