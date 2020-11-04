f01b5f2d3e5afaf63bdfdb3526f905b74034ddf1
Sarah Deborde <sarah.deborde@biblibre.com>
2020-11-04 16:48:49 +0100

    edit sql requests in db and atomicupdate

--- public/setup/atomicupdate/paths.php
+++ public/setup/atomicupdate/paths.php
@@ -1,5 +1,9 @@
 <?php
 
 $sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`groupe`,`page`,`categorie`,`ordre`) VALUES ('Planning Hebdo - Index','1101','Gestion des heures de présences, validation niveau 1','/attendance','Heures de présence','80');";
+
 $sql[]="INSERT INTO `{$dbprefix}menu` (`niveau1`,`niveau2`,`titre`,`url`,`condition`) VALUES   ('50','75','Heures de présence','/attendance','config=PlanningHebdo');";
-$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`page`) VALUES ('Planning Hebdo - Modif','100','/attendance/edit');";
+
+$sql[]="UPDATE `{$dbprefix}acces` SET `nom` = 'Planning Hebdo - Ajout' `page`='/attendance/add' WHERE `page`='planningHebdo/modif.php' ;";
+
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='planningHebdo/valid.php' ;";
