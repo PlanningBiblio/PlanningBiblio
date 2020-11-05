e1714afefb207355de9d3cbd74b8bef7a943e9e8
Sarah Deborde <sarah.deborde@biblibre.com>
2020-11-05 10:13:38 +0100

    rename attendance -> workinghour

--- public/setup/atomicupdate/paths.php
+++ public/setup/atomicupdate/paths.php
@@ -1,9 +1,8 @@
 <?php
 
-$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`groupe`,`page`,`categorie`,`ordre`) VALUES ('Planning Hebdo - Index','1101','Gestion des heures de présences, validation niveau 1','/attendance','Heures de présence','80');";
+//todo : supprimer ligne 4 apres validation de la PR254
+$sql[]="INSERT INTO `{$dbprefix}acces` (`nom`,`groupe_id`,`groupe`,`page`,`categorie`,`ordre`) VALUES ('Planning Hebdo - Index','1101','Gestion des heures de présences, validation niveau 1','/workinghour','Heures de présence','80');";
 
-$sql[]="INSERT INTO `{$dbprefix}menu` (`niveau1`,`niveau2`,`titre`,`url`,`condition`) VALUES   ('50','75','Heures de présence','/attendance','config=PlanningHebdo');";
-
-$sql[]="UPDATE `{$dbprefix}acces` SET `nom` = 'Planning Hebdo - Ajout' `page`='/attendance/add' WHERE `page`='planningHebdo/modif.php' ;";
+$sql[]="UPDATE `{$dbprefix}acces` SET `nom` = 'Planning Hebdo - Ajout' `page`='/workinghour/add' WHERE `page`='planningHebdo/modif.php' ;";
 
 $sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='planningHebdo/valid.php' ;";
