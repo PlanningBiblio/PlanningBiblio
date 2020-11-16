12fa015c711af6824cd7c9c725c3a58492f24e87
Sarah Deborde <sarah.deborde@biblibre.com>
2020-11-16 12:01:43 +0100

    adapt the file to any local connection

--- migrations-db.php
+++ migrations-db.php
@@ -1,10 +1,23 @@
 <?php
-use Doctrine\DBAL\DriverManager;
 
-return DriverManager::getConnection([
-    'dbname' => 'planningbiblio',
-    'user' => 'planningbadmin',
-    'password' => 'MxZ5sEsAE',
-    'host' => 'localhost:3306',
-    'driver' => 'pdo_mysql',
+require 'vendor/autoload.php';
+require 'public/include/config.php';
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
+use Symfony\Component\Console\Helper\HelperSet;
+
+$dbParams = array(
+    'dbname' => $config['dbname'],
+    'user' => $config['dbuser'],
+    'password' => $config['dbpass'],
+    'host' => $config['dbhost'],
+    'port' => $config['dbport'],
+    'driver' => 'pdo_mysql'
+);
+
+$connection = DriverManager::getConnection($dbParams);
+
+return new HelperSet([
+    'db' => new ConnectionHelper($connection),
 ]);
