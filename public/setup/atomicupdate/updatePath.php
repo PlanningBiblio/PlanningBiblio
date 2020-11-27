e681d055333cfc19cd4c9b504d12a46402e57efe
Sarah Deborde <sarah.deborde@biblibre.com>
2020-11-27 10:15:47 +0100

    correct link in absence menu

--- public/setup/atomicupdate/updatePath.php
+++ public/setup/atomicupdate/updatePath.php
@@ -3,6 +3,7 @@
 $sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/absence' WHERE `page`='absences/index.php';";
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`='/absence' WHERE `url` = 'absences/index.php';";
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`= '/absence' WHERE `url` = 'absences/voir.php';";
+$sql[]="UPDATE `{$dbprefix}menu` SET `url`= '/absence/add' WHERE `url` = '/absence' AND `titre` = 'Ajouter une absence';";
 
 $sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page` = 'absences/voir.php';";
 
