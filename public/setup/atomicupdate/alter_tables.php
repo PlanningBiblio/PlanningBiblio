ec4424f91b7cf93677d68d60cd109b7923940bf3
Sarah Deborde <sarah.deborde@biblibre.com>
2020-12-16 15:19:54 +0100

    displya message if agent's service is not an id

--- public/setup/atomicupdate/alter_tables.php
+++ public/setup/atomicupdate/alter_tables.php
@@ -11,21 +11,26 @@ if($services){
 
 
 $db = new db();
-$db->select("personnel","service",array("service" => "NOT REGEXP '^[0-9]+$'", null));
-if(!$db->result){
-    $sql[] = "ALTER TABLE `{$dbprefix}personnel` MODIFY `service` int(11) NOT NULL;";
+$db->select("personnel", array("id", "service"));
+foreach($db->result as $agent){
+    if(!preg_match('/^[0-9]+$/', $agent['service'])){
+        echo "Le service de l'agent n°{$agent['id']} n'est pas un id ! \n";
+    }
+}
 
-    $db2 = new db();
-    $db2->select("select_services","*", null,null);
-    if($db2->result){
-        foreach ($db2->result as $elem) {
-            $id = $elem['id'];
-            $old = $elem['valeur'];
-            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
-            if ($new != $old) {
-                $new = addslashes($new);
-                $sql[] = "UPDATE `{$dbprefix}select_services` SET `valeur`= '$new'  WHERE `id` = '$id';";
-            }
+$sql[] = "ALTER TABLE `{$dbprefix}personnel` MODIFY `service` int(11) NOT NULL;";
+
+$db2 = new db();
+$db2->select("select_services","*", null,null);
+if($db2->result){
+    foreach ($db2->result as $elem) {
+        $id = $elem['id'];
+        $old = $elem['valeur'];
+        $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+        if ($new != $old) {
+            $new = addslashes($new);
+            $sql[] = "UPDATE `{$dbprefix}select_services` SET `valeur`= '$new'  WHERE `id` = '$id';";
         }
     }
 }
+
