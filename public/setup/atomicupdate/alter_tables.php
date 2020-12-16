4f5ac0c2095178f02b910de6e4484384d73f64f5
Sarah Deborde <sarah.deborde@biblibre.com>
2020-12-16 14:51:12 +0100

    manage edition in only one file

--- public/setup/atomicupdate/alter_tables.php
+++ public/setup/atomicupdate/alter_tables.php
@@ -1,32 +1,31 @@
 <?php
 
 $db = new db();
-$db->select("select_services","*", null,null);
+$db->select2("select_services",array("id","valeur"), null,null);
+$services = $db->result;
+if($services){
+    foreach($services as $elem){
+        $sql[] = "UPDATE `{$dbprefix}personnel` SET `service`= '{$elem['id']}'  WHERE `service` = '{$elem['valeur']}';";
+    }
+}
 
-if($db->result){
-    foreach ($db->result as $elem) {
-        $id = $elem['id'];
-        $old = $elem['valeur'];
-        $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
-        if ($new != $old) {
-            $new = addslashes($new);
-            $sql[] = "UPDATE `{$dbprefix}select_services` SET `valeur`= '$new'  WHERE `id` = '$id';";
+
+$db = new db();
+$db->select("personnel","service",array("service" => "NOT REGEXP '^[0-9]+$'", null));
+if(!$db->result){
+    $sql[] = "ALTER TABLE `{$dbprefix}personnel` MODIFY `service` int(11) NOT NULL;";
+
+    $db2 = new db();
+    $db2->select("select_services","*", null,null);
+    if($db2->result){
+        foreach ($db2->result as $elem) {
+            $id = $elem['id'];
+            $old = $elem['valeur'];
+            $new = html_entity_decode($old, ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+            if ($new != $old) {
+                $new = addslashes($new);
+                $sql[] = "UPDATE `{$dbprefix}select_services` SET `valeur`= '$new'  WHERE `id` = '$id';";
+            }
         }
     }
 }
-
-$sql[] = "ALTER TABLE `{$dbprefix}personnel` ADD `service_tmp` varchar(255);";
-
-$db = new db();
-$db->select("personnel","*", null,null);
-
-if($db->result){
-    foreach ($db->result as $elem) {
-        $service = $elem['service'];
-        $id = $elem['id'];
-        $sql[] = "UPDATE `{$dbprefix}personnel` SET `service_tmp` = '$service', `service` = null  WHERE `id` = '$id';";
-    }
-}
-
-$sql[] = "ALTER TABLE `{$dbprefix}personnel` MODIFY `service` int(11) NOT NULL;";
-
