654ae740edcc810acfd3d3f5341e4aeb695f58dd
Sarah Deborde <sarah.deborde@biblibre.com>
2021-01-08 10:57:31 +0100

    add yaml file and update files in public/setup repo

--- public/setup/atomicupdate/updatepath.php
+++ public/setup/atomicupdate/updatepath.php
@@ -1,7 +1,7 @@
 <?php
 
-
-$sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/agent' WHERE `page`='personnel/index.php';";
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='personnel/index.php';";
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `nom`='Personnel - Modif';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/agent' WHERE `url`='personnel/index.php';";
 
