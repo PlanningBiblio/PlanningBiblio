49c99e7bd2540cc60b45c510743ce7151bb286dc
Jérôme Combes <jerome@planningbiblio.fr>
2021-01-15 15:24:39 +0100

    POC teleworking
    TEST PLAN :
    - Create an absence reason named "Teleworking" (Absences / Add / Reason (Motif) : Green cross)
    - Add the following line into your custom_options.php file (create the file if it doesn't exist) :
    $config['Absences-teleworking_reasons'] = array('Teleworking');
    - Edit a position and change "Teleworking compatibility" to "Yes"
    - You can now choose someone on this position even if he is absent for teleworking
    
    TODO :
    - Statistics pages must be adapted
    - Add a checkbox "teleworking compatibility" in the "absence reason" management interface instead of using a $config option

--- public/planning/poste/semaine.php
+++ public/planning/poste/semaine.php
@@ -151,6 +151,8 @@ echo "</div>";
 echo "</td><td id='td_boutons'>\n";
 
 //	----------------------------	Récupération des postes		-----------------------------//
+// $postes will also be used in the cellule_poste function. Using a global variable will avoid multiple access to the database and enhance performances
+global $postes;
 $postes=array();
 
 // Récupération des activités pour appliquer les classes aux lignes postes en fonction de celles-ci
@@ -196,7 +198,7 @@ if ($db->result) {
     
 
         // Tableau $postes
-        $postes[$elem['id']]=array("nom"=>$elem['nom'], "etage"=>$elem['etage'], "obligatoire"=>$elem['obligatoire'], "classes"=>join(" ", $classesPoste));
+        $postes[$elem['id']]=array("nom"=>$elem['nom'], "etage"=>$elem['etage'], "obligatoire"=>$elem['obligatoire'], "teleworking"=>$elem['teleworking'], "classes"=>join(" ", $classesPoste));
     }
 }
 
@@ -287,12 +289,14 @@ for ($j=0;$j<=$fin;$j++) {
       "ORDER BY `{$dbprefix}pl_poste`.`absent` desc,`{$dbprefix}personnel`.`nom`, `{$dbprefix}personnel`.`prenom`"
     );
 
+        // $cellules will be used in the cellule_poste function. Using a global variable will avoid multiple access to the database and enhance performances
         global $cellules;
         $cellules=$db->result?$db->result:array();
         usort($cellules, "cmp_nom_prenom");
 
         // Recherche des absences
         // Le tableau $absences sera utilisé par la fonction cellule_poste pour barrer les absents dans le plannings et pour afficher les absents en bas du planning
+        // $cellules will be used in the cellule_poste function. Using a global variable will avoid multiple access to the database and enhance performances
         $a=new absences();
         $a->valide=true;
         $a->fetch("`nom`,`prenom`,`debut`,`fin`", null, $date, $date);
@@ -316,6 +320,7 @@ for ($j=0;$j<=$fin;$j++) {
         usort($absences, "cmp_nom_prenom_debut_fin");
 
         // Informations sur les congés
+        // $conges will be used in the cellule_poste function. Using a global variable will avoid multiple access to the database and enhance performances
         $conges = array();
         global $conges;
         if ($config['Conges-Enable']) {
