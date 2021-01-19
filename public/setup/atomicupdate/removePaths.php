88ba9896eae625acb5f89ce867aa6ab6dc8af3b5
Sarah Deborde <sarah.deborde@biblibre.com>
2021-01-19 16:11:47 +0100

    update to fit installation from master

--- public/setup/atomicupdate/removePaths.php
+++ public/setup/atomicupdate/removePaths.php
@@ -1,4 +1,6 @@
 <?php
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `page`='' WHERE `groupe`!=''  AND `page` LIKE '%planningHebdo%' OR `page` = '/workinghour'";
-$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `groupe`='' AND `page` LIKE '%planningHebdo%' OR `page` = '/workinghour'";
+$sql[]="UPDATE `{$dbprefix}acces` SET `groupe`='', `page`='/workinghour' WHERE `nom`='Planning Hebdo - suppression'";
+$sql[]="UPDATE `{$dbprefix}acces` SET `nom`='Planning Hebdo - Admin N1' WHERE `nom`='Planning Hebdo - Index '";
+$sql[]="UPDATE `{$dbprefix}acces` SET `page`='' WHERE `groupe`!=''  AND (`page` LIKE '%planningHebdo%' OR `page` = '/workinghour')";
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `groupe`='' AND (`page` LIKE '%planningHebdo%' OR `page` = '/workinghour')";
