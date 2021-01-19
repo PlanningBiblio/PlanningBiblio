f709b1932c234a2f3967262e5cd05a446bbc9643
Sarah Deborde <sarah.deborde@biblibre.com>
2021-01-19 16:39:53 +0100

    update branch

--- public/setup/atomicupdate/updatepath.php
+++ public/setup/atomicupdate/updatepath.php
@@ -1,7 +1,6 @@
 <?php
 
 $sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='personnel/index.php';";
-$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `nom`='Personnel - Modif';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/agent' WHERE `url`='personnel/index.php';";
 
