061b7feb558ada91e9beed597a223aadb98c3fe3
Sarah Deborde <sarah.deborde@biblibre.com>
2021-01-19 15:30:23 +0100

    update file following feedback

--- public/setup/atomicupdate/removePaths.php
+++ public/setup/atomicupdate/removePaths.php
@@ -1,4 +1,4 @@
 <?php
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `page`='' WHERE `groupe_id` > 0 AND `page` LIKE '%planningHebdo%' OR `page` = '/workinghour'";
-$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `groupe_id` <= 0 AND `page` LIKE '%planningHebdo%' OR `page` = '/workinghour'";
+$sql[]="UPDATE `{$dbprefix}acces` SET `page`='' WHERE `groupe`!=''  AND `page` LIKE '%planningHebdo%' OR `page` = '/workinghour'";
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `groupe`='' AND `page` LIKE '%planningHebdo%' OR `page` = '/workinghour'";
