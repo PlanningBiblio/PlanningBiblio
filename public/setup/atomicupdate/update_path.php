4b8b2615858942d388021450d9c6892537142be1
Sarah Deborde <sarah.deborde@biblibre.com>
2021-01-20 16:24:17 +0100

    update link with atomic update

--- public/setup/atomicupdate/update_path.php
+++ public/setup/atomicupdate/update_path.php
@@ -1,6 +1,6 @@
  <?php
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `page` = '/statistics/time' WHERE `page`='statistiques/temps.php';";
+$sql[]="UPDATE `{$dbprefix}acces` SET `page` = '' WHERE `page`='statistiques/temps.php';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/statistics/time' WHERE `url`='statistiques/temps.php';";
 
