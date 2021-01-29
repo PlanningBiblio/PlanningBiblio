e583851080c56c15d405b57a4b15f48fce167fbc
Sarah Deborde <sarah.deborde@biblibre.com>
2021-01-29 16:23:06 +0100

    remove htmlentities in display for batch edition

--- public/setup/atomicupdate/updatepath.php
+++ public/setup/atomicupdate/updatepath.php
@@ -1,6 +1,7 @@
 <?php
 
 $sql[]="UPDATE `{$dbprefix}acces`SET `page` = '' WHERE `page`='personnel/index.php';";
+$sql[]="UPDATE `{$dbprefix}acces` SET `page`= '' WHERE `page`='personnel/valid.php';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/agent' WHERE `url`='personnel/index.php';";
 
