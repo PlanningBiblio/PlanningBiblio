b60baea0ab5645f3dadaf77016918f05a4ebe5b1
Sarah Deborde <sarah.deborde@biblibre.com>
2021-02-10 14:49:54 +0100

    include symfonization of lines' deletion in a table

--- src/Controller/FrameworkController.php
+++ src/Controller/FrameworkController.php
@@ -382,6 +382,97 @@ class FrameworkController extends BaseController
     }   
 
     /**
+     * @Route ("framework-table/save-line", name="framework.save_table_line", methods={"POST"})
+     */
+    public function saveTableLine(Request $request, Session $session){
+        $form_post = $request->request->all();
+        $CSRFToken = $form_post['CSRFToken'];
+        $tableauNumero = $form_post['id'];
+        $dbprefix = $GLOBALS['dbprefix'];
+
+        $post=array();
+        foreach ($_POST as $key => $value) {
+            $key = filter_var($key, FILTER_SANITIZE_STRING);
+            $post[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+        }
+
+        // Suppression des infos concernant ce tableau dans la table pl_poste_lignes
+        $db = new \db();
+        $db->CSRFToken = $CSRFToken;
+        $db->delete("pl_poste_lignes", array("numero" => $tableauNumero));
+
+        // Insertion des données dans la table pl_poste_lignes
+        $values = array();
+        foreach ($post as $key => $value) {
+            if ($value and substr($key, 0, 6) == "select") {
+                $tab = explode("_", $key);  //1: tableau ; 2 lignes
+                if (substr($tab[1], -5) == "Titre") {
+                    $type = "titre";
+                    $tab[1] = substr($tab[1], 0, -5);
+                } elseif (substr($tab[1], -6) == "Classe") {
+                    $type = "classe";
+                    $tab[1] = substr($tab[1], 0, -6);
+                } elseif (substr($value, -5) == "Ligne") {
+                    $type = "ligne";
+                    $value = substr($value, 0, -5);
+                } else {
+                    $type = "poste";
+                }
+                $values[] = array(
+                    ":numero"  => $tableauNumero, 
+                    ":tableau" => $tab[1], 
+                    ":ligne"   => $tab[2], 
+                    ":poste"   => $value, 
+                    ":type"    =>$type
+                );
+            }
+        }
+        if ($values[0]) {
+            $sql = "INSERT INTO `{$dbprefix}pl_poste_lignes` (`numero`,`tableau`,`ligne`,`poste`,`type`) ";
+            $sql.="VALUES (:numero, :tableau, :ligne, :poste, :type);";
+
+            $db = new \dbh();
+            $db->CSRFToken = $CSRFToken;
+            $db->prepare($sql);
+            foreach ($values as $elem) {
+                $db->execute($elem);
+            }
+        }
+
+        // Suppression des infos concernant ce tableau dans la table pl_poste_cellules
+        $db = new \db();
+        $db->CSRFToken = $CSRFToken;
+        $db->delete("pl_poste_cellules", array("numero" => $tableauNumero));
+
+        // Insertion des données dans la table pl_poste_cellules
+        $values=array();
+        foreach ($post as $key => $value) {
+            if ($value and substr($key, 0, 8)=="checkbox") {
+                $tab = explode("_", $key);  //1: tableau ; 2 lignes ; 3 colonnes
+                $values[] = array(
+                    ":numero"   =>$tableauNumero,
+                    ":tableau"  =>$tab[1],
+                    ":ligne"    =>$tab[2],
+                    ":colonne"  =>$tab[3]
+                );
+            }
+        }
+        if (!empty($values)) {
+            $sql="INSERT INTO `{$dbprefix}pl_poste_cellules` (`numero`,`tableau`,`ligne`,`colonne`) ";
+            $sql.="VALUES (:numero, :tableau, :ligne, :colonne)";
+
+            $db = new \dbh();
+            $db->CSRFToken = $CSRFToken;
+            $db->prepare($sql);
+            foreach ($values as $elem) {
+                $db->execute($elem);
+            }
+        }
+
+        return $this->json('ok');
+    }
+
+    /**
      * @Route ("/framework-group/add", name="framework.add_group", methods={"GET"})
      */
     public function addGroup (Request $request, Session $session){
