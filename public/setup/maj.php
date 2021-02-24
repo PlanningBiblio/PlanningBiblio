744977e837c5a93ebb0aec18e32ba1b4f5fabb0f
Alex Arnaud <alex.arnaud@biblibre.com>
2021-02-24 07:34:15 +0000

    Release 20.10.05

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -2109,6 +2109,11 @@ if (version_compare($config['Version'], $v) === -1) {
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
 
+$v="20.10.05.000";
+if (version_compare($config['Version'], $v) === -1) {
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
+
 //	Execution des requetes et affichage
 foreach ($sql as $elem) {
     $db=new db();
