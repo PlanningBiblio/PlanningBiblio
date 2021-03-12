ac51206bd94ad9b6f71c448d8e2dbc5be308e4b7
Alex Arnaud <alex.arnaud@biblibre.com>
2021-03-12 09:57:10 +0000

    Remove depreciations for dotrine naming strategy, security and class loader

--- public/init_plugins.php
+++ public/init_plugins.php
@@ -1,14 +1,15 @@
 <?php
 
-use Symfony\Component\ClassLoader\ClassMapGenerator;
-use Symfony\Component\ClassLoader\MapClassLoader;
+use Composer\Autoload\ClassMapGenerator;
+//use Symfony\Component\ClassLoader\MapClassLoader;
+use Composer\Autoload\ClassLoader;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $dispatcher = new EventDispatcher();
 
 if (file_exists(__DIR__ . '/../plugins')) {
     $mapping = ClassMapGenerator::createMap(__DIR__ . '/../plugins');
-    $loader = new MapClassLoader($mapping);
+    $loader = new ClassLoader($mapping);
     $loader->register();
 
     foreach ($mapping as $className => $classPath){
