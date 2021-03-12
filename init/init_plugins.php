8dddad0647c761638df9dcb859b1fee58486f4bb
Alex Arnaud <alex.arnaud@biblibre.com>
2021-03-12 13:35:34 +0000

    Fix plugins loader

--- init/init_plugins.php
+++ init/init_plugins.php
@@ -1,15 +1,15 @@
 <?php
 
 use Composer\Autoload\ClassMapGenerator;
-//use Symfony\Component\ClassLoader\MapClassLoader;
 use Composer\Autoload\ClassLoader;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $dispatcher = new EventDispatcher();
+$plugins_dir = __DIR__ . '/../plugins';
 
-if (file_exists(__DIR__ . '/../plugins')) {
-    $mapping = ClassMapGenerator::createMap(__DIR__ . '/../plugins');
-    $loader = new ClassLoader($mapping);
+if (file_exists($plugins_dir)) {
+    $mapping = ClassMapGenerator::createMap($plugins_dir);
+    $loader = new Composer\Autoload\ClassLoader($plugins_dir);
     $loader->register();
 
     foreach ($mapping as $className => $classPath){
