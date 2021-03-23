9b9cbeb5eaec64b4d7707d95fa2dfe6fe7f04912
Alex Arnaud <alex.arnaud@biblibre.com>
2021-03-23 16:44:10 +0000

    symfonize statistics by status: use yaml permissions

--- public/setup/atomicupdate/symfonize_statistics_status.php
+++ public/setup/atomicupdate/symfonize_statistics_status.php
@@ -1,5 +1,5 @@
 <?php
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `page`='/statistics/status' WHERE `page`='statistiques/statut.php';";
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='statistiques/statut.php';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET  `url`='/statistics/status' WHERE `url`='statistiques/statut.php';";
