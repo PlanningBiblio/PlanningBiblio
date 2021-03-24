792d9b44d07e1c6007424d3e96e142b5280e77ca
Alex Arnaud <alex.arnaud@biblibre.com>
2021-03-24 10:09:34 +0000

    Fix symfonize_statistics_services atomicupdate

--- public/setup/atomicupdate/symfonize_statistics_services.php
+++ public/setup/atomicupdate/symfonize_statistics_services.php
@@ -1,4 +1,4 @@
 <?php
-$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page` = 'statistiques/service.php'"
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page` = 'statistiques/service.php'";
 $sql[]="UPDATE `{$dbprefix}menu` SET `url` = '/statistics/service' WHERE `url` = 'statistiques/service.php' ;";
 
