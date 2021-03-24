c47acd23a96591c24b9d31e62bdf140dbad71e15
Sarah Deborde <sarah.deborde@etu.utc.fr>
2021-03-24 10:11:36 +0000

    Symfonise stats absences

---
+++ public/setup/atomicupdate/update_paths_absence.php
@@ -0,0 +1,3 @@
+<?php
+    $sql[]="UPDATE `{$dbprefix}acces` SET  `page`='/statistics/absence' WHERE `page`='statistiques/absences.php';";
+    $sql[]="UPDATE `{$dbprefix}menu` SET  `url`='/statistics/absence' WHERE `url`='statistiques/absences.php';";
