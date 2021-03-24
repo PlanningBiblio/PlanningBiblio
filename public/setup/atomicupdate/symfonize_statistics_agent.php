aec7e141a89e8fed7b9322fa32e66bd54abf3fd3
Alex Arnaud <alex.arnaud@biblibre.com>
2021-03-24 15:44:07 +0000

    Fix 2 atomicupdates

--- public/setup/atomicupdate/symfonize_statistics_agent.php
+++ public/setup/atomicupdate/symfonize_statistics_agent.php
@@ -1,3 +1,4 @@
 <?php
 
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='statistiques/agents.php';";
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`= '/statistics/agent' WHERE `url` = 'statistiques/agents.php';";
--- public/setup/atomicupdate/symfonize_statistics_position_summary.php
+++ public/setup/atomicupdate/symfonize_statistics_position_summary.php
@@ -1,3 +1,4 @@
 <?php
 
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='statistiques/postes_synthese.php';";
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`='/statistics/positionsummary' WHERE `url`='statistiques/postes_synthese.php';";
