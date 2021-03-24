2f01c69c479a430ad0b2542f928cc367d28fad97
Alex Arnaud <alex.arnaud@biblibre.com>
2021-03-24 15:23:53 +0000

    Symfonize stats by agent: Use yaml permissions and update stats index template

--- public/setup/atomicupdate/symfonize_statistics_agent.php
+++ public/setup/atomicupdate/symfonize_statistics_agent.php
@@ -1,5 +1,3 @@
 <?php
 
-$sql[]="UPDATE `{$dbprefix}acces` SET `page`= '/statistics/agent' WHERE `page` = 'statistiques/agents.php';";
-
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`= '/statistics/agent' WHERE `url` = 'statistiques/agents.php';";
