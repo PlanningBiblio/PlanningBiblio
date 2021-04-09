c27864f83a439153af479762490cc8feafc8f3d4
Alex Arnaud <alex.arnaud@biblibre.com>
2021-04-09 13:20:03 +0000

    Symfonize stats position: - fix calculation of hours and use twig hours filter - fix totals and average by site - update stats index - use YAML permissions

--- src/Controller/StatisticController.php
+++ src/Controller/StatisticController.php
@@ -3138,7 +3138,7 @@ class StatisticController extends BaseController
         $multisites= [];
         if ($nbSites > 1){
             for ($i = 1; $i <= $nbSites; $i++){
-                $multisites[] = $this->config("Multisites-site{$i}");
+                $multisites[$i] = $this->config("Multisites-site{$i}");
             }
         }
 
@@ -3246,11 +3246,10 @@ class StatisticController extends BaseController
                                 $agents[$elem['perso_id']] = array($elem['perso_id'], $elem['nom'], $elem['prenom'], 0, "site" => $elem['site']);
                             }
                             $agents[$elem['perso_id']][3] = floatval($agents[$elem['perso_id']][3]) + diff_heures($elem['debut'], $elem['fin'], "decimal");
-                            $agents[$elem['perso_id']][3] = heure4($agents[$elem['perso_id']][3]);
 
                             // On compte les heures de chaque site
                             if ($nbSites > 1) {
-                                $sites[$elem['site']]  = floatval($sites[$elem['site']]) + diff_heures($elem['debut'], $elem['fin'], "decimal");
+                                $sites[$elem['site']] = floatval($sites[$elem['site']]) + diff_heures($elem['debut'], $elem['fin'], "decimal");
                             }
 
                             // On compte toutes les heures (globales)
@@ -3265,7 +3264,6 @@ class StatisticController extends BaseController
                                 $services[$service] = array("nom"=>$service,"heures"=>0);
                             }
                             $services[$service]["heures"] = floatval($services[$service]["heures"]) + diff_heures($elem['debut'], $elem['fin'], "decimal");
-                            $services[$service]["heures"] = heure4($services[$service]["heures"]);
 
                             // On créé un tableau par statut
                             if (array_key_exists($elem['perso_id'], $agents_infos)) {
@@ -3276,7 +3274,6 @@ class StatisticController extends BaseController
                                 $statuts[$statut] = array("nom" => $statut, "heures" => 0);
                             }
                             $statuts[$statut]["heures"] = floatval($statuts[$statut]["heures"]) + diff_heures($elem['debut'], $elem['fin'], "decimal");
-                            $statuts[$statut]["heures"] = heure4($statuts[$statut]["heures"]);
 
                             // On met dans tab tous les éléments (infos postes + agents + heures du poste)
                             $tab[$poste] = array($poste_tab, $agents, $heures, "services" => $services, "statuts" => $statuts, "sites" => $sites);
@@ -3312,7 +3309,6 @@ class StatisticController extends BaseController
                         }
                     }
                 }
-                $tab[$key][2] = heure4($tab[$key][2]);
                 if ($tab[$key][0][2]) {
                     $siteEtage[] = $tab[$key][0][2];
                 }
@@ -3321,22 +3317,25 @@ class StatisticController extends BaseController
                 } else {
                     $siteEtage=null;
                 }
-                $jour = heure4($tab[$key][2])/$nbJours;
+                $jour = $tab[$key][2] / $nbJours;
                 $hebdo = $jour*$joursParSemaine;
                 $av_jour = null;
                 $av_hebdo = null;
 
                 if ($nbSites>1) {
                     for ($i = 1 ; $i <= $nbSites; $i++) {
-                        if ($tab[$key]["sites"][$i] and $tab[$key]["sites"][$i] != $tab[$key][2]) {
-                            $av_jour = $tab[$key]["sites"][$i]/$nbJours;
-                            $av_hebdo = $jour*$joursParSemaine;
-                        }
+                        $total = $tab[$key]["sites"][$i];
+                        $av_jour = $total / $nbJours;
+                        $average = $av_jour * $joursParSemaine;
+                        $tab[$key]["sites"][$i] = array(
+                            'total' => $total,
+                            'average' => $average
+                        );
                     }
                 }
 
-                $tab[$key]["jour"] = heure4($jour);
-                $tab[$key]["hebdo"] = heure4($hebdo);
+                $tab[$key]["jour"] = $jour;
+                $tab[$key]["hebdo"] = $hebdo;
                 $tab[$key]["av_hebdo"] = $av_hebdo;
                 $tab[$key]["av_jour"] = $av_jour;
                 $tab[$key]["siteEtage"] = $siteEtage;
