de2786eb95f5d07bfbf4e287a633f9fc76d1a666
Alex Arnaud <alex.arnaud@biblibre.com>
2021-04-14 13:58:35 +0000

    Symfonize support position: update index and use YAML permissions

--- public/setup/atomicupdate/symfonize_support_position.php
+++ public/setup/atomicupdate/symfonize_support_position.php
@@ -1,5 +1,5 @@
 <?php
 
-$sql[]="UPDATE`{$dbprefix}acces` SET  `page`='/statistics/supportposition' WHERE `page`='statistiques/postes_renfort.php';";
+$sql[]="DELETE FROM `{$dbprefix}acces` WHERE `page`='statistiques/postes_renfort.php';";
 
 $sql[]="UPDATE `{$dbprefix}menu` SET `url`='/statistics/supportposition' WHERE `url`='statistiques/postes_renfort.php';";
