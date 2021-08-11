6a741d4ab5715ef8b7f2a6ae86b4a621e2f9b6b9
Jérôme Combes <jerome@planningbiblio.fr>
2021-08-11 09:35:43 +0200

    MT 33011 Timepicker: Do not change config Granularite for current users

--- public/setup/atomicupdate/MT33011_timepicker.php
+++ public/setup/atomicupdate/MT33011_timepicker.php
@@ -1,3 +1,2 @@
 <?php
 $sql[]="UPDATE `{$dbprefix}config` SET valeurs = '[[1, \"Libre\"],[60,\"Heure\"],[30,\"Demi-heure\"],[15,\"Quart d\'heure\"],[5,\"5 minutes\"]]' WHERE nom = 'Granularite'";
-$sql[]="UPDATE `{$dbprefix}config` SET valeur = '1' WHERE nom = 'Granularite'";
