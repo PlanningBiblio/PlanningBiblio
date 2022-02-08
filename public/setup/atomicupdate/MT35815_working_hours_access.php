1c62943db014822c7fd05ffbde24d9b1a0d8e79e
Jérôme Combes <jerome@planningbiblio.fr>
2022-02-08 11:51:20 +0100

    MT35815 : rename working hours ACL

---
+++ public/setup/atomicupdate/MT35815_working_hours_access.php
@@ -0,0 +1,4 @@
+<?php
+
+$sql[] = "UPDATE `acces` SET `groupe` = 'Gestion des heures de présence, validation niveau 1' WHERE `groupe` = 'Gestion des heures de présences, validation niveau 1';";
+$sql[] = "UPDATE `acces` SET `groupe` = 'Gestion des heures de présence, validation niveau 2' WHERE `groupe` = 'Gestion des heures de présences, validation niveau 2';";
