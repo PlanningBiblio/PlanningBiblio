9b3517834217c8c37c41ec0b1efc9487780e3ecc
Alex Arnaud <alex.arnaud@biblibre.com>
2022-02-15 12:47:30 +0000

    Validation by agent: Dynamically get validation statuses in absence

--- public/setup/atomicupdate/MT35062_new_validation_schema.php
+++ public/setup/atomicupdate/MT35062_new_validation_schema.php
@@ -7,3 +7,5 @@ $sql[] = "ALTER TABLE `{$dbprefix}responsables` ADD COLUMN `notification_level2`
 $sql[] = "ALTER TABLE `{$dbprefix}responsables` ADD COLUMN `level1` INT(1) NOT NULL DEFAULT '1' AFTER `responsable`";
 $sql[] = "ALTER TABLE `{$dbprefix}responsables` ADD COLUMN `level2` INT(1) NOT NULL DEFAULT '0' AFTER `level1`";
 
+$sql[] = "INSERT INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `valeurs`, `categorie`, `commentaires`, `ordre` ) VALUES ('Absences-Validation-N2', 'enum2', '0', '[[0,\"Validation directe autoris&eacute;e\"],[1,\"L\'absence doit &ecirc;tre valid&eacute; au niveau 1\"]]', 'Absences', 'La validation niveau 2 des absences peut se faire directement ou doit attendre la validation niveau 1', '31')";
+
