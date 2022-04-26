d1637ec0ea3dcabcf1953f3273ae146cc78ca545
Jérôme Combes <jerome@planningbiblio.fr>
2022-04-26 18:27:18 +0200

    Make installer set APP_ENV=prod

--- install.sh
+++ install.sh
@@ -185,6 +185,7 @@ sed -i "s/APP_SECRET=.*/APP_DEBUG=0\nAPP_SECRET=${planningbsecret}/" .env.local
 
 sed -i "s/DATABASE_URL=.*/DATABASE_URL=mysql:\/\/$planningbdbuser:$planningbdbpass@$planningdbhost:$planningdbport\/$planningbdbname/" .env.local
 sed -i "s/DATABASE_PREFIX=.*/DATABASE_PREFIX=$planningbdbprefix/" .env.local
+sed -i "s/APP_ENV=dev/APP_ENV=prod/g" .env.local
 
 # Set the light_blue theme
 mysql -h $planningdbhost -u $planningbdbuser --password=$planningbdbpass -e "UPDATE $planningbdbname.\`config\` SET \`valeur\` = 'light_blue' WHERE \`nom\` = 'Affichage-theme';"
