011423641028cbf02f6bc9d2f1276964b556c67b
Jérôme Combes <jerome@planningbiblio.fr>
2022-08-26 16:16:15 +0200

    MT 37705: remove Holiday CET, Follow-up

--- public/setup/atomicupdate/MT37705_remove_conges_cet_table.php
+++ public/setup/atomicupdate/MT37705_remove_conges_cet_table.php
@@ -1,4 +1,4 @@
 <?php
 
 $sql[] = "DROP TABLE IF EXISTS `{$dbprefix}conges_cet`;";
-$sql[] = "DELETE FROM {$dbprefix}acces WEHERE page = 'conges/cet.php';";
+$sql[] = "DELETE FROM {$dbprefix}acces WHERE page = 'conges/cet.php';";
