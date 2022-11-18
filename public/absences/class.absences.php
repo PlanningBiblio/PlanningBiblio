f79b238b2828c98d6f7ef5e7ec43aa8c260198b1
Alex Arnaud <alex.arnaud@biblibre.com>
2022-11-18 09:50:03 +0100

    Fix a require for test ClassAbsencesRRulesTest::testEveryTwoWeeks

--- public/absences/class.absences.php
+++ public/absences/class.absences.php
@@ -1889,7 +1889,7 @@ class absences
     public function infoPlannings()
     {
         $version="absences";
-        require_once "postes/class.postes.php";
+        require_once(__DIR__ . '/../../public/postes/class.postes.php');
   
         $debut=dateSQL($this->debut);
         $fin=dateSQL($this->fin);
