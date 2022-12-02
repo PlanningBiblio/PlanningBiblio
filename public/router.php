d21aee0ea01e0777f06b24d953a849598addb7e2
Jérôme Combes <jerome@planningbiblio.fr>
2022-12-02 15:56:43 +0100

    DBRev: 22.11.00.000, change the release number on init files

--- public/router.php
+++ public/router.php
@@ -1,6 +1,6 @@
 <?php
 
-    $version = '22.10.00.000';
+    $version = '22.11.00.000';
 
     $file = __DIR__ . '/../public' . $_SERVER['REQUEST_URI'];
     $file = str_replace("?version=$version", '', $file);
