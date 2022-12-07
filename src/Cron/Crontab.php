5c7428f90970124e0714649686acc50d6aa98c08
Alex Arnaud <alex.arnaud@biblibre.com>
2022-12-07 14:20:09 +0100

    MT 37569: adapt holiday reseting cron jobs - code refactoring

---
+++ src/Cron/Crontab.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Cron;
+
+use App\Model\Cron;
+
+class Crontab {
+
+    public static $crons_dir = __DIR__ . '/../../src/Cron/Legacy/';
+
+    private $executable_crons = array();
+
+    private $entityManager;
+
+    public function __construct()
+    {
+        $this->entityManager = $GLOBALS['entityManager'];
+
+        $today = date('Y-m-d');
+
+        $crons = $this->entityManager->getRepository(Cron::class)->findAll();
+
+        foreach ($crons as $cron) {
+            if ($cron->isDisabled()) {
+                continue;
+            }
+
+            $date_cron = $cron->last()->format('Y-m-d H:m:s');
+
+            // Daily crons.
+            if ($cron->dom() == '*' and $cron->mon() == '*' and $cron->dow() == '*') {
+                if ($date_cron < $today) {
+                    $this->executable_crons[] = $cron;
+                }
+                continue;
+            }
+
+            // Yearly Cron
+            if ($cron->dom() != '*' and $cron->mon() != '*') {
+                $command_date = strtotime("{$cron->mon()}/{$cron->dom()}");
+                if ($command_date > time()) {
+                    $command_date = strtotime('-1 year', $command_date);
+                }
+
+                $command_date = date('Y-m-d 00:00:00', $command_date);
+
+                if ($date_cron < $command_date) {
+                    $this->executable_crons[] = $cron;
+                }
+            }
+        }
+    }
+
+    public function crons()
+    {
+        return $this->executable_crons;
+    }
+
+    public static function update_cron($cron)
+    {
+        $last = date('Y-m-d H:i:s');
+        $last = \DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'));
+
+        $cron->last($last);
+        $entityManager = $GLOBALS['entityManager'];
+        $entityManager->persist($cron);
+        $entityManager->flush();
+    }
+}
---
+++ src/Model/Cron.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Model;
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\GeneratedValue;
+
+/**
+ * @Entity @Table(name="cron")
+ **/
+class Cron extends PLBEntity
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+
+    /** @Column(type="string", length=2) **/
+    protected $m;
+
+    /** @Column(type="string", length=2) **/
+    protected $h;
+
+    /** @Column(type="string", length=2) **/
+    protected $dom;
+
+    /** @Column(type="string", length=2) **/
+    protected $mon;
+
+    /** @Column(type="string", length=2) **/
+    protected $dow;
+
+    /** @Column(type="text") **/
+    protected $command;
+
+    /** @Column(type="text") **/
+    protected $comments;
+
+    /** @Column(type="datetime") **/
+    protected $last;
+
+    /** @Column(type="string", length=1) **/
+    protected $disabled;
+
+    public function isDisabled()
+    {
+        if ($this->disabled() == '1') {
+            return true;
+        }
+
+        return false;
+    }
+}
