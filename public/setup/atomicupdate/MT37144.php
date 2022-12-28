c6fe274667a18b4de3d9337de8fa5292758864f5
Jérôme Combes <jerome@planningbiblio.fr>
2022-12-28 09:25:00 +0100

    Remove SR warning on lunch positions : add the atomic update to check the lunch option

--- public/setup/atomicupdate/MT37144.php
+++ public/setup/atomicupdate/MT37144.php
@@ -1,3 +1,9 @@
 <?php
 
 $sql[] = "ALTER TABLE `{$dbprefix}postes` ADD COLUMN IF NOT EXISTS `lunch` TINYINT(1) NOT NULL DEFAULT 0 AFTER `bloquant`;";
+
+if (!empty($config['Position-Lunch']) and is_array($config['Position-Lunch'])) {
+    foreach ($config['Position-Lunch'] as $elem) {
+        $sql[] = "UPDATE `{$dbprefix}postes` SET `lunch` = '1' WHERE `id` = '$elem';";
+    }
+}
