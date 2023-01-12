24d82327c8dad94c22776b3d69b9c56956742775
Louise Molé <louise.mole@biblibre.com>
2023-01-12 14:06:34 +0100

    Add revue on AbsenceInfoController unit test

--- tests/Controller/AbsenceInfoControllerTest.php
+++ tests/Controller/AbsenceInfoControllerTest.php
@@ -35,7 +35,6 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
 
 
         $info = $entityManager->getRepository(AbsenceInfo::class)->findOneBy(array('texte' => 'salut'));
-        echo($info->fin()->format('Y-m-d'));
         $this->assertEquals($start, $info->debut(), "debut is ok");
         $this->assertEquals($end, $info->fin(), "fin is ok");
         $this->assertEquals('salut', $info->texte(), 'info texte is salut');
@@ -63,13 +62,13 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
         $this->assertSelectorTextContains('h4', 'Ajout d\'une information');
 
         $result=$crawler->filter('label')->eq(0);
-        $this->assertEquals($result->text(),'Date de début','label 1 is Date de début');
+        $this->assertEquals($result->text(null,false),'Date de début','label 1 is Date de début');
 
         $result=$crawler->filter('label')->eq(1);
-        $this->assertEquals($result->text(),'Date de fin','label 2 is Date de fin');
+        $this->assertEquals($result->text(null,false),'Date de fin','label 2 is Date de fin');
 
         $result = $crawler->filter('label')->eq(2);
-        $this->assertEquals($result->text(),'Texte','label 3 is Texte');
+        $this->assertEquals($result->text(null,false),'Texte','label 3 is Texte');
 
         $result = $crawler->filterXPath('//input[@class="datepicker"]');
         $this->assertEquals($result->eq(0)->attr('name'),'start','input datepicker name is start');
@@ -82,7 +81,7 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
         $this->assertEquals($class->attr('value'),'Valider','input submit value is Valider');
 
         $class = $crawler->filterXPath('//a[@class="ui-button ui-button-type2"]');
-        $this->assertEquals($class->text(),'Annuler','a button is Annuler');
+        $this->assertEquals($class->text(null,false),'Annuler','a button is Annuler');
     }
 
     public function testFormEdit()
@@ -120,9 +119,9 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
         $this->assertSelectorTextContains('h4', 'Modifications des informations sur les absences');
 
         $result=$crawler->filter('label');
-        $this->assertEquals($result->eq(0)->text(),'Date de début','label 1 is Date de début');
-        $this->assertEquals($result->eq(1)->text(),'Date de fin','label 2 is Date de fin');
-        $this->assertEquals($result->eq(2)->text(),'Texte','label 3 is Texte');
+        $this->assertEquals($result->eq(0)->text(null,false),'Date de début','label 1 is Date de début');
+        $this->assertEquals($result->eq(1)->text(null,false),'Date de fin','label 2 is Date de fin');
+        $this->assertEquals($result->eq(2)->text(null,false),'Texte','label 3 is Texte');
 
         $class = $crawler->filterXPath('//input[@name="start"]');
         $this->assertEquals($class->attr('value'),'05/10/2022','input submit start is 05/10/2022');
@@ -131,16 +130,16 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
         $this->assertEquals($class->attr('value'),'10/10/2022','input submit end is 10/10/2022');
 
         $class = $crawler->filterXPath('//textarea');
-        $this->assertEquals($class->text(),'salut','input submit text is salut');
+        $this->assertEquals($class->text(null,false),'salut','input submit text is salut');
 
         $class = $crawler->filterXPath('//input[@class="ui-button"]');
         $this->assertEquals($class->attr('value'),'Valider','input submit value is Valider');
 
         $class = $crawler->filterXPath('//a[@class="ui-button ui-button-type2"]');
-        $this->assertEquals($class->text(),'Annuler','a button is Annuler');
+        $this->assertEquals($class->text(null,false),'Annuler','a button is Annuler');
 
         $class = $crawler->filterXPath('//a[@class="ui-button ui-button-type3"]');
-        $this->assertEquals($class->text(),'Supprimer','a button is Supprimer');
+        $this->assertEquals($class->text(null,false),'Supprimer','a button is Supprimer');
     }
 
     public function testAbsenceInfoList()
@@ -160,14 +159,16 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
         $this->assertSelectorTextContains('h3', 'Informations sur les absences');
 
         $result = $crawler->filterXPath('//a[@class="ui-button"]');
-        $this->assertEquals('Ajouter', $result->text(),'a is Ajouter');
+        $this->assertEquals('Ajouter', $result->text(null,false),'a is Ajouter');
 
         $result = $crawler->filterXPath('//div');
-        $this->assertStringContainsString('Aucune information enregistrée.', $result->eq(7)->text(),  'text no info is Aucune information enregistrée.');	
+        $this->assertStringContainsString('Aucune information enregistrée.', $result->eq(7)->text(null,false),  'text no info is Aucune information enregistrée.');	
 
         $d = date("d")+1;
+        if($d < 10){ $d = "0$d"; }
         $m_1 = date("m");
         $m_2 = date("m")+1;
+        if($m_2 < 10){ $m_2 = "0$m_2"; }
         $Y = date("Y");
 
         $start = \DateTime::createFromFormat("d/m/Y", "$d/$m_1/$Y");
@@ -187,22 +188,21 @@ class AbsenceInfoControllerTest extends PLBWebTestCase
         $this->assertSelectorTextContains('h3', 'Informations sur les absences');
 
         $result = $crawler->filterXPath('//a[@class="ui-button"]');
-        $this->assertEquals('Ajouter', $result->text(),'a is Ajouter');
+        $this->assertEquals('Ajouter', $result->text(null,false),'a is Ajouter');
 
-        $result = $crawler->filterXPath('//th[@class="dataTableDateFR tableSort"]');
-        $this->assertEquals($result->eq(0)->text(),'Début','table title id Début');
-        $this->assertEquals($result->eq(1)->text(),'Fin','table title is Fin');
+        $result = $crawler->filterXPath('//table[@id="AbsenceInfoTable"]');
+        $this->assertStringContainsString('Début',$result->text(null,false),'table title id Début');
+        $this->assertStringContainsString('Fin',$result->text(null,false),'table title is Fin');
 
-        $result = $crawler->filterXPath('//th[@class="tableSort"]');
-        $this->assertEquals($result->text(),'Texte','table title is Texte');
+        $this->assertStringContainsString('Texte',$result->text(null,false),'table title is Texte');
 
         $result = $crawler->filterXPath('//span[@class="pl-icon pl-icon-edit"]');
         $this->assertEquals($result->attr('title'),'Edit','span logo edit title is Edit');
 
         $result = $crawler->filterXPath('//tbody/tr/td');
-        $this->assertEquals($result->eq(1)->text(),"$d/$m_1/$Y",'date début is ok');
-        $this->assertEquals($result->eq(2)->text(),"$d/$m_2/$Y",'date fin is ok');
-        $this->assertEquals($result->eq(3)->text(),'hello','text info is ok');
+        $this->assertEquals($result->eq(1)->text(null,false),"$d/$m_1/$Y",'date début is ok');
+        $this->assertEquals($result->eq(2)->text(null,false),"$d/$m_2/$Y",'date fin is ok');
+        $this->assertEquals($result->eq(3)->text(null,false),'hello','text info is ok');
     }
 }
 
