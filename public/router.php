9b39843daee9302cf03b00172eee95288a5562f6
Alex Arnaud <alex.arnaud@biblibre.com>
2023-01-27 10:45:47 +0100

    Release 22.10.04

--- public/router.php
+++ public/router.php
@@ -1,6 +1,6 @@
 <?php
 
-    $version = '22.10.03.001';
+    $version = '22.10.04.000';
 
     $file = __DIR__ . '/../public' . $_SERVER['REQUEST_URI'];
     $file = str_replace("?version=$version", '', $file);
