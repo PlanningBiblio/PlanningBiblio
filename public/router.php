4ba7b680a8eb78d563adc46e70912a4dedcfed7d
Jérôme Combes <jerome@planningbiblio.fr>
2023-01-31 17:12:36 +0100

    DBRev 22.10.04.001: MT39656 Holiday reset script

--- public/router.php
+++ public/router.php
@@ -1,6 +1,6 @@
 <?php
 
-    $version = '22.10.04.000';
+    $version = '22.10.04.001';
 
     $file = __DIR__ . '/../public' . $_SERVER['REQUEST_URI'];
     $file = str_replace("?version=$version", '', $file);
