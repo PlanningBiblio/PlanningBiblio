c3203c825b99989f97f1e9b5e6aef74bf43e4251
Jérôme Combes <jerome@planningbiblio.fr>
2023-02-14 14:43:56 +0100

    MT39932 - remove HTML entities from users name

--- public/setup/atomicupdate/MT39932.php
+++ public/setup/atomicupdate/MT39932.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // MT39932 - remove HTML entities from frameworks title
 $db = new db();
 $db->select2('pl_poste_lignes');
@@ -12,6 +11,24 @@ if ($db->result) {
         if ($new != $old) {
             $new = addslashes($new);
             $sql[] = "UPDATE `{$dbprefix}pl_poste_lignes` SET `poste` = '$new' WHERE `id` = '{$elem['id']}';";
+        }
+    }
+}
+
+// MT39932 - remove HTML entities from users firstname and lastname
+$db = new db();
+$db->select2('personnel');
+
+if ($db->result) {
+    foreach ($db->result as $elem) {
+        $oldLastName = $elem['nom'];
+        $newLastName = html_entity_decode($elem['nom'], ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+        $oldFirstName = $elem['prenom'];
+        $newFirstName = html_entity_decode($elem['prenom'], ENT_QUOTES|ENT_IGNORE, 'UTF-8');
+        if (($newLastName != $oldLastName) or ($newFirstName != $oldFirstName)) {
+            $newLastName = addslashes($newLastName);
+            $newFirstName = addslashes($newFirstName);
+            $sql[] = "UPDATE `{$dbprefix}personnel` SET `nom` = '$newLastName', `prenom` = '$newFirstName' WHERE `id` = '{$elem['id']}';";
         }
     }
 }
