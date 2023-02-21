a4a72a7538755c4ba9d80cefbf3a4222616abef5
Matthias Meusburger <matthias.meusburger@biblibre.com>
2023-02-21 16:50:16 +0100

    MT39491: Fix save agent save when holidays values are empty
    Test plan:
    
     1) without the patch, edit an agent and blank one value or more in the
        holidays tab.
    
     2) Check that saving fails.
    
     3) With the patch, edit an agent and blank one value or more in the
        holidays tab.
    
     4) Check that saving works and that the values have been defaulted to 0.

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -1112,6 +1112,7 @@ class AgentController extends BaseController
 
         foreach ($available_keys as $key) {
             $params[$key . '_min'] = isset($params[$key . '_min']) ? $params[$key . '_min'] : 0;
+            $params[$key] = !empty($params[$key]) ? $params[$key] : 0;
         }
 
         if ($this->config('Conges-Mode') == 'jours' ) {
