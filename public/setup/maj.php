ef391cc1ec74b18cc794e5e931e8c936f6bc6d73
Jérôme Combes <jerome@planningbiblio.fr>
2023-02-24 17:03:49 +0100

    MT37657: Add IF NOT EXISTS on the create table absences_documents query

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -1715,7 +1715,7 @@ if (version_compare($config['Version'], $v) === -1) {
 
 $v="19.11.00.004";
 if (version_compare($config['Version'], $v) === -1) {
-    $sql[] = "CREATE TABLE `{$dbprefix}absences_documents` (id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,absence_id int(11) NOT NULL,filename text NOT NULL, date DATETIME NOT NULL);";
+    $sql[] = "CREATE TABLE IF NOT EXISTS `{$dbprefix}absences_documents` (id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,absence_id int(11) NOT NULL,filename text NOT NULL, date DATETIME NOT NULL);";
     $sql[] = "INSERT IGNORE INTO `{$dbprefix}acces` VALUES(NULL, 'Absences - Voir document', 100, '', '/absences/document', 0, 'Absences')";
     $sql[] = "INSERT IGNORE INTO `{$dbprefix}acces` VALUES(NULL, 'Absences - liste documents', 100, '', '/absences/documents', 0, 'Absences')";
     $sql[] = "INSERT IGNORE INTO `{$dbprefix}config` (`nom`, `type`, `valeur`, `categorie`, `ordre`, `commentaires`) VALUES ('Absences-DelaiSuppressionDocuments', 'text', '90', 'Absences','100', 'Les documents associ&eacute;s aux absences sont supprim&eacute;s au-del&agrave; du nombre de jours d&eacute;finis par ce param&egrave;tre.');";
