eb29556ae28578aa308e707753993c66799c3eb6
Jérôme Combes <jerome@planningbiblio.fr>
2023-06-16 12:17:37 +0200

    MT41142: Color the logged in agent, Follow up
    - Add the color picker on the configuration page

--- public/setup/atomicupdate/MT41142.php
+++ public/setup/atomicupdate/MT41142.php
@@ -1,3 +1,6 @@
 <?php
 
-$sql[] = "INSERT IGNORE INTO `{$dbprefix}config` ( `nom`, `type`, `valeur`, `commentaires`, `categorie`, `valeurs`, `extra`, `ordre`) VALUES ('Affichage-Agent', 'text', '#FFF3B3', 'Couleur des cellules de l\'agent connecté. Au format hexadécimal, exemple : #FFF3B3.', 'Affichage', '', NULL, '40');";
+// FIXME : la requête DELETE FROM est utile pour les tests mais ne doit pas être copiée dans le fichier maj.php
+$sql[] = "DELETE FROM `{$dbprefix}config` WHERE `nom` = 'Affichage-Agent';";
+
+$sql[] = "INSERT IGNORE INTO `{$dbprefix}config` ( `nom`, `type`, `valeur`, `commentaires`, `categorie`, `valeurs`, `extra`, `ordre`) VALUES ('Affichage-Agent', 'color', '#FFF3B3', 'Couleur des cellules de l\'agent connecté', 'Affichage', '', NULL, '40');";
