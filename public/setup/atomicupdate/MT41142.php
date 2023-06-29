91e2fa63b717c5a7f0ece8145b34660338ec7ddb
Jérôme Combes <jerome@planningbiblio.fr>
2023-06-29 13:49:05 +0200

    MT41142: Color the logged in agent, Follow up
    - Set the type of the new paramater to "color" in db_data.php
    - Remove the delete query from atomic update

--- public/setup/atomicupdate/MT41142.php
+++ public/setup/atomicupdate/MT41142.php
@@ -1,6 +1,3 @@
 <?php
 
-// FIXME : la requête DELETE FROM est utile pour les tests mais ne doit pas être copiée dans le fichier maj.php
-$sql[] = "DELETE FROM `{$dbprefix}config` WHERE `nom` = 'Affichage-Agent';";
-
 $sql[] = "INSERT IGNORE INTO `{$dbprefix}config` ( `nom`, `type`, `valeur`, `commentaires`, `categorie`, `valeurs`, `extra`, `ordre`) VALUES ('Affichage-Agent', 'color', '#FFF3B3', 'Couleur des cellules de l\'agent connecté', 'Affichage', '', NULL, '40');";
