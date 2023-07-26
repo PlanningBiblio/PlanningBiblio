54e8aa53d5c8507ca183c958ce45a0f330c4cc38
Jérôme Combes <jerome@planningbiblio.fr>
2023-07-26 11:18:35 +0200

    Use the standard symfony index with Planno's additions

--- public/index.php
+++ public/index.php
@@ -1,12 +1,22 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+/**
+ * Start of Planno additions
+ * TODO : All these checks must be done in listeners
+ * TODO : includes and init files should be deleted
+ */
 include_once(__DIR__.'/../init/init.php');
 include_once(__DIR__.'/../init/init_menu.php');
 include_once(__DIR__.'/../init/init_templates.php');
@@ -14,12 +24,6 @@ include_once(__DIR__ . '/../init/common.php');
 
 if (!empty($_SESSION['login_id'])) {
     require_once(__DIR__.'/include/cron.php');
-}
-
-if ($_SERVER['APP_DEBUG']) {
-    #umask(0000);
-
-    #Debug::enable();
 }
 
 $request = Request::createFromGlobals();
@@ -41,16 +45,20 @@ if (!empty($_SESSION['login_id']) && $path == '/login') {
     header("Location: {$config['URL']}");
     exit();
 }
+/**
+ * End of Planno additions
+ */
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
 }
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
