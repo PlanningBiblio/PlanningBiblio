822598da392e55e7c42e8f7764d4a62381052d4a
Jérôme Combes <jerome@planningbiblio.fr>
2023-09-21 15:46:24 +0200

    MT32603: Upgrade symfony from version 4.4 to 5.4
    - Remove unnecessary usePutenv call from tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ if (!file_exists(__DIR__ . "/../.env.test.local")) {
     die("Unable to find the .env.test.local file\n");
 }
 
-(new Dotenv())->usePutenv(false)->load(__DIR__ . "/../.env.test.local");
+(new Dotenv())->load(__DIR__ . "/../.env.test.local");
 $database_url = $_ENV['DATABASE_URL'];
 
 $pattern = '/.[^\/]*\/\/(.[^:]*):(.[^@]*)@(.[^:]*):(\d*)\/(.*)/';
