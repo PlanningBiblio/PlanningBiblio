98fb0fac0d97cc355f52a5d3dc6291650911d112
Matthias Meusburger <matthias.meusburger@biblibre.com>
2023-10-27 15:33:53 +0200

    Release v22.10.12

--- public/router.php
+++ public/router.php
@@ -1,6 +1,6 @@
 <?php
 
-    $version = '22.10.11.000';
+    $version = '22.10.12.000';
 
     $file = __DIR__ . '/../public' . $_SERVER['REQUEST_URI'];
     $file = str_replace("?version=$version", '', $file);
