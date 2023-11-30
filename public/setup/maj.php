19802a7695c94074cfa9efdfdd54b4368454c63f
Matthias Meusburger <matthias.meusburger@biblibre.com>
2023-11-30 09:21:25 +0100

    Release v22.10.13

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3190,6 +3190,12 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+
+$v="22.10.13";
+if (version_compare($config['Version'], $v) === -1) {
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
