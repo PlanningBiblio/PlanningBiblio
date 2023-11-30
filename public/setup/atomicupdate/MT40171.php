0a9c75fb0fd840dcf773fea450ec7920159311d0
Matthias Meusburger <matthias.meusburger@biblibre.com>
2023-11-30 11:40:56 +0100

    Release v23.10.02

--- public/setup/atomicupdate/MT40171.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql[] = "UPDATE `{$dbprefix}config` SET `valeur` = 'no-reply@planno.fr' WHERE `valeur` = 'no-reply@planningbiblio.fr';";
-
-$sql[] = "UPDATE `{$dbprefix}config` SET `valeur` = 'Ce message a été envoyé par Planno.\nMerci de ne pas y répondre.' WHERE `valeur` = 'Ce message a été envoyé par Planning Biblio.\nMerci de ne pas y répondre.';";
-
-$sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'URL de Planno. A renseigner seulement si la redirection ne fonctionne pas après authentification sur le serveur CAS, si vous utilisez un Reverse Proxy par exemple.' WHERE `nom` = 'CAS-ServiceURL';";
-
-$sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Champ Planno à utiliser pour mapper les agents.' WHERE `nom` = 'Hamac-id';";
--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3224,6 +3224,18 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+
+$v="23.10.02.000";
+
+if (version_compare($config['Version'], $v) === -1) {
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur` = 'no-reply@planno.fr' WHERE `valeur` = 'no-reply@planningbiblio.fr';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur` = 'Ce message a été envoyé par Planno.\nMerci de ne pas y répondre.' WHERE `valeur` = 'Ce message a été envoyé par Planning Biblio.\nMerci de ne pas y répondre.';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'URL de Planno. A renseigner seulement si la redirection ne fonctionne pas après authentification sur le serveur CAS, si vous utilisez un Reverse Proxy par exemple.' WHERE `nom` = 'CAS-ServiceURL';";
+    $sql[] = "UPDATE `{$dbprefix}config` SET `commentaires` = 'Champ Planno à utiliser pour mapper les agents.' WHERE `nom` = 'Hamac-id';";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
