296ecb8339e63cd19e8dd2d39dbfd029fc8e2be0
Matthias Meusburger <matthias.meusburger@biblibre.com>
2023-12-06 16:53:59 +0100

    MT42875: Week number in week cycle should not be reset on new year
    Add migration script: tools/update_dateDebutPl.php

--- public/include/function.php
+++ public/include/function.php
@@ -113,7 +113,7 @@ class datePl
         return $this->getCycleNumber($interval, $nb_semaine);
     }
 
-    private function getCycleNumber($weeknumber, $cycles) {
+    public function getCycleNumber($weeknumber, $cycles) {
         $weekcycle = 0;
 
         for ($i = 1; $i <= $weeknumber; $i++) {
@@ -126,7 +126,7 @@ class datePl
         return $weekcycle;
     }
 
-    private function getNumberOfWeeksSinceStartDate($date) {
+    public function getNumberOfWeeksSinceStartDate($date) {
         $position=date("w", strtotime(dateSQL($GLOBALS['config']['dateDebutPlHebdo'])))-1;
         $position=$position==-1?6:$position;
         $dateFrom=new dateTime(dateSQL($GLOBALS['config']['dateDebutPlHebdo']));
