dd2a4816eb678bc5958d1f9fc675820674c1e229
Jérôme Combes <jerome@planningbiblio.fr>
2024-01-24 09:30:25 +0100

    Release v23.04.08

--- init/init.php
+++ init/init.php
@@ -10,7 +10,7 @@ $_SESSION['login_prenom'] = isset($_SESSION['login_prenom']) ? $_SESSION['login_
 $_SESSION['oups']['Auth-Mode'] = isset($_SESSION['oups']['Auth-Mode']) ? $_SESSION['oups']['Auth-Mode'] : '';
 
 // Version
-$version="23.04.07.000"; // xx.xx.xx.xxx
+$version="23.04.08.000"; // xx.xx.xx.xxx
 $GLOBALS['version'] = $version;
 $displayed_version = preg_replace('/(\d*\.\d*\.\d*).*/','\1', $version);
 
--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3273,6 +3273,13 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+ 
+$v="23.04.08.000";
+
+if (version_compare($config['Version'], $v) === -1) {
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
