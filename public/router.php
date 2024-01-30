78f531067cfaff1c5b823ea173d1a0f7264eabc2
Jérôme Combes <jerome@planningbiblio.fr>
2024-01-30 19:25:15 +0100

    MT42880: New release format (23.04.xx)

--- public/router.php
+++ public/router.php
@@ -1,6 +1,6 @@
 <?php
 
-    $version = '23.04.09.000';
+    $version = '23.04.09';
 
     $file = __DIR__ . '/../public' . $_SERVER['REQUEST_URI'];
     $file = str_replace("?version=$version", '', $file);
