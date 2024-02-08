52ec6b05ea6a892e4f59dcabf5c36fef01cbbef9
Jérôme Combes <jerome@planningbiblio.fr>
2024-02-08 11:25:12 +0100

    MT40653: Right copy does not return to top of page, follow up

--- public/planning/poste/js/planning.js
+++ public/planning/poste/js/planning.js
@@ -1440,7 +1440,7 @@ function bataille_navale(poste,date,debut,fin,perso_id,barrer,ajouter,site,tout,
 
       // Ajout du widget pour copier les agents dans
       // la cellule immédiatement à droite.
-      $("#td"+cellule).append('<a class="pl-icon arrow-right"></a>');
+      $("#td"+cellule).append('<a class="pl-icon arrow-right" role="button"></a>');
 
       // Suppresion de la surbrillance sur toutes les cellules une fois l'agent posté ou supprimé
       $('.pl-highlight').removeClass('pl-highlight', {duration:2500});
