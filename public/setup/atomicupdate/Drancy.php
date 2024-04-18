f9adf0273fc380636af33561f483fc582514af9d
Jérôme Combes <jerome@planningbiblio.fr>
2024-04-18 11:50:55 +0200

    MT44188: Choose the Office 365 calendars to import
    - Allow changes on the personnel table from atomic update

--- public/setup/atomicupdate/Drancy.php
+++ public/setup/atomicupdate/Drancy.php
@@ -1,3 +1,3 @@
 <?php
 
-$sql[] = "ALTER TABLE `{$dbprefix}personnel` ADD `check_ms_graph` TINYINT(1) NOT NULL DEFAULT 0 AFTER `check_hamac`;";
+$sql[] = "ALTER TABLE `{$dbprefix}personnel` ADD COLUMN IF NOT EXISTS `check_ms_graph` TINYINT(1) NOT NULL DEFAULT 0 AFTER `check_hamac`;";
