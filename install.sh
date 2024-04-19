3ad13862feb91fea37949fd81fd292aa47ef912d
Jérôme Combes <jerome@planningbiblio.fr>
2024-04-19 17:00:49 +0200

    MT44207: update install.sh
    - Rename Planning Biblio to Planno
    - Rename variables
    - Use a new dataset
    - Remove init/init.php call (database is now updated by composer install)
    
    TEST PLAN
    IMPORTANT: You need to know credentials of a DB User who has the rights to create databases and users.
    - Rename .env.local to .env.local.bak (or remove .env.local)
    - Run ./install.sh and follow the instructions
    -- Try with default values when possible
    -- Try changing the values
    - Connect to the Planno instance and check that it is working correctly

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-version='21.04.00.000'
+version='22.04.00'
 
 # Check PHP version
-phpversion=$(php -r "echo version_compare(phpversion(), '7.4');");
-if [[ $phpversion -lt 0 ]]; then
-    echo "You need PHP version 7.4 or greater to install Planning Biblio";
+phpVersion=$(php -r "echo version_compare(phpVersion(), '8.1');");
+if [[ $phpVersion -lt 0 ]]; then
+    echo "You need PHP version 8.1 or greater to install Planno";
     exit
 fi
 
@@ -13,12 +13,12 @@ fi
 if [[ -f .env.local ]]; then
     echo "The file .env.local exists.";
     echo "If you want to start a new installation, remove the file .env.local then run ./install.sh";
-    echo "If you want to update your current installation, open Planning Biblio in your web browser.";
+    echo "If you want to update your current installation, open Planno in your web browser.";
     exit
 fi
 
 # Prompt user for information
-echo "You are about to install Planning Biblio";
+echo "You are about to install Planno";
 echo "You will be asking for information to create the database.";
 echo "A database and a user will be created with given information. WARNING: if the database and the user already exist, they will be overwritten";
 echo "We strongly recommend that you back up your databases before starting this installation";
@@ -32,17 +32,17 @@ if [[ $proceed != 'yes'  && $proceed != 'y' ]]; then
 fi
 
 # Generate default passwords
-planningbdbpass_default=$(head /dev/urandom|tr -dc "a-zA-Z0-9"|fold -w 10|head -n 1)
-planningbadminpass_default=$(head /dev/urandom|tr -dc "a-zA-Z0-9"|fold -w 10|head -n 1)
+defaultDBPass=$(head /dev/urandom|tr -dc "a-zA-Z0-9"|fold -w 10|head -n 1)
+defaultAdminPass=$(head /dev/urandom|tr -dc "a-zA-Z0-9"|fold -w 10|head -n 1)
 
 echo "DB Host [localhost] :"
-read planningdbhost
+read dBHost
 
 echo "DB Port [3306] :"
-read planningdbport
+read dBPort
 
 echo "DB Admin User [root] :"
-read dbroot
+read dBRoot
 
 echo "DB Admin Pass :"
 prompt=''
@@ -53,14 +53,14 @@ do
         break
     fi
     prompt='*'
-    dbpass+="$char"
+    dBRootPass+="$char"
 done
 
 echo ""
-echo "DB User [planningbiblio] (will be overwritten if exists) :"
-read planningbdbuser
+echo "DB User [planno] (will be overwritten if exists) :"
+read dBUser
 
-echo "DB Pass [$planningbdbpass_default] (will be overwritten if exists) :"
+echo "DB Pass [$defaultDBPass] (will be overwritten if exists) :"
 prompt=''
 while IFS= read -p "$prompt" -r -s -n 1 char
 do
@@ -69,27 +69,23 @@ do
         break
     fi
     prompt='*'
-    planningbdbpass+="$char"
+    dBPass+="$char"
 done
 
 echo ""
-echo "DB Name [planningbiblio] (will be overwritten if exists) :"
-read planningbdbname
+echo "DB Name [planno] (will be overwritten if exists) :"
+read dBName
 
-# DB Prefix : does not work by loading the sql file
-# echo "DB Prefix [] (optional) :"
-# read planningbdbprefix
+echo "Planno admin's lastname [admin] :"
+read adminLastname
 
-echo "Planning Biblio admin's lastname [admin] :"
-read planningbadminlastname
+echo "Planno admin's firstname [admin] :"
+read adminFirstname
 
-echo "Planning Biblio admin's firstname [admin] :"
-read planningbadminfirstname
+echo "Planno admin's e-mail address [admin@example.com] :"
+read adminEmail
 
-echo "Planning Biblio admin's e-mail address [admin@example.com] :"
-read planningbadminemail
-
-echo "Planning Biblio admin's password [$planningbadminpass_default] :"
+echo "Planno admin's password [$defaultAdminPass] :"
 prompt=''
 while IFS= read -p "$prompt" -r -s -n 1 char
 do
@@ -98,82 +94,82 @@ do
         break
     fi
     prompt='*'
-    planningbadminpass+="$char"
+    adminPass+="$char"
 done
 
 echo ""
 echo "Update composer dependencies [no] :"
-read updatecomposer
+read updateComposer
 
 # Set defaults
-if [[ $planningdbhost = '' ]]; then
-    planningdbhost='localhost'
+if [[ $dBHost = '' ]]; then
+    dBHost='localhost'
 fi
 
-if [[ $planningdbport = '' ]]; then
-    planningdbport='3306'
+if [[ $dBPort = '' ]]; then
+    dBPort='3306'
 fi
 
-if [[ $dbroot = '' ]]; then
-    dbroot='root'
+if [[ $dBRoot = '' ]]; then
+    dBRoot='root'
 fi
 
-if [[ $planningbdbuser = '' ]]; then
-    planningbdbuser='planningbiblio'
+if [[ $dBUser = '' ]]; then
+    dBUser='planno'
 fi
 
-if [[ $planningbdbpass = '' ]]; then
-    planningbdbpass=$planningbdbpass_default
+if [[ $dBPass = '' ]]; then
+    dBPass=$defaultDBPass
 fi
 
-if [[ $planningbdbname = '' ]]; then
-    planningbdbname='planningbiblio'
+if [[ $dBName = '' ]]; then
+    dBName='planno'
 fi
 
-if [[ $planningbadminlastname = '' ]]; then
-    planningbadminlastname='admin'
+if [[ $adminLastname = '' ]]; then
+    adminLastname='admin'
 fi
 
-if [[ $planningbadminfirstname = '' ]]; then
-    planningbadminfirstname='admin'
+if [[ $adminFirstname = '' ]]; then
+    adminFirstname='admin'
 fi
 
-if [[ $planningbadminemail = '' ]]; then
-    planningbadminemail='admin@example.com'
+if [[ $adminEmail = '' ]]; then
+    adminEmail='admin@example.com'
 fi
 
-if [[ $planningbadminpass = '' ]]; then
-    planningbadminpass=$planningbadminpass_default
+if [[ $adminPass = '' ]]; then
+    adminPass=$defaultAdminPass
 fi
 
-updatecomposer=$(echo $updatecomposer | tr '[:upper:]' '[:lower:]');
-if [[ $updatecomposer = '' ]]; then
-    updatecomposer='no'
+updateComposer=$(echo $updateComposer | tr '[:upper:]' '[:lower:]');
+if [[ $updateComposer = '' ]]; then
+    updateComposer='no'
 fi
 
 
-if [[ $planningdbhost = 'localhost' ]] || [[ $planningdbhost = '127.0.0.1' ]]; then
-    planningdbuserhost='localhost'
+if [[ $dBHost = 'localhost' ]] || [[ $dBHost = '127.0.0.1' ]]; then
+    dBUserhost='localhost'
 else
-    planningdbuserhost='%'
+    dBUserhost='%'
 fi
 
 # Set variables
-planningbdatas=data/planningb_2104.sql.gz
-planningbsecret=$(head /dev/urandom|tr -dc "a-f0-9"|fold -w 32|head -n 1)
+data=data/planno_24.04.xx.sql.gz
+secret=$(head /dev/urandom|tr -dc "a-f0-9"|fold -w 32|head -n 1)
 
 # Create the database
-mysql --defaults-file=/dev/null -h $planningdbhost -u $dbroot --password=$dbpass -e "DROP USER IF EXISTS '$planningbdbuser'@'$planningdbuserhost';"
-mysql --defaults-file=/dev/null -h $planningdbhost -u $dbroot --password=$dbpass -e "DROP DATABASE IF EXISTS $planningbdbname;"
+mysql --defaults-file=/dev/null -h $dBHost -u $dBRoot --password=$dBRootPass -e "DROP USER IF EXISTS '$dBUser'@'$dBUserhost';"
+mysql --defaults-file=/dev/null -h $dBHost -u $dBRoot --password=$dBRootPass -e "DROP DATABASE IF EXISTS $dBName;"
 
-mysql --defaults-file=/dev/null -h $planningdbhost -u $dbroot --password=$dbpass -e "CREATE DATABASE IF NOT EXISTS $planningbdbname CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;"
+mysql --defaults-file=/dev/null -h $dBHost -u $dBRoot --password=$dBRootPass -e "CREATE DATABASE IF NOT EXISTS $dBName CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;"
 
-mysql --defaults-file=/dev/null -h $planningdbhost -u $dbroot --password=$dbpass -e "CREATE USER '$planningbdbuser'@'$planningdbuserhost' IDENTIFIED BY '$planningbdbpass';"
-mysql --defaults-file=/dev/null -h $planningdbhost -u $dbroot --password=$dbpass -e "GRANT ALL PRIVILEGES ON $planningbdbname.* TO '$planningbdbuser'@'$planningdbuserhost' IDENTIFIED BY '$planningbdbpass'"
+mysql --defaults-file=/dev/null -h $dBHost -u $dBRoot --password=$dBRootPass -e "CREATE USER '$dBUser'@'$dBUserhost' IDENTIFIED BY '$dBPass';"
+mysql --defaults-file=/dev/null -h $dBHost -u $dBRoot --password=$dBRootPass -e "GRANT ALL PRIVILEGES ON $dBName.* TO '$dBUser'@'$dBUserhost' IDENTIFIED BY '$dBPass'"
 
-mysql --defaults-file=/dev/null -h $planningdbhost -u $dbroot --password=$dbpass -e "FLUSH PRIVILEGES"
-zcat $planningbdatas | mysql -h $planningdbhost -u $dbroot --password=$dbpass $planningbdbname
-mysql --defaults-file=/dev/null -h $planningdbhost -u $planningbdbuser --password=$planningbdbpass -e "UPDATE $planningbdbname.\`personnel\` SET \`nom\`='$planningbadminlastname', \`prenom\`='$planningbadminfirstname', \`mail\`='$planningbadminemail', \`password\`=MD5('$planningbadminpass') WHERE \`id\` = 1;"
+mysql --defaults-file=/dev/null -h $dBHost -u $dBRoot --password=$dBRootPass -e "FLUSH PRIVILEGES"
+zcat $data | mysql -h $dBHost -u $dBRoot --password=$dBRootPass $dBName
+mysql --defaults-file=/dev/null -h $dBHost -u $dBUser --password=$dBPass -e "UPDATE $dBName.\`personnel\` SET \`nom\`='$adminLastname', \`prenom\`='$adminFirstname', \`mail\`='$adminEmail', \`password\`=MD5('$adminPass') WHERE \`id\` = 1;"
 
 if [[ $? -ne 0 ]]; then
     exit;
@@ -181,14 +177,13 @@ fi
 
 # Create the .env.local file
 cp .env .env.local
-sed -i "s/APP_SECRET=.*/APP_DEBUG=0\nAPP_SECRET=${planningbsecret}/" .env.local
+sed -i "s/APP_SECRET=.*/APP_DEBUG=0\nAPP_SECRET=${secret}/" .env.local
 
-sed -i "s/DATABASE_URL=.*/DATABASE_URL=mysql:\/\/$planningbdbuser:$planningbdbpass@$planningdbhost:$planningdbport\/$planningbdbname/" .env.local
-sed -i "s/DATABASE_PREFIX=.*/DATABASE_PREFIX=$planningbdbprefix/" .env.local
+sed -i "s/DATABASE_URL=.*/DATABASE_URL=mysql:\/\/$dBUser:$dBPass@$dBHost:$dBPort\/$dBName/" .env.local
 sed -i "s/APP_ENV=dev/APP_ENV=prod/g" .env.local
 
 # Set the default theme
-mysql -h $planningdbhost -u $planningbdbuser --password=$planningbdbpass -e "UPDATE $planningbdbname.\`config\` SET \`valeur\` = 'default' WHERE \`nom\` = 'Affichage-theme';"
+mysql -h $dBHost -u $dBUser --password=$dBPass -e "UPDATE $dBName.\`config\` SET \`valeur\` = 'default' WHERE \`nom\` = 'Affichage-theme';"
 
 # Download composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -196,7 +191,7 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 # Update dependencies ?
-if [[ $updatecomposer = 'yes' && -f composer.lock ]]; then
+if [[ $updateComposer = 'yes' && -f composer.lock ]]; then
     echo "Removing composer.lock"
     rm composer.lock
     if [[ -d vendor ]];then
@@ -226,13 +221,7 @@ fi
 # Remove composer.phar
 php -r "unlink('composer.phar');"
 
-# Run database update
-grep "\$version=\"$version\";" init/init.php
-if [[ $? != 0 ]]; then
-    php -f init/init.php
-fi
-
 echo ""
 echo -e "One more step, run : \e[1m\033[32msudo chmod -R 777 var\e[0m";
-echo "Then, the installation will be completed and you will be able to use Planning Biblio in your web browser with these cretentials : admin / $planningbadminpass";
+echo "Then, the installation will be completed and you will be able to use Planno in your web browser with these cretentials : admin / $adminPass";
 echo ""
