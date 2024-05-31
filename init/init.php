4eb943d0e70a8b49fba1974d2b3bd0afd65166ba
Jérôme Combes <jerome@planningbiblio.fr>
2024-05-31 12:08:12 +0200

    Release v23.10.13

--- init/init.php
+++ init/init.php
@@ -16,7 +16,7 @@ $_SESSION['login_prenom'] = isset($_SESSION['login_prenom']) ? $_SESSION['login_
 $_SESSION['oups']['Auth-Mode'] = isset($_SESSION['oups']['Auth-Mode']) ? $_SESSION['oups']['Auth-Mode'] : '';
 
 // Version
-$version="23.10.12"; // xx.xx.xx
+$version="23.10.13"; // xx.xx.xx
 $GLOBALS['version'] = $version;
 
 // Redirection vers setup si le fichier config est absent
--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3406,6 +3406,20 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+ 
+$v="23.10.13";
+
+if (version_compare($config['Version'], $v) === -1) {
+
+    // MT44188
+    $sql[] = "ALTER TABLE `{$dbprefix}personnel` ADD COLUMN IF NOT EXISTS `check_ms_graph` TINYINT(1) NOT NULL DEFAULT 0 AFTER `check_hamac`;";
+
+    if (!empty($_ENV['MS_GRAPH_CLIENT_ID'])) {
+        $sql[] = "UPDATE `{$dbprefix}personnel` SET `check_ms_graph` = 1 WHERE `supprime` = '0';";
+    }
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
