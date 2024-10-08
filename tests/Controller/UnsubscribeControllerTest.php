dcbc99e88d4ffea5a8b47e05463919cca3b23679
Matthias Meusburger <matthias.meusburger@biblibre.com>
2024-10-08 12:15:20 +0200

    MT43946: Add unsubscribe link in Planno's emails headers.
     - Add token processing
    
    Note: symfony does not like urlencoded route parameters. Since every character
    is allowed in a route parameter, except for slash, we add it to the list of
    allowed characters, using:
    
    requirements: ['token' => '.+']
    
    See https://symfony.com/doc/current/routing.html#slash-characters-in-route-parameters
    
    Test plan update:
    
    To test interactive unsubscription:
    
    1) comment:
    $mail->addCustomHeader("List-Unsubscribe-Post", "List-Unsubscribe=One-Click");
    in public/include/function.php
    
    2) Send a test email from Planno (Administration > Configuration > Messagerie)
    
    3) In Thunderbird, you should see an "Unsubscribe" button.
    
    4) Click on it, then Unsubscribe. The addon will open the unsubscribe page,
    which will display your email address with an unsubscribe button.
    
    Uninteractive unsubscription test plan remains unchanged.

--- tests/Controller/UnsubscribeControllerTest.php
+++ tests/Controller/UnsubscribeControllerTest.php
@@ -26,19 +26,37 @@ class UnsubscribeControllerTest extends PLBWebTestCase
         $client = static::createClient();
         $builder = new FixtureBuilder();
         $builder->delete(Agent::class);
+        $email = 'johndoe@example.org';
+        $token = encrypt($email);
 
-        $crawler = $client->request('GET', '/unsubscribe');
+        $crawler = $client->request('GET', "/unsubscribe/$token");
 
         $result = $crawler->filterXPath('//h3');
         $this->assertEquals($result->text(null,false),'Désinscription','h3 is Désinscription unlogged');
+        $result = $crawler->filterXPath('//p[@id="unsubscribe-text"]');
+        $this->assertEquals($result->text(null,false),'Désinscrire johndoe@example.org?','mail is retrieved unlogged');
+
+        $crawler = $client->request('OPTIONS', "/unsubscribe/$token");
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200, 'OPTIONS returns 200 unlogged');
+
+        $crawler = $client->request('POST', "/unsubscribe/$token");
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200, 'POST returns 200 unlogged');
 
         $agent = $builder->build(Agent::class, array('login' => 'jdevoe'));
         $this->logInAgent($agent, array(99,100));
 
-        $crawler = $client->request('GET', '/unsubscribe');
+        $crawler = $client->request('GET', "/unsubscribe/$token");
 
         $result = $crawler->filterXPath('//h3');
         $this->assertEquals($result->text(null,false),'Désinscription','h3 is Désinscription logged-in');
+        $result = $crawler->filterXPath('//p[@id="unsubscribe-text"]');
+        $this->assertEquals($result->text(null,false),'Désinscrire johndoe@example.org?','mail is retrieved logged-in');
+
+        $crawler = $client->request('OPTIONS', "/unsubscribe/$token");
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200, 'OPTIONS returns 200 logged-in');
+
+        $crawler = $client->request('POST', "/unsubscribe/$token");
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200, 'POST returns 200 logged-in');
 
     }
 }
