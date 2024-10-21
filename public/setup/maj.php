ff2fce250557a00b6629878f117602e423ebf935
Jérôme Combes <jerome@planningbiblio.fr>
2024-10-21 15:23:33 +0200

    Release v24.04.07

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3480,6 +3480,13 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+ 
+$v="24.04.07";
+
+if (version_compare($config['Version'], $v) === -1) {
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
