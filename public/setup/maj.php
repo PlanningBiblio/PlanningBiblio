1760a36d718c088c3cff08f87d0a81772a7d5d6e
Jérôme Combes <jerome@planningbiblio.fr>
2024-10-27 17:08:36 +0100

    Release v23.10.19

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3459,6 +3459,13 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+ 
+$v="23.10.19";
+
+if (version_compare($config['Version'], $v) === -1) {
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
