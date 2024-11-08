5c6b33c45e8ac1423893e219eb5d8666d11fb5c8
Jérôme Combes <jerome@planningbiblio.fr>
2024-11-08 12:26:04 +0100

    MT45129: Enhance routes to plannings

--- tests/Controller/PlanningControllerTest.php
+++ tests/Controller/PlanningControllerTest.php
@@ -19,8 +19,6 @@ class PlanningControllerTest extends PLBWebTestCase
         $this->builder->delete(PlanningPosition::class);
         $this->builder->delete(PlanningPositionTabAffectation::class);
 
-        $client = static::createClient();
-
         $agent = $this->builder->build(
             Agent::class,
             array(
@@ -30,14 +28,12 @@ class PlanningControllerTest extends PLBWebTestCase
 
         $this->logInAgent($agent, array(99,100));
 
-        $crawler = $this->client->request('GET', '/');
-
-        $result = $crawler->filter('.decalage-gauche p');
-        $date = date('d/m/Y');
-        $this->assertEquals("Le planning du $date n'est pas prêt.", $result->text('Node does not exist', false));
-
         $date = new \DateTime();
-        $today = $date->format('d/m/Y');
+        $today = date('d/m/Y');
+
+        $crawler = $this->client->request('GET', '/');
+        $result = $crawler->filter('.decalage-gauche p');
+        $this->assertEquals("Le planning du $today n'est pas prêt.", $result->text('Node does not exist', false));
 
         $pl_post_lock = $this->builder->build
         (
@@ -62,10 +58,7 @@ class PlanningControllerTest extends PLBWebTestCase
             )
         );
 
-        $this->logInAgent($agent, array(99,100));
-
-        $crawler = $client->request('GET', '/');
-
+        $crawler = $this->client->request('GET', '/');
         $result = $crawler->filter('.decalage-gauche p');
         $this->assertEquals("Le planning du $today n'est pas validé !", $result->text('Node does not exist', false), 'test index with no lock planning');
     }
