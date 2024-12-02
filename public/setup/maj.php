20f5f75eded5ee17ad8ba79741ba05b9b50735b6
Jérôme Combes <jerome@planningbiblio.fr>
2024-12-02 14:35:23 +0100

    Release v24.10.02

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3517,6 +3517,15 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+ 
+$v="24.10.02";
+
+if (version_compare($config['Version'], $v) === -1) {
+    // MT46229
+    $sql[] = "INSERT IGNORE INTO `{$dbprefix}config` (`nom`, `type`, `valeurs`, `valeur`, `categorie`, `ordre`, `commentaires`) VALUES ('Absences-Exclusion', 'enum2', '[[0, \"Les agents ayant une absence validée sont exclus des plannings.\"],[1,\"Les agents ayant des absences importées validées peuvent être ajoutés au planning.\"],[2,\"Les agents ayant des absences validées, importées ou non, peuvent être ajoutés au planning.\"]]', '0', 'Absences','160', 'Autoriser l\'affectation au planning des agents absents.');";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
