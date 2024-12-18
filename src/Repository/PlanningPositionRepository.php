582b4f0cc1cfd4ccffda0c08ab03ac9aa6b3d7f4
Jérôme Combes <jerome@planningbiblio.fr>
2024-12-18 14:32:55 +0100

    MT46717: Enhance Statistics by Position
    Enhance the query used to get used positions by adding "group by"
    There are now fewer results to analyze in the loops and the response time is better

---
+++ src/Repository/PlanningPositionRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\PlanningPosition;
+use Doctrine\ORM\EntityRepository;
+
+class PlanningPositionRepository extends EntityRepository
+{
+
+    public function getPositions()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select(array('p'))
+                ->from(PlanningPosition::class, 'p')
+                ->addGroupBy('p.poste');
+
+        $query = $qb->getQuery();
+
+        return $query->getResult();
+    }
+}
