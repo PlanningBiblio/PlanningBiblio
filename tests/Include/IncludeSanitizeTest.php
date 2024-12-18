84dae52165f2f0641c16c6b69accb89fa26fcb5a
Matthias Meusburger <matthias.meusburger@biblibre.com>
2024-12-18 15:29:49 +0100

    MT44184: use symfony/html-sanitizer instead of htmlpurifier
    Test plan:
    
     1) Edit a planning comment and check that the input in sanitized.
     2) Save a planning as model and check that the model name is sanitized.
     3) prove tests/Include/IncludeSanitizeTest.php
    
    Note:
     htmlpurifier used to html-encode PHP code, html-sanitizer removes it.

--- tests/Include/IncludeSanitizeTest.php
+++ tests/Include/IncludeSanitizeTest.php
@@ -9,9 +9,9 @@ class IncludeSanitizeTest extends TestCase
     public function testSanitizeHtml()
     {
         $sanitized = sanitize_html("Pre Text <script>alert('payload');</script> Post Text");
-        $this->assertEquals($sanitized, 'Pre Text  Post Text', 'Remove javascript');
+        $this->assertEquals('Pre Text  Post Text', $sanitized, 'Remove javascript');
 
         $sanitized = sanitize_html("Pre Text <?php echo 'payload'; ?> Post Text");
-        $this->assertEquals($sanitized, "Pre Text &lt;?php echo 'payload'; ?&gt; Post Text", 'Sanitize PHP');
+        $this->assertEquals("Pre Text  Post Text", $sanitized, 'Remove PHP');
     }
 }
