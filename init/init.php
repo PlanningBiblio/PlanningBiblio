23a5d9869bf4b3669346c383bb8469b53bddbe58
Jérôme Combes <jerome@planningbiblio.fr>
2025-04-18 13:53:45 +0200

    Release v24.11.13

--- init/init.php
+++ init/init.php
@@ -17,7 +17,7 @@ $_SESSION['login_prenom'] = isset($_SESSION['login_prenom']) ? $_SESSION['login_
 $_SESSION['oups']['Auth-Mode'] = isset($_SESSION['oups']['Auth-Mode']) ? $_SESSION['oups']['Auth-Mode'] : '';
 
 // Version
-$version="24.11.12"; // xx.xx.xx
+$version="24.11.13"; // xx.xx.xx
 $GLOBALS['version'] = $version;
 
 // Redirection vers setup si le fichier config est absent
