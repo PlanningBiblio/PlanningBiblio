9337ff5c45af9fdd278ca28a5c228f7440fb8a30
Matthias Meusburger <matthias.meusburger@biblibre.com>
2025-06-04 16:34:33 +0200

    MT48100: Improve hours to days conversions
     - Add unit tests for string representation of decimalToHoursMinutes

--- tests/PlanningBiblio/Helper/HourHelper/HourHelperTest.php
+++ tests/PlanningBiblio/Helper/HourHelper/HourHelperTest.php
@@ -31,11 +31,13 @@ class HourHelperTest extends TestCase
         $result = $hh->decimalToHoursMinutes(0);
         $this->assertSame('0', $result['hours'], '0 gives 0 hours');
         $this->assertEquals(0, $result['minutes'], '0 gives 0 minutes');
+        $this->assertEquals('', $result['as_string'], '0 hours 0 minutes gives an empty string');
 
         # Negative duration within an hour gives a negative hour value
         $result = $hh->decimalToHoursMinutes(-0.25);
         $this->assertSame('-0', $result['hours'], '-0.25 gives -0 hours');
         $this->assertEquals(15, $result['minutes'], '-0.25 gives 15 minutes');
+        $this->assertEquals('-0h15', $result['as_string'], 'minus sign within an hour is kept in string representation');
 
         # In case we have values inbetween minutes in the database:
         # A value betweeen minutes goes to the closest minute
@@ -43,16 +45,19 @@ class HourHelperTest extends TestCase
         $result = $hh->decimalToHoursMinutes(8.9967);
         $this->assertSame('9', $result['hours'], '8.9967 gives 9 hours (a value betweeen minutes goes to the closest minute, positive test)');
         $this->assertEquals(00, $result['minutes'], '8.9967 gives 00 minutes (a value between minutes goes to the closest minute, positive test)');
+        $this->assertEquals('9h00', $result['as_string'], '0 minutes are shown as 00 in string representation');
 
         # Between -8h59 and -9h
         $result = $hh->decimalToHoursMinutes(-8.9967);
         $this->assertSame('-9', $result['hours'], '-8.9967 gives -9 hours (a value betweeen minutes goes to the closest minute, negative test)');
         $this->assertEquals(00, $result['minutes'], '-8.9967 gives 00 minutes (a value between minutes goes to the closest minute, negative test)');
+        $this->assertEquals('-9h00', $result['as_string'], 'minus sign is kept in string representation');
 
         # Between 8h00 and 8h01, closer to 01
         $result = $hh->decimalToHoursMinutes(8.015);
         $this->assertSame('8', $result['hours'], '8.015 gives 8 hours (a value betweeen minutes goes to the closest minute, closer to the next minute)');
         $this->assertEquals(01, $result['minutes'], '8.015 gives 01 minutes (a value between minutes goes to the closest minute, closer to the next minute)');
+        $this->assertEquals('8h01', $result['as_string'], 'minutes under 10 are zero left-padded');
 
         # Between 8h00 and 8h01, closer to 00
         $result = $hh->decimalToHoursMinutes(8.0015);
