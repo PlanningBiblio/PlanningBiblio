24064be12ee7fea75d2b7b400ff6c431bc6c0180
Jérôme Combes <jerome@planningbiblio.fr>
2025-07-23 09:07:08 +0200

    MT49520: adds buttons allowing rotation of working hours

--- public/js/plb/workinghour.js
+++ public/js/plb/workinghour.js
@@ -46,4 +46,39 @@ $(function(){
         updateTables(this.value);
     });
 
+    $('.workingHoursRotationBtn').click(function() {
+        const count = $('table[id^="tableau"]').length;
+        const offset = (count - 1) * 7;
+        const rotation = $(this).attr('data-rotation');
+
+        var times = [];
+        $('[name^="temps"]').each(function() {
+            const name = $(this).attr('name');
+            const indexes = name.match(/\d+/g);
+            const value = $(this).val();
+            const elem = [indexes[0], indexes[1], value];
+            times.push(elem);
+        });
+
+        for(elem in times) {
+            var index1 = parseInt(times[elem][0]);
+            const index2 = parseInt(times[elem][1]);
+            const value = times[elem][2];
+
+            if (rotation == 'clockwise') {
+                index1 = index1 >= offset ? (index1 - offset) : (index1 + 7);
+            } else {
+                index1 = index1 < 7 ? (index1 + offset) : (index1 - 7);
+            }
+
+            $('[name="temps[' + index1 + '][' + index2 + ']"]').val(value);
+        }
+
+        //$('[name^="temps"]').effect('highlight', null, 3000);
+    });
+
+//    $('.workingHoursRotationBtn').one('click', function() {
+//        $('[name^="temps"]').effect('highlight', null, 3000);
+//    });
+
 });
