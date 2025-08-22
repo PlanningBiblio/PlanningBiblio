f5b72a65598940203a911b203574b1bbdcf63848
Jérôme Combes <jerome@planningbiblio.fr>
2025-08-22 15:04:51 +0200

    Release v25.05.05

--- public/setup/maj.php
+++ public/setup/maj.php
@@ -3753,6 +3753,30 @@ if (version_compare($config['Version'], $v) === -1) {
 
     $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
 }
+ 
+$v="25.05.05";
+
+if (version_compare($config['Version'], $v) === -1) {
+    // MT45367: Migrate from annotations to php attributes
+    $sql[] = "ALTER TABLE `{$dbprefix}pl_poste` CHANGE `chgt_time` `chgt_time` DATETIME NULL DEFAULT NULL;";
+    $sql[] = "ALTER TABLE `{$dbprefix}postes` CHANGE `bloquant` `bloquant` TINYINT(1) NULL DEFAULT '1';";
+    $sql[] = "ALTER TABLE `{$dbprefix}postes` CHANGE `statistiques` `statistiques` TINYINT(1) NULL DEFAULT 1;";
+    $sql[] = "ALTER TABLE `{$dbprefix}postes` CHANGE `teleworking` `teleworking` TINYINT(1) NOT NULL DEFAULT 0;";
+
+    $sql[] = "UPDATE `{$dbprefix}postes` SET `bloquant` = 0 WHERE `bloquant` = 1;";
+    $sql[] = "UPDATE `{$dbprefix}postes` SET `statistiques` = 0 WHERE `statistiques` = 1;";
+    $sql[] = "UPDATE `{$dbprefix}postes` SET `teleworking` = 0 WHERE `teleworking` = 1;";
+
+    $sql[] = "UPDATE `{$dbprefix}postes` SET `bloquant` = 1 WHERE `bloquant` > 1;";
+    $sql[] = "UPDATE `{$dbprefix}postes` SET `statistiques` = 1 WHERE `statistiques` > 1;";
+    $sql[] = "UPDATE `{$dbprefix}postes` SET `teleworking` = 1 WHERE `teleworking` > 1;";
+
+    $sql[] = "ALTER TABLE `{$dbprefix}postes` CHANGE `bloquant` `bloquant` TINYINT(1) NOT NULL DEFAULT 1;";
+    $sql[] = "ALTER TABLE `{$dbprefix}postes` CHANGE `statistiques` `statistiques` TINYINT(1) NOT NULL DEFAULT 1;";
+    $sql[] = "ALTER TABLE `{$dbprefix}postes` CHANGE `teleworking` `teleworking` TINYINT(1) NOT NULL DEFAULT 0;";
+
+    $sql[] = "UPDATE `{$dbprefix}config` SET `valeur`='$v' WHERE `nom`='Version';";
+}
 # MARKER
 
 //	Execution des requetes et affichage
