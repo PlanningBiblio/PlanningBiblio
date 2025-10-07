f40edf54a1e555cd873cd2b592e356cc014a7fec
Jérôme Combes <jerome@planningbiblio.fr>
2025-10-07 13:02:54 +0200

    MT50258: Add and use ConfigRepository

--- src/Entity/Config.php
+++ src/Entity/Config.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
+use App\Repository\ConfigRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: ConfigRepository::class)]
 #[ORM\Table(name: 'config')]
 class Config
 {
