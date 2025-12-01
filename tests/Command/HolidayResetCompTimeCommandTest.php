cc4604f908c585b10af80e24e35277f0b5b35e5f
Jerome Combes <jerome@planningbiblio.fr>
2025-12-01 18:00:47 +0100

    MT50832: holiday_reset_comp_time_command, Follow up

--- tests/Command/HolidayResetCompTimeCommandTest.php
+++ tests/Command/HolidayResetCompTimeCommandTest.php
@@ -1,5 +1,5 @@
 <?php
-//fini
+
 namespace App\Tests\Command;
 
 use App\Entity\Agent;
@@ -7,34 +7,51 @@ use Tests\PLBWebTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Output\OutputInterface;
+
 class HolidayResetCompTimeCommandTest extends PLBWebTestCase
 {
 
     public function testSomething(): void
     {
-        $jdupont = $this->builder->build(Agent::class, array(
-            'login' => 'jduponttt', 'nom' => 'Duponttt', 'prenom' => 'Jean', 'temps'=>'',
-            'droits' => array(3,4,5,6,9,17,20,21,22,23,25,99,100,201,202,301,302,401,402,501,502,601,602,701,801,802,901,1001,1002,1101,1201,1301),
-            'sites' => '["1"]',
-            'conges_credit' => "3.8",
-            'conges_reliquat' => "3.8",
-            'conges_anticipation' => "0",
-            'comp_time' => "1.9",
-            'conges_annuel' => "3.8",
-        ));
+        $jdoe = $this->builder->build(Agent::class, [
+            'login' => 'jdoe',
+            'comp_time' => '120',
+        ]);
+
+        $jdupont = $this->builder->build(Agent::class, [
+            'login' => 'jdupont',
+            'comp_time' => '1.9',
+        ]);
+
+        $kboivin = $this->builder->build(agent::class, [
+            'login' => 'kboivin',
+            'comp_time' => '21.89',
+        ]);
 
         $repo = $this->entityManager->getRepository(Agent::class);
-        $agent = $repo->findOneBy(['login' => 'jduponttt']);
-        $this->assertNotNull($agent, 'Agent should exist in database');
+
+        $agent = $repo->findOneBy(['login' => 'jdoe']);
+        $this->assertEquals(120, $agent->getCompTime(), 'comp_time should be 120');
+
+        $agent = $repo->findOneBy(['login' => 'jdupont']);
         $this->assertEquals(1.9, $agent->getCompTime(), 'comp_time should be 1.9');
 
+        $agent = $repo->findOneBy(['login' => 'kboivin']);
+        $this->assertEquals(21.89, $agent->getCompTime(), 'comp_time should be 21.89');
+
         $this->execute();
+
         $this->entityManager->clear();
-        
         $repo = $this->entityManager->getRepository(Agent::class);
-        $agentAfter = $repo->findOneBy(['login' => 'jduponttt']);
-        $this->assertNotNull($agentAfter, 'Agent should still exist after cron');
-        $this->assertEquals(0.0, $agentAfter->getCompTime(), 'After the command comp_time should be 0');
+
+        $agent = $repo->findOneBy(['login' => 'jdoe']);
+        $this->assertEquals(0, $agent->getCompTime(), 'After the command comp_time should be 0');
+        
+        $agent = $repo->findOneBy(['login' => 'jdupont']);
+        $this->assertEquals(0, $agent->getCompTime(), 'After the command comp_time should be 0');
+        
+        $agent = $repo->findOneBy(['login' => 'kboivin']);
+        $this->assertEquals(0, $agent->getCompTime(), 'After the command comp_time should be 0');
         
         $this->restore();
     }
@@ -50,7 +67,8 @@ class HolidayResetCompTimeCommandTest extends PLBWebTestCase
             'command' => $command->getName(),
             '--not-really' => true
         ]);
+
         $commandTester->assertCommandIsSuccessful();
     }
-    
 }
+
