f97a3a063ee3dea4fb9144456b2c736236185b60
Your Name <Your E-mail Address>
2025-12-19 16:20:13 +0100

    MT50832: Use EntityManager in Holiday Reset Commands

--- src/Command/HolidayResetCompTimeCommand.php
+++ src/Command/HolidayResetCompTimeCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use App\Entity\Agent;
+use App\Entity\Holiday;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -10,10 +11,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-
-require_once __DIR__ . '/../../legacy/Common/function.php';
-require_once(__DIR__ . '/../../legacy/Class/class.conges.php');
-require_once(__DIR__ . '/../../legacy/Class/class.personnel.php');
 
 #[AsCommand(
     name: 'app:holiday:reset:comp-time',
@@ -50,24 +47,16 @@ class HolidayResetCompTimeCommand extends Command
             }
         }
 
-        $CSRFToken = CSRFToken();
+        $agents = $this->entityManager->getRepository(Agent::class)->getByDeletionStatus([0,1]);
 
-        $p = new \personnel();
-        $p->supprime = array(0,1);
-        $p->fetch();
-        if ($p->elements) {
-            foreach ($p->elements as $elem) {
-                $credits=array();
-                $credits['comp_time'] = 0;
-                $credits['conges_credit'] = $elem['conges_credit'];
-                $credits['conges_anticipation'] = $elem['conges_anticipation'];
-                $credits['conges_reliquat'] = $elem['conges_reliquat'];
+        foreach ($agents as $elem) {
+            $credits = array();
+            $credits['comp_time'] = 0;
+            $credits['conges_credit'] = $elem->getHolidayCredit();
+            $credits['conges_anticipation'] = $elem->getHolidayAnticipation();
+            $credits['conges_reliquat'] = $elem->getHolidayRemainder();
 
-                $c = new \conges();
-                $c->perso_id=$elem['id'];
-                $c->CSRFToken = $CSRFToken;
-                $c->maj($credits, 'modif', true);
-            }
+            $this->entityManager->getRepository(Holiday::class)->insert($elem->getId(), $credits, 'update', true);
         }
 
         // Modifie les crédits
