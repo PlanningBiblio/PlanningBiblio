9aedf6bcbc2c9bd3e49b5cf63ac79d7cda0f8301
Your Name <Your E-mail Address>
2025-12-19 12:37:04 +0100

    MT50832: cron_tab_command_merge_ver2- block on test unitaire

--- tests/Command/CronTabCommandTest.php
+++ tests/Command/CronTabCommandTest.php
@@ -2,15 +2,30 @@
 
 namespace App\Tests\Command;
 
+use App\Entity\Agent;
 use Tests\PLBWebTestCase;
 
 class CronTabCommandTest extends PLBWebTestCase
 {
     public function testSomething(): void
     {
-        $client = static::createPantherClient();
-        $crawler = $client->request('GET', '/');
+        $this->setUpPantherClient();
 
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $agent = $this->builder->build(Agent::class);
+        $this->login($agent);
+
+        $dateStr = (new \DateTime())->format('d/m/Y');
+
+        $crawler = $this->client->request('GET', '/absence/add');
+        $form = $crawler->selectButton('Valider')->form(['start' => '19/12/2025']);
+        $token = $crawler->filter('input[name="debut"]')->attr($dateStr);
+
+        // Load a model
+        $recurrence = $crawler->filter('#recurrence-checkbox');
+        $this->assertTrue($recurrence->count() == 1, 'Récurrence check');
+        $recurrence->click();
+        $crawler = $this->client->refreshCrawler();
+
+
     }
 }
