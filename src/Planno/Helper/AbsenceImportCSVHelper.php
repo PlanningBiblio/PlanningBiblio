5f23ed4b6ac81b5c1935dd7200a82ec6f4161dbb
Matthias Meusburger <matthias.meusburger@biblibre.com>
2025-12-22 15:07:27 +0100

    MT48492: Import absences from CSV file
    Test plan:
    
     - DB Upgrade:
     php bin/console doctrine:migrations:execute --up 'App\Migrations\Version20251218101900'
    
     - Go to Absences -> Absences list, check that you don't have an import button
    
     - Go to /absence/import, check that access is forbidden
    
     - Go to Administration -> Technical configuration -> Absences Import CSV
       Enable AbsImport-CSV
    
     - Go to Absences -> Absences list, check that you have an import button
    
     - Upload a csv file with the following structure:
        - First line is column headers (not actual data, will be ignored)
        - Agent identifier (see below) in the first column
        - Start date in the sixth column
        - End date in the seventh column
    
       Agent identifier can be one of the following:
        - register (matricule)
        - login
        - mail
       The first column will be considered having one of those according to the
       AbsImport-Agent configuration option
    
       If an agent is not found in Planno given the selected identifier, the absence
       will not be imported.
    
       AbsImport-ConvertBegin and AbsImport-ConvertEnd contains one or more regular
       expressions, which will be used to match start date and end date, one after
       the other, until one matches.
    
       The default regexes for both AbsImport-ConvertBegin and AbsImport-ConvertEnd
       are the following:
         /^(\d{2}\/\d{2}\/\d{4})$/
         /^(\d{2}\/\d{2}\/\d{4}) (matin)$/
         /^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/
    
       The conversion goes as follows:
    
        - for the start date:
          - jj/mm/aaaa            -> yyyy-mm-dd 09:00:00
          - jj/mm/aaaa matin      -> yyyy-mm-dd 09:00:00
          - jj/mm/aaaa après-midi -> yyyy-mm-dd 13:00:00
    
        - for the end date:
          - jj/mm/aaaa            -> yyyy-mm-dd 20:00:00
          - jj/mm/aaaa matin      -> yyyy-mm-dd 13:00:00
          - jj/mm/aaaa après-midi -> yyyy-mm-dd 20:00:00
    
       AbsImport-Reason is empty by default.
    
     - Once the file is processed check that you have a table with the results.
       Traditional log levels are used: ERROR, WARNING, INFO, DEBUG
       INFO is used by default, but you can set the log level you want using the
       dropdown list.
    
     - Results are also stored in the log table, using AbsenceImportCSV as program,
       which you can display with a query like:
       select * from log where program="AbsenceImportCSV" order by id desc limit 10;
    
     - Import the same file again, and check that already existing absences are not
       duplicated.
    
     - See tests/Planno/Helper/AbsenceImportCSVHelperTest.php for unit tests

---
+++ src/Planno/Helper/AbsenceImportCSVHelper.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Planno\Helper;
+
+use App\Planno\Helper\BaseHelper;
+use App\Entity\Absence;
+use App\Entity\Agent;
+
+class AbsenceImportCSVHelper extends BaseHelper
+{
+
+    use \App\Traits\LoggerTrait;
+    private string $program = "AbsenceImportCSV";
+    private array $importResults = array();
+    private const ERROR   = 1;
+    private const WARNING = 2;
+    private const INFO    = 3;
+    private const DEBUG   = 4;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    private function checkRegexes($config_option) {
+        $regexes = $this->config($config_option);
+        if (trim($regexes) == '') {
+            return "$config_option est vide";
+        }
+        $regexes_array = explode("\n", $regexes);
+        foreach ($regexes_array as $regex) {
+            if (@preg_match($regex, '') === FALSE) {
+                return "$regex n'est pas une expression régulière valide dans l'option de configuration $config_option";
+            }
+        }
+        return '';
+    }
+
+    public function import($file, $loggedin_id)
+    {
+        $agent_match = $this->config('AbsImport-Agent');
+        $filename    = $file->getClientOriginalName();
+        $inF         = fopen($file->getPathname(), 'r');
+        $dbprefix    = $GLOBALS['dbprefix'];
+        $CSRFToken   = $GLOBALS['CSRFSession'];
+
+        # Empty previous results
+        $this->importResults = array();
+
+        # Should this be hardcoded?
+        $structure   = array('debut' => 5, 'fin' => 6);
+        $date_format = "d/m/Y";
+
+        $msg = "Début d'import des absences du fichier CSV '$filename': les agents sont cherchés via leur $agent_match";
+        $this->importLog(0, $msg, self::INFO);
+
+        $dbi = new \dbh();
+        $dbi->CSRFToken = $CSRFToken;
+
+        $dbi->prepare("INSERT INTO `{$dbprefix}absences` (`perso_id`, `debut`, `fin`, `motif`, `commentaires`, `demande`, `valide`, `validation`, `valide_n1`, `validation_n1`, `cal_name`, `ical_key`)
+    VALUES (:perso_id, :debut, :fin, :motif, :commentaires, :demande, :valide, :validation, :valide_n1, :validation_n1, :cal_name, :ical_key);");
+
+        $line = 0;
+        $imported = 0;
+
+        # We could use League\Csv\Reader instead
+        while ($tab = fgetcsv($inF, 1024, ';')) {
+            $line++;
+            if ($line == 1) {
+                $msg = "La ligne d'en-tête est ignorée";
+                $this->importLog($line,$msg, self::INFO);
+                continue;
+            }
+            $id = $tab[0];
+
+            if (!$id) {
+                $msg = "Impossible de récupérer l'identifiant de l'usager";
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            }
+
+            // Regex start
+            $regex_number = 0;
+            $sql_debut = '';
+
+            $regex_error = $this->checkRegexes('AbsImport-ConvertBegin');
+            if ($regex_error == '') {
+                $start_regexes = explode("\n", $this->config('AbsImport-ConvertBegin'));
+                foreach ($start_regexes as $regex) {
+                    $regex_number++;
+                    $value = $tab[$structure['debut']];
+                    if (preg_match($regex, $value, $capture)) {
+                        $csv_date = $capture[1];
+                        $date = \DateTime::createFromFormat($date_format, $csv_date);
+                        $sql_date = date_format($date, 'Y-m-d');
+                        if (array_key_exists(2, $capture) && $capture[2] == 'après-midi') {
+                            $hour = '13:00:00';
+                        } else {
+                            $hour = '09:00:00';
+                        }
+                        $sql_debut = $sql_date . " " . $hour;
+                        $msg = "La date de début $value a été convertie en $sql_debut en utilisant l'expression régulière $regex_number";
+                        $this->importLog($line,$msg, self::DEBUG);
+                        break;
+                    }
+                }
+            } else {
+                $msg = $regex_error;
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            }
+
+            if (!$sql_debut) {
+                $msg = "Impossible de définir une date de début à partir de la valeur $value";
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            }
+
+            // Regex end
+            $regex_number = 0;
+            $sql_fin = '';
+            $regex_error = $this->checkRegexes('AbsImport-ConvertEnd');
+            if ($regex_error == '') {
+                $end_regexes   = explode("\n", $this->config('AbsImport-ConvertEnd'));
+                foreach ($end_regexes as $regex) {
+                    $regex_number++;
+                    $value = $tab[$structure['fin']];
+                    if (preg_match($regex, $value, $capture)) {
+                        $csv_date = $capture[1];
+                        $date = \DateTime::createFromFormat($date_format, $csv_date);
+                        $sql_date = date_format($date, 'Y-m-d');
+                        if (array_key_exists(2, $capture) && $capture[2] == 'matin') {
+                            $hour = '13:00:00';
+                        } else {
+                            $hour = '20:00:00';
+                        }
+                        $sql_fin = $sql_date . " " . $hour;
+                        $msg = "La date de fin $value a été convertie en $sql_fin en utilisant l'expression régulière $regex_number";
+                        $this->importLog($line,$msg, self::DEBUG);
+                        break;
+                    }
+                }
+            } else {
+                $msg = $regex_error;
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            }
+
+            if (!$sql_fin) {
+                $msg = "Impossible de définir une date de fin à partir de la valeur $value";
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            }
+
+            // Find agent id based on first column
+            $agent = $this->entityManager->getRepository(Agent::class)->findOneBy(array($agent_match => $id));
+            if (!$agent) {
+                $msg = "Impossible de trouver un agent qui a $id pour $agent_match";
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            }
+
+            $perso_id  = $agent->getId();
+            $firstname = $agent->getFirstname();
+            $lastname  = $agent->getLastName();
+            $msg = "Agent avec $id pour $agent_match trouvé: agent $perso_id ($firstname $lastname)";
+            $this->importLog($line,$msg, self::DEBUG);
+
+
+            // Check if absence already exists
+            $absence_already_exists = $this->entityManager->getRepository(Absence::class)->findOneBy(
+                [
+                    'perso_id' => $perso_id,
+                    'debut'    => new \DateTime($sql_debut),
+                    'fin'      => new \DateTime($sql_fin),
+                    'motif'    => $this->config('AbsImport-Reason'),
+                ]
+            );
+            if ($absence_already_exists) {
+                $msg = "L'absence pour $firstname $lastname existe déjà (absence " . $absence_already_exists->getId() . ")";
+                $this->importLog($line,$msg, self::WARNING);
+                continue;
+            }
+
+            $now = date('Y-m-d H:i:s');
+            $insert = array(
+                ':perso_id'      => $perso_id,
+                ':debut'         => $sql_debut,
+                ':fin'           => $sql_fin,
+                ':motif'         => $this->config('AbsImport-Reason'),
+                ':commentaires'  => $this->program,
+                ':demande'       => $now,
+                ':valide'        => $loggedin_id,
+                ':validation'    => $now,
+                ':valide_n1'     => $loggedin_id,
+                ':validation_n1' => $now,
+                ':cal_name'      => '',
+                ':ical_key'      => '',
+            );
+
+            $result = $dbi->execute($insert);
+            if ($dbi->error) {
+                $msg = "L'absence n'a pas pu être ajoutée: " . $dbi->error;
+                $this->importLog($line,$msg, self::ERROR);
+                continue;
+            } else {
+                $msg = "Absence ajoutée pour l'agent $perso_id ($firstname, $lastname)";
+                $this->importLog($line,$msg, self::INFO);
+            }
+            $imported++;
+        }
+        $msg = "Fin d'import des absences du fichier CSV '$filename': $line lines traitées, $imported absences importées.";
+        $this->importLog($line+1 ,$msg, self::INFO);
+
+
+        return $this->importResults;
+    }
+
+    private function importLog($line, $message, $type)
+    {
+        $this->log("$line, $type: $message", $this->program);
+        $logline = array('line' => $line, 'message' => $message, 'type' => $type);
+        $this->importResults[] = $logline;
+    }
+}
---
+++ tests/Planno/Helper/AbsenceImportCSVHelperTest.php
@@ -0,0 +1,170 @@
+<?php
+
+use App\Planno\Helper\AbsenceImportCSVHelper;
+use App\Entity\Agent;
+use App\Entity\Absence;
+use Tests\PLBWebTestCase;
+use Tests\FixtureBuilder;
+
+class AbsenceImportCSVHelperTest extends PLBWebTestCase
+{
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->builder->delete(Agent::class);
+        $this->builder->delete(Absence::class);
+    }
+
+    public function testImport() {
+        $entityManager = $GLOBALS['entityManager'];
+        $GLOBALS['config']['AbsImport-Reason']        = 'Test reason';
+        $GLOBALS['config']['AbsImport-Agent']         = 'matricule';
+        $GLOBALS['config']['AbsImport-ConvertBegin']  = "/^(\d{2}\/\d{2}\/\d{4})$/\n/^(\d{2}\/\d{2}\/\d{4}) (matin)$/\n/^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/";
+        $GLOBALS['config']['AbsImport-ConvertEnd']    = "/^(\d{2}\/\d{2}\/\d{4})$/\n/^(\d{2}\/\d{2}\/\d{4}) (matin)$/\n/^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/";
+
+        $loggedin = $this->builder->build(Agent::class, array(
+             'login' => 'loggedin', 'nom' => 'In', 'prenom' => 'Logged',
+             'droits' => array(99,100)
+        ));
+        $loggedin_id = $loggedin->getId();
+
+        $example_agent = $this->builder->build(Agent::class, array(
+             'login' => 'alogin', 'prenom' => 'Ex', 'nom' => 'Ample', 'matricule' => 24, 'mail' => 'john.doe@example.com',
+             'droits' => array(99,100)
+        ));
+
+        $uploadedFile = $this->setUploadedFile('AbsenceImportCSVHelperTest-matricule.csv');
+        $helper   = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+
+        # New absence, agent found by matricule
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 1, "One absence has been added");
+        $absence = $absences[0];
+
+        $this->assertEquals($absence->getReason(), 'Test reason', "Absence reason is set according to AbsImport-Reason");
+        $this->assertEquals($absence->getStart(),  \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-11 13:00:00'), "start date was successfully transformed");
+        $this->assertEquals($absence->getEnd(),    \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-14 13:00:00'), "end date was sucessfully transformed");
+
+        # New import, absence already exists
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $this->assertEquals(sizeof($absences), 1, "Absence not added when it already exists");
+        $this->assertStringStartsWith("L'absence pour Ex Ample existe déjà", $results[5]['message'], "Log message: absence not added when it already exist");
+
+        # agent not found by matricule
+        $this->assertEquals(sizeof($absences), 1, "Absence not added when agent it not found");
+        $this->assertEquals($results[8]['message'], 'Impossible de trouver un agent qui a 25 pour matricule', "Agent not found by matricule");
+
+        # Empty start regex
+        $this->builder->delete(Absence::class);
+        $GLOBALS['config']['AbsImport-ConvertBegin'] = "";
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 0, "absence has not been added when AbsImport-ConvertBegin is empty");
+        $this->assertEquals($results[2]['message'], 'AbsImport-ConvertBegin est vide', "Log message: AbsImport-ConvertBegin is empty");
+
+        # Empty end regex
+        $GLOBALS['config']['AbsImport-ConvertBegin']  = "/^(\d{2}\/\d{2}\/\d{4})$/\n/^(\d{2}\/\d{2}\/\d{4}) (matin)$/\n/^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/";
+        $GLOBALS['config']['AbsImport-ConvertEnd']    = "";
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 0, "absence has not been added when AbsImport-ConvertEnd is empty");
+        $this->assertEquals($results[3]['message'], 'AbsImport-ConvertEnd est vide', "Log message: AbsImport-ConvertEnd is empty");
+
+        # Invalid start regex
+        $GLOBALS['config']['AbsImport-ConvertBegin']  = "I'm an invalid start regex";
+        $GLOBALS['config']['AbsImport-ConvertEnd']    = "";
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 0, "absence has not been added when AbsImport-ConvertBegin is invalid ");
+        $this->assertEquals($results[2]['message'], "I'm an invalid start regex n'est pas une expression régulière valide dans l'option de configuration AbsImport-ConvertBegin", "Log message: AbsImport-ConvertBegin is invalid");
+
+        # Invalid end regex
+        $GLOBALS['config']['AbsImport-ConvertBegin']  = "/^(\d{2}\/\d{2}\/\d{4})$/\n/^(\d{2}\/\d{2}\/\d{4}) (matin)$/\n/^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/";
+        $GLOBALS['config']['AbsImport-ConvertEnd']    = "I'm an invalid end regex";
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 0, "absence has not been added when AbsImport-ConvertEnd is invalid ");
+        $this->assertEquals($results[3]['message'], "I'm an invalid end regex n'est pas une expression régulière valide dans l'option de configuration AbsImport-ConvertEnd", "Log message: AbsImport-ConvertEnd is invalid");
+
+        $helper   = new AbsenceImportCSVHelper();
+        $uploadedFile = $this->setUploadedFile('AbsenceImportCSVHelperTest-login.csv');
+
+        # New absence, agent found by login
+        $this->builder->delete(Absence::class);
+        $GLOBALS['config']['AbsImport-ConvertBegin']  = "/^(\d{2}\/\d{2}\/\d{4})$/\n/^(\d{2}\/\d{2}\/\d{4}) (matin)$/\n/^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/";
+        $GLOBALS['config']['AbsImport-ConvertEnd']    = "/^(\d{2}\/\d{2}\/\d{4})$/\n/^(\d{2}\/\d{2}\/\d{4}) (matin)$/\n/^(\d{2}\/\d{2}\/\d{4}) (après-midi)$/";
+        $GLOBALS['config']['AbsImport-Agent']         = 'login';
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 1, "One absence has been added");
+        $absence = $absences[0];
+
+        # agent not found by login
+        $this->assertEquals(sizeof($absences), 1, "Absence not added when agent it not found");
+        $this->assertEquals($results[8]['message'], 'Impossible de trouver un agent qui a notalogin pour login', "Agent not found by login");
+
+        $uploadedFile = $this->setUploadedFile('AbsenceImportCSVHelperTest-mail.csv');
+
+        # New absence, agent found by email
+        $this->builder->delete(Absence::class);
+        $GLOBALS['config']['AbsImport-Agent'] = 'mail';
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 1, "One absence has been added");
+
+        # agent not found by login
+        $this->assertEquals(sizeof($absences), 1, "Absence not added when agent is not found");
+        $this->assertEquals($results[8]['message'], 'Impossible de trouver un agent qui a does.not.exist@example.com pour mail', "Agent not found by email");
+
+        $this->builder->delete(Absence::class);
+        $uploadedFile = $this->setUploadedFile('AbsenceImportCSVHelperTest-regexes.csv');
+
+        # Check regexes
+        $helper = new AbsenceImportCSVHelper();
+        $results  = $helper->import($uploadedFile, $loggedin_id);
+        $absences = $entityManager->getRepository(Absence::class)->findBy(array('perso_id' => $example_agent->getId()));
+        $this->assertEquals(sizeof($absences), 3, "Three absences have been added");
+        $absence = $absences[0];
+
+        # Start morning
+        $this->assertEquals($absence->getStart(),  \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-01 09:00:00'), "start date was successfully transformed");
+
+        # End afternoon
+        $this->assertEquals($absence->getEnd(),    \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-01 20:00:00'), "end date was successfully transformed");
+
+        $absence = $absences[1];
+
+        # Start afternoon
+        $this->assertEquals($absence->getStart(),  \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-02 13:00:00'), "start date was successfully transformed");
+
+        # End morning
+        $this->assertEquals($absence->getEnd(),  \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-03 13:00:00'), "start date was successfully transformed");
+
+        $absence = $absences[2];
+
+        # Start nothing
+        $this->assertEquals($absence->getStart(),  \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-04 09:00:00'), "start date was successfully transformed");
+
+        # End nothing
+        $this->assertEquals($absence->getEnd(),  \DateTime::createFromFormat('Y-m-d H:i:s', '2025-04-05 20:00:00'), "start date was successfully transformed");
+    }
+
+    private function setUploadedFile($filename) {
+        $local_file = __DIR__ . '/' . $filename;
+        return new Symfony\Component\HttpFoundation\File\UploadedFile(
+            $local_file,
+            $filename,
+            'text/csv',
+            null,
+            true
+        );
+    }
+}
