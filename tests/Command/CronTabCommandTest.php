76e1cde70768b10a89ff40561905f15f4323a543
Xinying SUN <xinying.sun@biblibre.com>
2025-12-29 11:10:54 +0100

    block

--- tests/Command/CronTabCommandTest.php
+++ tests/Command/CronTabCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Command;
 
+use App\Entity\Absence;
 use App\Entity\Agent;
 use Tests\PLBWebTestCase;
 
@@ -17,7 +18,7 @@ class CronTabCommandTest extends PLBWebTestCase
         $dateStr = (new \DateTime())->format('d/m/Y');
 
         $crawler = $this->client->request('GET', '/absence/add');
-        dump($crawler->html());
+        
         $crawler->filter('input[name="debut"]')->sendKeys($dateStr);
         $crawler->filter('input[name="fin"]')->sendKeys($dateStr);
 
@@ -28,9 +29,38 @@ class CronTabCommandTest extends PLBWebTestCase
         $crawler = $this->client->refreshCrawler();
 
         // click enregistre
+        $dialog = $crawler->filter('.ui-dialog')
+            ->reduce(fn ($node) => str_contains($node->attr('style') ?? '', 'display: block'))
+            ->first();
+        $button = $dialog->filter('.ui-dialog-buttonpane button')->eq(1);
+        $button->click();
+        $crawler = $this->client->refreshCrawler();
 
-        //
-        
+        // select motif
+        $this->client->executeScript('document.body.click();');
+        $crawler = $this->client->refreshCrawler();
 
+        $select = $this->getSelect('motif');
+        $options = $select->getOptions();
+
+        $this->assertGreaterThan(
+            1,
+            count($options),
+            'There should be options'
+        );
+
+        $this->client->executeScript('document.body.click();');
+        $crawler = $this->client->refreshCrawler();
+        dump($crawler->html());
+        $select->selectByValue('Congés payés');
+        $crawler = $this->client->refreshCrawler();
+
+        // submit form
+        $button = $dialog->filter('.ui-dialog-buttonpane button')->eq(1);
+        $button->click();
+        $crawler = $this->client->refreshCrawler();
+
+        $countAbsences = $this->entityManager->getRepository(Absence::class)->count(['agent' => $agent]);
+        $this->assertEquals(140, $countAbsences, 'There should be 140 absences created');
     }
 }
