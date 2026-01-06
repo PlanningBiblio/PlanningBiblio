d949c70683cec8fc89fa3a2977ab265b5c6ccc70
Xinying SUN <xinying.sun@biblibre.com>
2026-01-06 16:28:29 +0100

    MT50832: Agent- add a migration to update teble personnel,for all the 0000-00-00 change to null

---
+++ migrations/2026/Version20260106103039.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260106103039 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $dbprefix = $_ENV['DATABASE_PREFIX'];
+
+        $this->addSql("UPDATE {$dbprefix}personnel SET arrivee = NULL WHERE arrivee IN ('0000-00-00', '-0001-11-30')");
+        $this->addSql("UPDATE {$dbprefix}personnel SET depart = NULL WHERE depart IN ( '0000-00-00', '-0001-11-30')");
+        $this->addSql("UPDATE {$dbprefix}personnel SET last_login = NULL WHERE last_login IN ( '0000-00-00 00:00:00', '-0001-11-30 00:00:00')");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $dbprefix = $_ENV['DATABASE_PREFIX'];
+        
+        $this->addSql("UPDATE {$dbprefix}personnel SET arrivee = '0000-00-00' WHERE arrivee IS NULL");
+        $this->addSql("UPDATE {$dbprefix}personnel SET depart = '0000-00-00' WHERE depart IS NULL");
+        $this->addSql("UPDATE {$dbprefix}personnel SET last_login = '0000-00-00 00:00:00' WHERE last_login IS NULL");
+    }
+
+}
