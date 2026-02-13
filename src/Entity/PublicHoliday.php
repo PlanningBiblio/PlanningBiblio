65727cac49e889e814808c3bdf9a41c1e97b37f7
Xinying SUN <xinying.sun@biblibre.com>
2026-02-13 16:12:58 +0100

    MT52180: PublicHoliday- complete Entity,add TU,add glossary

--- src/Entity/PublicHoliday.php
+++ src/Entity/PublicHoliday.php
@@ -36,4 +36,70 @@ class PublicHoliday
     {
         return $this->id;
     }
+
+    public function getYear(): ?string
+    {
+        return $this->annee;
+    }
+
+    public function setYear(?string $year): static
+    {
+        $this->annee = $year;
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->jour;
+    }
+
+    public function setDate(?\DateTime $date): static
+    {
+        $this->jour = $date;
+        return $this;
+    }
+
+    public function getDayOff(): ?int
+    {
+        return $this->ferie;
+    }
+
+    public function setDayOff(?int $dayOff): static
+    {
+        $this->ferie = $dayOff;
+        return $this;
+    }
+
+    public function getDayClosed(): ?int
+    {
+        return $this->fermeture;
+    }
+
+    public function setDayClosed(?int $dayClosed): static
+    {
+        $this->fermeture = $dayClosed;
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->nom = $name;
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setComment(?string $comment): static
+    {
+        $this->commentaire = $comment;
+        return $this;
+    }
 }
