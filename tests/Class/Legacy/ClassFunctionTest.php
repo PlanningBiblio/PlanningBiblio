05af57b92ac44703e538438b86a425ade4fb86f3
Matthias Meusburger <matthias.meusburger@biblibre.com>
2026-03-12 16:48:07 +0100

    MT51960: Sanitize dateSQL and dateFr3 return values
     - Keep exact compatibility for dateSQL and dateFr3 by allowing to have date
       and hour as a parameter

--- tests/Class/Legacy/ClassFunctionTest.php
+++ tests/Class/Legacy/ClassFunctionTest.php
@@ -6,15 +6,19 @@ class ClassFunctionTest extends TestCase
 {
     public function testDateSQL(): void
     {
-        $this->assertEquals('1980-12-25', dateSQL('25/12/1980'), 'a valid date is converted to an SQL date');
-        $this->assertEquals('1980-12-25', dateSQL('1980-12-25'), 'a valid SQL date is kept');
-        $this->assertEquals('', dateSQL('SQL Injection'), 'a invalid date is converted to an empty string');
+        $this->assertEquals('1980-12-25',          dateSQL('25/12/1980'),          'a valid date is converted to an SQL date');
+        $this->assertEquals('1980-12-25 12:00:00', dateSQL('25/12/1980 12:00:00'), 'a valid date and hour is converted to an SQL date');
+        $this->assertEquals('1980-12-25',          dateSQL('1980-12-25'),          'a valid SQL date is kept');
+        $this->assertEquals('1980-12-25 12:00:00', dateSQL('1980-12-25 12:00:00'), 'a valid SQL date and hour is kept');
+        $this->assertEquals('',                    dateSQL('SQL Injection'),       'a invalid date is converted to an empty string');
     }
 
     public function testDateFr3(): void
     {
-        $this->assertEquals('25/12/1980', dateFr3('1980-12-25'), 'a valid SQL date is converted to a fr date');
-        $this->assertEquals('25/12/1980', dateFr3('25/12/1980'), 'a valid fr date is kept');
-        $this->assertEquals('', dateFr3('SQL Injection'), 'a invalid date is converted to an empty string');
+        $this->assertEquals('25/12/1980',          dateFr3('1980-12-25'),          'a valid SQL date is converted to a fr date');
+        $this->assertEquals('25/12/1980 12:00:00', dateFr3('1980-12-25 12:00:00'), 'a valid SQL date and hour is converted to a fr date');
+        $this->assertEquals('25/12/1980',          dateFr3('25/12/1980'),          'a valid fr date is kept');
+        $this->assertEquals('25/12/1980 12:00:00', dateFr3('25/12/1980 12:00:00'), 'a valid fr date and hour is kept');
+        $this->assertEquals('',                    dateFr3('SQL Injection'),       'a invalid date is converted to an empty string');
     }
 }
