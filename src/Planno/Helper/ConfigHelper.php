795ea11295f78c1aa364b8502507a5d56b539e6f
Matthias Meusburger <matthias.meusburger@biblibre.com>
2026-04-01 16:03:43 +0200

    MT51958: Do not display password in configuration page
    The password are no longer displayed in the configuration page.
    (currently affects Mail-Password and LDAP-Password)
    
    If the password field is left blank, the current password in the database
    will not be updated.
    
    The test button will use the password in the input field if provided by the
    user, or the password in database if the input field is left blank.
    
    See tests/Planno/Helper/ConfigHelperTest.php for unit tests.

---
+++ src/Planno/Helper/ConfigHelper.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Planno\Helper;
+
+use App\Planno\Helper\BaseHelper;
+use App\Entity\Config;
+
+class ConfigHelper extends BaseHelper
+{
+
+    private $configRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->configRepository = $this->entityManager->getRepository(Config::class);
+    }
+
+    public function saveConfig($params): string
+    {
+            $technical = $params['technical'];
+            $error = '';
+
+            $configParams = $this->configRepository->getParams($technical);
+
+            foreach ($configParams as $cp) {
+                if (in_array($cp->getType(), ['hidden', 'info'])) {
+                    continue;
+                }
+                // boolean and checkboxes elements.
+                if (!isset($params[$cp->getName()])) {
+                    $params[$cp->getName()] = $cp->getType() == 'boolean' ? '0' : array();
+                }
+                $value = $params[$cp->getName()];
+
+                if (is_string($value)) {
+                    $value = trim($value);
+                }
+
+                // Passwords
+                if ($cp->getType() == 'password') {
+                    if ($value == '') {
+                        continue;
+                    }
+                    $value = encrypt($value);
+                }
+                // Checkboxes
+                if (is_array($value)) {
+                    $value = json_encode($value);
+                }
+
+                if ($cp->getType() == 'color') {
+                    $value = filter_var($value, FILTER_CALLBACK, ['options' => 'sanitize_color']);
+                }
+
+                try {
+                    $cp->setValue($value);
+                    $this->entityManager->persist($cp);
+                }
+                catch (Exception $e) {
+                    $error = 'Une erreur est survenue pendant la modification de la configuration !';
+                }
+            }
+            $this->entityManager->flush();
+
+            return $error;
+    }
+}
