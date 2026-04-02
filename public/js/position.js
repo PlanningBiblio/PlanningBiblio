5f67d60c655768af48351a6f9a8ecea4ea134ef3
Lauranne Fossat <98071615+lau-fst@users.noreply.github.com>
2026-04-02 16:54:27 +0200

    MT46629: Use bootstrap for position's modals

--- public/js/position.js
+++ public/js/position.js
@@ -4,114 +4,102 @@
 
 $(function() {
 
-  // Paramétrage de la boite de dialogue permettant la modification des étages
-  $('#add-etage-form').dialog({
-    autoOpen: false,
-    height: 480,
-    width: 560,
-    modal: true,
-    resizable: false,
-    draggable: false,
-    buttons: {
-      Enregistrer: function() {
-        // Supprime les lignes cachées lors du clic sur la corbeille
-        $('#etages-sortable li:hidden').each(function() {
-          $(this).remove();
-        });
+  // Permet de rendre la liste des étages triable
+  if($('#floors_sortable').length) {
+    Sortable.create(floors_sortable, {ghostClass: 'bg-blue', animation: 150}); 
+  }
 
-        // Enregistre les éléments du formulaire dans un tableau
-        tab = new Array();
-        $('#etages-sortable li').each(function() {
-          var id = $(this).attr('id').replace('li_', '');
-          tab.push({
-            id: id,
-            value: $("#valeur_"+id).text(),
-            place: $(this).index()
-          });
-        });
-
-        tab = JSON.stringify(tab);
-
-        // Transmet le tableau à la page de validation ajax
-        var _token = $('input[name=_token]').val();
-
-        $.ajax({
-          url: url('ajax/update-select-options'),
-          type: 'post',
-          dataType: 'json',
-          data: {
-            _token: _token,
-            CSRFToken: $('#CSRFSession').val(),
-            menu: 'etages',
-            tab: tab,
-          },
-
-          success: function() {
-            var current_val = $('#etage').val();
-            $('#etage').empty();
-            $('#etage').append('<option value=""></option>');
-
-            $('#etages-sortable li').each(function() {
-              var id = $(this).attr('id').replace('li_', '');
-              var val = $('#valeur_' + id).text();
-              var selected = id == current_val;
-              var option = new Option(val, id, selected, selected);
-              $('#etage').append(option);
-            });
-
-            $('#add-etage-form').dialog('close');
-            $('#etage').effect('highlight', null, 2000);
-          },
-
-          error: function(){
-            alert('Erreur lors de l\'enregistrement des modifications');
-          }
-        });
-      },
-
-      Annuler: function() {
-        $(this).dialog('close');
-      },
-    },
-
-    close: function() {
-      $('#etages-sortable li:hidden').each(function() {
-        $(this).show();
-      });
-    }
-  });
-
-  // Affiche la boite de dialogue permettant la modification des etages
-  $("#add-etage-button").click(function() {
-      $("#add-etage-form").dialog( "open" );
-      return false;
+  $('#arrange-floors').on('submit', function(e) {
+    e.preventDefault();
+    // Supprime les lignes cachées lors du clic sur la corbeille
+    $('#floors_sortable li:hidden').each(function() {
+      $(this).remove();
     });
 
-  // Permet de rendre la liste des etages triable
-  $("#etages-sortable" ).sortable({
-    placeholder: "ui-state-highlight",
+    // Enregistre les éléments du formulaire dans un tableau
+    tab = new Array();
+    $('#floors_sortable li').each(function() {
+      var id = $(this).attr('id').replace('li_', '');
+      tab.push({
+        id: id,
+        value: $("#valeur_"+id).text(),
+        place: $(this).index()
+      });
+    });
+
+    tab = JSON.stringify(tab);
+
+    // Transmet le tableau à la page de validation ajax
+    var _token = $('input[name=_token]').val();
+
+    $.ajax({
+      url: url('ajax/update-select-options'),
+      type: 'post',
+      dataType: 'json',
+      data: {
+        _token: _token,
+        CSRFToken: $('#CSRFSession').val(),
+        menu: 'etages',
+        tab: tab,
+      },
+
+      success: function() {
+        var current_val = $('#etage').val();
+        $('#etage').empty();
+        $('#etage').append('<option value=""></option>');
+
+        $('#floors_sortable li').each(function() {
+          var id = $(this).attr('id').replace('li_', '');
+          var val = $('#valeur_' + id).text();
+          var selected = id == current_val;
+          var option = new Option(val, id, selected, selected);
+          $('#etage').append(option);
+        });
+
+        $('#add-floor-modal').modal('hide');
+        $('#etage').effect('highlight', null, 2000);
+      },
+
+      error: function() {
+        alert('Erreur lors de l\'enregistrement des modifications');
+      },
+    });
   });
 
+  $('#add-floor-modal').on('hidden.bs.modal', function() {
+    $('#floors_sortable li:hidden').each(function() {
+      $(this).show();
+    });
+  });
+
+  // Suppression message invalidité lors du changement d'input
+  $('#add-floor-text').on('input', function(e) {
+    $('#add-floor-text').removeClass('is-invalid');
+  })
+
   // Permet d'ajouter de nouveaux etages (clic sur le bouton ajouter)
-  $("#add-etage-button2").click(function(){
-    var text=sanitize_string($("#add-etage-text").val());
+  $('#add-floor').on('submit', function(e) {
+    e.preventDefault();
+    $('.invalid-feedback').text('Etage invalide');
+
+    var text = sanitize_string($('#add-floor-text').val());
     if(!text){
-      CJInfo("Donnée invalide","error");
-      $("#add-etage-text").val();
+      $('#add-floor-text').addClass('is-invalid');
       return;
     }
 
     // Vérifie si l'étage existe déjà
     var exist = false;
-    $('#etages-sortable > li > font').each(function(){
+    $('#floors_sortable > li > span').each(function() {
       if($(this).text().toLowerCase() == text.toLowerCase()){
-        CJInfo("Cette valeur existe déjà.","error");
+        $('.invalid-feedback').text('Un étage avec ce nom existe déjà.');
+        $('#add-floor-text').addClass('is-invalid');
         exist = true;
         return;
       }
     });
 
-    if(exist){
+    if(exist) {
       return;
     }
 
@@ -119,122 +107,112 @@ $(function() {
     while($('#li_'+number).length){
       number++;
     }
-    $("#etages-sortable").append("<li id='li_"+number+"' class='ui-state-default'><span class='pl-icon pl-icon-arrowupdown'></span>"
-      +"<font id='valeur_"+number+"'>"+text+"</font>"
-      +"<span class='pl-icon pl-icon-trash' style='position:absolute;left:480px;cursor:pointer;' onclick='$(this).closest(\"li\").hide();'></span>"
-      +"</li>");
+    $('#floors_sortable').append(
+       '<li class="row row-sortable" id="li_' + number + '"> <i class="col-auto p-0 ps-2 bi bi-arrow-down-up"></i>'
+      + '<span class="col-10 p-2" id="valeur_' + number + '">' + text + '</span>'
+      + '<span class="col-1 ps-5" onclick="$(this).closest(\'li\').hide();"><i class="bi bi-trash3-fill"></i></span>'
+      + '</li>');
 
     // Reset du champ texte une fois l'ajout effectué
-    $("#add-etage-text").val(null);
+    $('#add-floor-text').val(null);
   });
-  
-  // Paramétrage de la boite de dialogue permettant la modification des groupes
-  $('#add-group-form').dialog({
-    autoOpen: false,
-    height: 480,
-    width: 560,
-    modal: true,
-    resizable: false,
-    draggable: false,
-    buttons: {
-      Enregistrer: function() {
-        // Supprime les lignes cachées lors du clic sur la corbeille
-        $('#groups-sortable li:hidden').each(function() {
-          $(this).remove();
-        });
-    
-        // Enregistre les éléments du formulaire dans un tableau
-        tab = new Array();
-        $('#groups-sortable li').each(function() {
-          var id = $(this).attr('id').replace('li_', '');
-            tab.push({
-              id: id,
-              value: $("#valeur_"+id).text(),
-              place: $(this).index()
-            });
-        });
-
-        tab = JSON.stringify(tab);
-
-        // Transmet le tableau à la page de validation ajax
-        var _token = $('input[name=_token]').val();
-
-        $.ajax({
-          url: url('ajax/update-select-options'),
-          type: 'post',
-          dataType: 'json',
-          data: {
-            _token: _token,
-            CSRFToken: $('#CSRFSession').val(),
-            menu: 'groupes',
-            tab: tab,
-          },
-
-          success: function() {
-            var current_val = $('#groupe').val();
-            $('#groupe').empty();
-            $('#groupe').append('<option value=""></option>');
-            
-            $('#groups-sortable li').each(function() {
-              var id=$(this).attr('id').replace('li_', '');
-              var val = $('#valeur_' + id).text();
-              var selected = id == current_val;
-              var option = new Option(val, id, selected, selected);
-              $('#groupe').append(option);
-            });
-            $('#add-group-form').dialog('close');
-            $('#groupe').effect('highlight', null, 2000);
-          },
-
-          error: function(){
-            alert('Erreur lors de l\'enregistrement des modifications');
-          }
-        });
-      },
-
-      Annuler: function() {
-        $(this).dialog( "close" );
-      },
-    },
-
-    close: function() {
-      $('#groups-sortable li:hidden').each(function() {
-        $(this).show();
-      });
-    }
-  });
-
-  // Affiche la boite de dialogue permettant la modification des groupes
-  $("#add-group-button").click(function() {
-      $("#add-group-form").dialog( "open" );
-      return false;
-    });
 
   // Permet de rendre la liste des groupes triable
-  $("#groups-sortable" ).sortable({
-    placeholder: "ui-state-highlight",
+  if($('#groups_sortable').length) {
+    Sortable.create(groups_sortable, {ghostClass: 'bg-blue', animation: 150}); 
+  }
+
+  $('#arrange-groups').on('submit', function(e) {
+    e.preventDefault();
+    // Supprime les lignes cachées lors du clic sur la corbeille
+    $('#groups_sortable li:hidden').each(function() {
+      $(this).remove();
+    });
+
+    // Enregistre les éléments du formulaire dans un tableau
+    tab = new Array();
+    $('#groups_sortable li').each(function() {
+      var id = $(this).attr('id').replace('li_', '');
+        tab.push({
+          id: id,
+          value: $("#valeur_"+id).text(),
+          place: $(this).index()
+        });
+    });
+
+    tab = JSON.stringify(tab);
+
+    // Transmet le tableau à la page de validation ajax
+    var _token = $('input[name=_token]').val();
+
+    $.ajax({
+      url: url('ajax/update-select-options'),
+      type: 'post',
+      dataType: 'json',
+      data: {
+        _token: _token,
+        CSRFToken: $('#CSRFSession').val(),
+        menu: 'groupes',
+        tab: tab,
+      },
+
+      success: function() {
+        var current_val = $('#groupe').val();
+        $('#groupe').empty();
+        $('#groupe').append('<option value=""></option>');
+        
+        $('#groups_sortable li').each(function() {
+          var id=$(this).attr('id').replace('li_', '');
+          var val = $('#valeur_' + id).text();
+          var selected = id == current_val;
+          var option = new Option(val, id, selected, selected);
+          $('#groupe').append(option);
+        });
+        $('#add-group-modal').modal('hide');
+        $('#groupe').effect('highlight', null, 2000);
+      },
+
+      error: function() {
+        alert('Erreur lors de l\'enregistrement des modifications');
+      }
+    });
   });
 
-  // Permet d'ajouter de nouveaux groupes (clic sur le bouton ajouter)
-  $("#add-group-button2").click(function(){
+  $('#add-group-modal').on('hidden.bs.modal', function() {
+    $('#groups_sortable li:hidden').each(function() {
+      $(this).show();
+    });
+  });
+
+  // Suppression message invalidité lors du changement d'input
+  $('#add-group-text').on('input', function(e) {
+    $('#add-group-text').removeClass('is-invalid');
+  })
+
+  // Permet d'ajouter de nouveaux groupe (clic sur le bouton ajouter)
+  $('#add-group').on('submit', function(e) {
+    e.preventDefault();
+    $('.invalid-feedback').text('Groupe invalide.');
+
     var text=sanitize_string($("#add-group-text").val());
     if(!text){
-      CJInfo("Donnée invalide","error");
-      $("#add-group-text").val();
+      $('#add-group-text').addClass('is-invalid');
       return;
     }
 
     // Vérifie si le groupe existe déjà
     var exist = false;
-    $('#groups-sortable > li > font').each(function(){
+
+    $('#groups_sortable > li > span').each(function() {
       if($(this).text().toLowerCase() == text.toLowerCase()){
-        CJInfo("Cette valeur existe déjà.","error");
+        $('.invalid-feedback').text('Un groupe avec ce nom existe déjà.');
+        $('#add-group-text').addClass('is-invalid');
         exist = true;
         return;
       }
     });
 
-    if(exist){
+    if(exist) {
       return;
     }
 
@@ -242,13 +220,17 @@ $(function() {
     while($('#li_'+number).length){
       number++;
     }
-    $("#groups-sortable").append("<li id='li_"+number+"' class='ui-state-default'><span class='pl-icon pl-icon-arrowupdown'></span>"
-      +"<font id='valeur_"+number+"'>"+text+"</font>"
-      +"<span class='pl-icon pl-icon-trash' style='position:absolute;left:480px;cursor:pointer;' onclick='$(this).closest(\"li\").hide();'></span>"
-      +"</li>");
+
+    $('#groups_sortable').append(
+       '<li class="row row-sortable" id="li_' + number + '">'
+      + '<i class="col-auto p-0 ps-2 bi bi-arrow-down-up"></i>'
+      + '<span class="col-10 p-2" id="valeur_' + number + '">' + text + '</span>'
+      + '<span class="col-1 ps-5" onclick="$(this).closest(\'li\').hide();">'
+      + '<i class="bi bi-trash3-fill"></i></span>'
+      + '</li>');
 
     // Reset du champ texte une fois l'ajout effectué
-    $("#add-group-text").val(null);
+    $('#add-group-text').val(null);
   });
 
 });
