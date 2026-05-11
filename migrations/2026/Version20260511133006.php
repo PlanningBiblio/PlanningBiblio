4a7455f694f5e0f0bc3a32c2be9979ef3be2d8bf
Lauranne Fossat <98071615+lau-fst@users.noreply.github.com>
2026-05-11 16:46:45 +0200

    PasswordLength param : text type -> number type
     - added migration type udpate
     - extracted options from "valeurs" element and initilized field accordingly

---
+++ migrations/2026/Version20260511133006.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260511133006 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Update the Auth-PasswordLength parameter type from text to number';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $dbprefix = $_ENV['DATABASE_PREFIX'];
+        $this->addSql("UPDATE {$dbprefix}config SET type = 'number', valeurs = '{\"min\":8,\"max\":100,\"step\":1}' where nom='Auth-PasswordLength'");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $dbprefix = $_ENV['DATABASE_PREFIX'];
+        $this->addSql("UPDATE {$dbprefix}config SET type = 'text', valeurs = '' where nom='Auth-PasswordLength'");
+    }
+}
