e55b43e295dfdeaef7bab1e5a58c0490e0cfeeab
Jerome Combes <jerome@planningbiblio.fr>
2026-05-11 15:57:45 +0200

    MT52706: Add descriptive tooltips and controls to the position form
    * Enable tooltip information on parameters
    * Invalid messages display when parameter incompatibility
    * JS script to copy tooltip text and paste it into the aria-label attribute
    * CSS display adapted to form validity
    * Grid structure for template
    * Translation messages enabled (french only)
    * Error messages on change input and not submit form
    * Translation enabled in english for tooltip
    * Translation enabled for form input
    * Update Help

--- public/js/script.js
+++ public/js/script.js
@@ -966,4 +966,10 @@ $(function(){
 
   // Infobulles
   $(document).tooltip();
+
+  $('button.tooltip-icon').each(function() {
+    var title = $(this).attr('data-bs-title');
+    $(this).attr('aria-label', title);
+  });
+
 });
