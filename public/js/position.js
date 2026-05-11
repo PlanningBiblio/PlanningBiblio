e2165742c1add362f03004830e13884578c7ae00
Jerome Combes <jerome@planningbiblio.fr>
2026-05-11 15:57:45 +0200

    MT52706: Add descriptive tooltips and controls to the position form
    * Enable tooltip information on parameters
    * Invalid messages display when parameter incompatibility
    * JS script to copy tooltip text and paste it into the aria-label attribute
    * CSS display adapted to form validity
    * Grid structure for template
    * Translation messages enabled (french only)
    * Error messages on change input and not submit form
    * Translation enabled in english for tooltip
    * Translation enabled for form input
    * Update Help

--- public/js/position.js
+++ public/js/position.js
@@ -4,6 +4,11 @@
 
 $(function() {
 
+  $(document).ready(function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  })
+
   // Permet de rendre la liste des étages triable
   if($('#floors_sortable').length) {
     Sortable.create(floors_sortable, {ghostClass: 'bg-blue', animation: 150}); 
@@ -237,6 +242,37 @@ $(function() {
     // Reset du champ texte une fois l'ajout effectué
     $('#add-group-text').val(null);
     $(this).removeClass('was-validated');
+  });
+
+  $('.form-check-input').on('change', function(e) {
+    other_choice = $(this).parent('.form-check').siblings().children();
+    if (other_choice.hasClass('is-invalid')) {
+      $('.form-check-input').removeClass('is-invalid');
+    }
+  })
+
+  $('.form-check-input').on('change', function(e) {
+    // Check that the parameters are compatible with each other and displays an error message if they are not
+    if ($('#lunch1').prop('checked')) {
+      if ($('#statistics1').prop('checked')) {
+        $('#lunch1').addClass('is-invalid');
+        $('#statistics1').addClass('is-invalid');
+      }
+      if ($('#quota_sp1').prop('checked')) {
+        $('#lunch1').addClass('is-invalid');
+        $('#quota_sp1').addClass('is-invalid');
+      }
+    }
+    if ($('#bloq2').prop('checked')) {
+      if ($('#statistics1').prop('checked')) {
+        $('#bloq2').addClass('is-invalid');
+        $('#statistics1').addClass('is-invalid');
+      }
+      if ($('#quota_sp1').prop('checked')) {
+        $('#bloq2').addClass('is-invalid');
+        $('#quota_sp1').addClass('is-invalid');
+      }
+    }
   });
 
 });
