541a732676016c2539807d771fd53254c540c097
Jerome Combes <jerome@planningbiblio.fr>
2026-06-25 18:15:22 +0200

    Release v26.05.02

---
+++ migrations/2026/Version20260625161520.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20260625161520 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Release 26.05.02';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $dbprefix = $_ENV['DATABASE_PREFIX'];
+
+        $this->addSql("UPDATE {$dbprefix}config SET valeur = '26.05.02' WHERE nom = 'Version';");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $dbprefix = $_ENV['DATABASE_PREFIX'];
+
+        $this->addSql("UPDATE {$dbprefix}config SET valeur = '26.05.01' WHERE nom = 'Version';");
+    }
+
+}
