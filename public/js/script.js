2fb78e8a0433b87091cf0fc24187c417f5fa94ec
Lauranne Fossat <98071615+lau-fst@users.noreply.github.com>
2026-07-21 14:52:41 +0200

    MT46629 : delete verif_date() fct

--- public/js/script.js
+++ public/js/script.js
@@ -507,11 +507,8 @@ function highlight(obj) {
   obj.addClass('highlight');setTimeout(() => {obj.removeClass('highlight');}, 3000);
 }
 
-// This function is intended to replace verif_date() in the long term
-// It checks the validity of a date (string) and ensures it complies with a specific format
-// The date format is loaded from the translation files for the selected locale
-function date_validation(date_value) {
-  format = Translator.trans('MM/DD/YYYY');
+// This function checks the validity of a date (string) and ensures it complies with a specific format
+function date_validation(date_value, format='DD/MM/YYYY') {
   return dayjs(date_value, format, true).isValid();
 };
 
@@ -530,46 +527,6 @@ function text_validation(text_value) {
 // This function checks the validity of an integer value
 function int_validation(value) {
   return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
-}
-
-function verif_date(d){
-  // Cette fonction vérifie le format AAAA-MM-JJ saisi et la validité de la date.
-  // Le séparateur est défini dans la variable separateur
-  var amin=1999; // année mini
-  var amax=2080; // année maxi
-  var separateur="-"; // separateur entre jour/mois/annee
-  var j=(d.substring(8));
-  var m=(d.substring(5,7));
-  var a=(d.substring(0,4));
-  var ok=1;
-  if ( ((isNaN(j))||(j<1)||(j>31)) && (ok==1) ) {
-  //       alert("Le jour n'est pas correct."); 
-    ok=0;
-  }
-  if ( ((isNaN(m))||(m<1)||(m>12)) && (ok==1) ) {
-  //       alert("Le mois n'est pas correct."); 
-    ok=0;
-  }
-  if ( ((isNaN(a))||(a<amin)||(a>amax)) && (ok==1) ) {
-  //     alert("L'année n'est pas correcte."); 
-    ok=0;
-  }
-  if ( ((d.substring(4,5)!=separateur)||(d.substring(7,8)!=separateur)) && (ok==1) ) {
-  //   alert("Les séparateurs doivent être des "+separateur); 
-    ok=0;
-  }
-  if (ok==1){
-    var d2=new Date(a,m-1,j);
-    j2=d2.getDate();
-    m2=d2.getMonth()+1;
-    a2=d2.getFullYear();
-    if (a2<=100) {a2=1900+a2}
-    if ( (j!=j2)||(m!=m2)||(a!=a2) ){
-    //           alert("La date "+d+" n'existe pas !");
-      ok=0;
-    }
-  }
-  return ok;
 }
 
 function verif_form(champs, form='form', callback=null)
@@ -601,7 +558,7 @@ function verif_form(champs, form='form', callback=null)
 	// Converti les dates JJ/MM/AAAA en AAAA-MM-JJ
         valeur=valeur.replace(/([0-9]{2})\/([0-9]{2})\/([0-9]{4})/g,"$3-$2-$1");
       }
-      if(type.substr(0,4)=="date" && verif_date(valeur)==0)
+      if(type.substr(0,4)=="date" && !date_validation(valeur, 'YYYY-MM-DD'))
 	erreurs=erreurs+"<li>"+objet+" doit être au format JJ/MM/AAAA</li>";
       if(type=="date1"){
 	objet1=objet;
@@ -799,7 +756,7 @@ $(function(){
 
     // Message display when text input is invalid
 
-    $('input[type=text]').on('keyup',function(e) {
+    $('input[type=text]:not(.start-date):not(.end-date)').on('keyup',function(e) {
       var required = typeof($(this).attr('required')) != 'undefined';
       var valid_text = text_validation($(this).val());
       
