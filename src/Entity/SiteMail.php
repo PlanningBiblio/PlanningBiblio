20e39bc89afc2a51572bfdcf1927a65bd9dde44b
mfu <mfu@decalog.net>
2026-08-07 16:44:19 +0200

    Site Management: Add database entity / CRUD interface
    Currently, site configuration relies on static parameters without a dedicated database schema.
    
    With this change, the Site entity is created alongside the database migration and necessary permissions to lay the foundation for dynamic multisite management. Futhermore, administrators can create, edit, and list sites directly through a dedicated interface.
    
    Test Plan:
     - Run the migration: php bin/console doctrine:migrations:execute --up "App\Migrations\Version20260804141602"
     - Go to menu -> Administration -> Configuration des sites or /sites
     - Create, edit, and delete a site

---
+++ src/Entity/SiteMail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'site_mail')]
+class SiteMail
+{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Site::class)]
+    private ?Site $site = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $mail = '';
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSite(): ?Site
+    {
+        return $this->site;
+    }
+
+    public function setSite(?Site $site): self
+    {
+        $this->site = $site;
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(?string $mail): void
+    {
+        $this->mail = $mail;
+    }
+}
