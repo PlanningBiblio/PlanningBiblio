dc9d649451a0f3a7f4f9175a0f5200507f22b079
Julian Maurice <julian.maurice@biblibre.com>
2026-09-01 15:18:52 +0200

    MT53885: Add tests of PresentSet with PlanningHebdo enabled
    Also remove direct usage of global $config

--- tests/Planno/PresentSetTest.php
+++ tests/Planno/PresentSetTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Planno;
 
 use App\Entity\Absence;
 use App\Entity\Agent;
+use App\Entity\WorkingHour;
 use App\Planno\DateTime\TimeSlot;
 use App\Planno\PresentSet;
 use DateTime;
@@ -17,16 +18,101 @@ class PresentSetTest extends KernelTestCase
     {
         self::bootKernel();
 
-        // TODO: Do the same tests but with PlanningHebdo enabled
-        $GLOBALS['config']['PlanningHebdo'] = 0;
-
         $container = static::getContainer();
 
-        $presentSet = $container->get(PresentSet::class);
         $em = $container->get(EntityManagerInterface::class);
+
+        $configRepository = $em->getRepository('App\Entity\Config');
+        $configRepository->setParam('PlanningHebdo', 0);
+
+        $presentSet = $container->get(PresentSet::class);
 
         $builder = new FixtureBuilder();
         $agent = $builder->build(Agent::class, ['actif' => 'Actif']);
+
+        $presents = $presentSet->all('2026-07-31');
+        $this->assertCount(1, $presents);
+        $this->assertEquals(
+            [
+                'id' => $agent->getId(),
+                'nom' => sprintf('%s %s', $agent->getLastname(), $agent->getFirstname()),
+                'site' => null,
+                'heures' => '09h00 - 13h00 & 14h00 - 17h00',
+            ],
+            $presents[0]
+        );
+
+        $absence = $builder->build(
+            Absence::class,
+            [
+                'perso_id' => $agent->getId(),
+                'debut' => new DateTime('2026-07-31 14:00'),
+                'fin' => new DateTime('2026-07-31 17:00'),
+                'valide' => 1,
+                'groupe' => '',
+            ],
+        );
+
+        $presents = $presentSet->all('2026-07-31');
+        $this->assertCount(1, $presents);
+        $this->assertEquals(
+            [
+                'id' => $agent->getId(),
+                'nom' => sprintf('%s %s', $agent->getLastname(), $agent->getFirstname()),
+                'site' => null,
+                'heures' => '09h00 - 13h00',
+            ],
+            $presents[0]
+        );
+
+        $absence = $builder->build(
+            Absence::class,
+            [
+                'perso_id' => $agent->getId(),
+                'debut' => new DateTime('2026-07-31 09:00'),
+                'fin' => new DateTime('2026-07-31 13:00'),
+                'valide' => 1,
+                'groupe' => '',
+            ],
+        );
+
+        $presents = $presentSet->all('2026-07-31');
+        $this->assertCount(0, $presents);
+    }
+
+    public function testAllWithPlanningHebdo(): void
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+
+        $em = $container->get(EntityManagerInterface::class);
+
+        $configRepository = $em->getRepository('App\Entity\Config');
+        $configRepository->setParam('PlanningHebdo', 1);
+
+        $presentSet = $container->get(PresentSet::class);
+
+        $builder = new FixtureBuilder();
+        $agent = $builder->build(Agent::class, ['actif' => 'Actif']);
+
+        $workingHour = new WorkingHour();
+        $workingHour->setUser($agent->getId());
+        $workingHour->setStart(new DateTime('2026-01-01'));
+        $workingHour->setEnd(new DateTime('2026-12-31'));
+        $workingHour->setValidLevel2(1);
+        $workingHour->setCurrent(true);
+        $workingHour->setWorkingHours([
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+            ['09:00:00', '13:00:00', '14:00:00', '17:00:00', '1'],
+        ]);
+        $em->persist($workingHour);
+        $em->flush();
 
         $presents = $presentSet->all('2026-07-31');
         $this->assertCount(1, $presents);
